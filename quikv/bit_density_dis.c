080e7fd497de5fa11360209ac2a17959bdfb6395
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
1995-12-14 00:00:00 +0000

    created new bit_density command

---
+++ quikv/bit_density_dis.c
@@ -0,0 +1,45 @@
+/* vlba bit_density display function */
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+
+#include "../include/params.h"
+#include "../include/fs_types.h"
+#include "../include/fscom.h"
+#include "../include/shm_addr.h"
+
+#define MAX_OUT 256
+
+void bit_density_dis(command,itask,ip)
+struct cmd_ds *command;
+int itask;
+long ip[5];
+{
+      int lclc;
+      int count, i;
+      char output[MAX_OUT];
+
+      lclc=shm_addr->bit_density;
+
+   /* format output buffer */
+
+      strcpy(output,command->name);
+      strcat(output,"/");
+
+      count=0;
+      while( count>= 0) {
+        if (count > 0) strcat(output,",");
+        count++;
+        bit_density_enc(output,&count,&lclc);
+      }
+
+      if(strlen(output)>0) output[strlen(output)-1]='\0';
+
+      for (i=0;i<5;i++) ip[i]=0;
+      cls_snd(&ip[0],output,strlen(output),0,0);
+      ip[1]=1;
+
+      return;
+
+}
