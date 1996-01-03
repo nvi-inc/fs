24b4b140502c00487a0e754773120dacf6309cc7
Ed Himwich <ed.himwich@nviinc.com>
1996-01-03 00:00:00 +0000

    various changes
    1995-12-14
    created new bit_density command
    
    1996-01-03
    use "vd" as error code

---
+++ quikv/bit_density.c
@@ -0,0 +1,51 @@
+/* vlba bit_density snap command */
+
+#include <stdio.h> 
+#include <string.h> 
+#include <sys/types.h>
+
+#include "../include/params.h"
+#include "../include/fs_types.h"
+#include "../include/fscom.h"         /* shared memory definition */
+#include "../include/shm_addr.h"      /* shared memory pointer */
+
+void bit_density(command,itask,ip)
+struct cmd_ds *command;                /* parsed command structure */
+int itask;                            /* sub-task, ifd number +1  */
+long ip[5];                           /* ipc parameters */
+{
+      int ilast, ierr, ind, ichold, i, count;
+      char *ptr;
+      int lcl;        /* local instance of bit_density command struc */
+
+      int bit_density_dec();                 /* parsing utilities */
+      char *arg_next();
+
+      ierr = 0;
+      if (command->equal != '=') {            /* read module */
+        bit_density_dis(command,itask,ip);
+	return;
+      } 
+
+/* if we get this far it is a set-up command so parse it */
+
+parse:
+      ilast=0;                                      /* last argv examined */
+      memcpy(&lcl,&shm_addr->bit_density,sizeof(lcl));
+
+      count=1;
+      while( count>= 0) {
+        ptr=arg_next(command,&ilast);
+        ierr=bit_density_dec(&lcl,&count, ptr);
+        if(ierr !=0 ) goto error;
+      }
+
+      memcpy(&shm_addr->bit_density,&lcl,sizeof(lcl));
+      
+error:
+      ip[0]=0;
+      ip[1]=0;
+      ip[2]=ierr;
+      memcpy(ip+3,"vd",2);
+      return;
+}
