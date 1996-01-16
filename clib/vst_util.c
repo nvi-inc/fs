dfef5650a48c5a90a9a619f6047cc6cff7acd93d
Ed Himwich <ed.himwich@nviinc.com>
1996-01-16 00:00:00 +0000

    various changes
    1995-12-19
    use cips internally and for display, accept 133.33 and 266.66 input
    use bit_density and formatter tape clock for default
    
    1996-01-05
    changed to handle both 120.00 and 135.00 and double speed versions
    
    1996-01-16
    accept M3 real speeds as well as 0.01 ips resolution versions

--- clib/vst_util.c
+++ clib/vst_util.c
@@ -5,21 +5,32 @@
 #include <sys/types.h>
 #include <limits.h>
 #include <math.h>
-#include "../include/params.h"
-#include "../include/macro.h"
-#include "../include/vst_ds.h"
 
-static char *sp1_key[ ]={"0","3.375","7.875","16.875","33.75","67.5","135",
-                         "270"};
-static char *sp2_key[ ]={"0","3",    "7",    "15",    "30",   "60",  "120",
-                         "240"};
-static float sp3_key[ ]={0,   3.375,  7.875,  16.875,  33.75,  67.5,  135., 
-                          270.};
+#include "../include/macro.h"
+#include "../include/params.h"
+#include "../include/fs_types.h"
+#include "../include/fscom.h"         /* shared memory definition */
+#include "../include/shm_addr.h"      /* shared memory pointer */
+
+static char *sp1_key[ ]={"0","3.38","7.88","16.88","33.75","67.5","133.33",
+                         "135", "266.66", "270"};
+static char *sp2_key[ ]={"0","3",    "7",    "15",    "30",   "60","133.33",
+                         "135", "266.66", "270"};
+static int  sp3_key[ ]={0,   338,    788,   1688,    3375,   6750,  13333,
+                          13500,  26666,   27000};
+static char *sp4_key[ ]={"0","3",    "7",    "15",    "30",   "60","133.33",
+                         "120", "266.66", "240"};
+static char *sp5_key[ ]={"0","3.375","7.875","16.875","33.75","67.5","133.33",
+                         "135", "266.66", "270"};
+
 static char *dir_key[ ]={ "rev","for"};
 static char *rec_key[ ]={ "off","on"};
 
 #define SP1_KEY sizeof(sp1_key)/sizeof( char *)
 #define SP2_KEY sizeof(sp2_key)/sizeof( char *)
+#define SP3_KEY sizeof(sp3_key)/sizeof( char *)
+#define SP4_KEY sizeof(sp4_key)/sizeof( char *)
+#define SP5_KEY sizeof(sp5_key)/sizeof( char *)
 #define DIR_KEY sizeof(dir_key)/sizeof( char *)
 #define REC_KEY sizeof(rec_key)/sizeof( char *)
 
@@ -41,21 +52,49 @@ char *ptr;
         break;
       case 2:
         ierr=arg_key(ptr,sp1_key,SP1_KEY,&lcl->speed,0,FALSE);
-        if (ierr !=0) {
-          ierr=0;
+        if (ierr !=0)
           ierr=arg_key_flt(ptr,sp2_key,SP2_KEY,&lcl->speed,0,FALSE);
-        }
+        if (ierr !=0)
+          ierr=arg_key_flt(ptr,sp4_key,SP4_KEY,&lcl->speed,0,FALSE);
+        if (ierr !=0)
+          ierr=arg_key_flt(ptr,sp5_key,SP5_KEY,&lcl->speed,0,FALSE);
+	if (ierr==0) {
+	  lcl->cips=sp3_key[lcl->speed];
+	  lcl->speed=-3;
+	} else if (ierr == -100 && (
+		   (shm_addr->equip.rack == VLBA && 
+		    shm_addr->bit_density > 0    &&
+		    shm_addr->vform.tape_clock > 0) ||
+				    (shm_addr->equip.rack == MK3 &&
+				     shm_addr->iratfm > 0))){
+	  lcl->speed=-3;
+	  ierr = 0;
+	  if (shm_addr->equip.rack == MK3) {
+	    int idum;
+
+	    idum=shm_addr->iratfm;
+	    if(idum=0)
+	      idum=8;
+	    lcl->cips=100*((9e6/(1<<8-idum))/shm_addr->bit_density);
+	  } else if (shm_addr->equip.rack == VLBA)
+	    if (shm_addr->vform.tape_clock<8)
+	      lcl->cips=100*((9.072e6/(1<<(0x7-shm_addr->vform.tape_clock)))/
+			     shm_addr->bit_density); 
+	    else
+	      lcl->cips=100*((9e6/(1<<(0xf-shm_addr->vform.tape_clock)))/
+			     shm_addr->bit_density);
+	  }
         break;
       case 3:
         ierr=arg_key(ptr,rec_key,REC_KEY,&lcl->rec,1,TRUE);
         break;
       default:
        *count=-1;
-   }
+      }
 
-   if(ierr!=0) ierr-=*count;
-   if(*count>0) (*count)++;
-   return ierr;
+    if(ierr!=0) ierr-=*count;
+    if(*count>0) (*count)++;
+    return ierr;
 }
 
 void vst_enc(output,count,lcl)
@@ -81,6 +120,8 @@ struct vst_cmd *lcl;
           strcpy(output,"330");
         else if (ivalue == -2)
           strcpy(output,"360");
+	else if (ivalue == -3)
+	  sprintf(output,"%d.%02d",lcl->cips/100,lcl->cips%100);
         else if (ivalue >=0 && ivalue <SP1_KEY)
           strcpy(output,sp1_key[ivalue]);
         else
@@ -106,23 +147,21 @@ struct vst_cmd *lcl;
 void vstb1mc(data,lcl)
 unsigned *data;
 struct vst_cmd *lcl;
-
 {
 
   *data = (bits16on(1) & lcl->dir);
 
-       return;
+  return;
 }
 
 void vstb5mc(data,lcl)
 unsigned *data;
 struct vst_cmd *lcl;
-
 {
 
-  *data = (bits16on(16) &  (int)(sp3_key[lcl->speed]*100.0));
+  *data = bits16on(16) & (lcl->cips);
 
-       return;
+  return;
 
 }
 
@@ -130,20 +169,15 @@ void mcb5vst(lcl, data)
 struct vst_cmd *lcl;
 unsigned data;
 {
-int ivalue;
-int i;
+  int ivalue;
+  int i;
   
-   ivalue = ( data >>  0 ) & bits16on(16);
-   if (ivalue == 0x80e8)   /* speed is 330 */
-     lcl->speed = -1;
-   else if (ivalue == 0x8ca0)   /* speed is 360 */
-     lcl->speed = -2;
-   else {
-     for (i=0;i< SP1_KEY;i++) {
-       if (( fabs ((double)((float)ivalue/100) - sp3_key[i])) < 1e-5)
-         lcl->speed=i;
-     }
-   }
+  ivalue = ( data >>  0 ) & bits16on(16);
+
+
+  lcl->speed = -3;
+  lcl->cips = ivalue;
+
   return;
 
 }
