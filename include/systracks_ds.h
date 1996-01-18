a2048e9ffb256b0e00dcdf19ac7300e2da5944ad
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
1996-01-18 00:00:00 +0000

    created

---
+++ include/systracks_ds.h
@@ -0,0 +1,5 @@
+/* header file for vlba systracks function data structures */
+
+struct systracks_cmd {   /* command parameters */
+     int track[4];       /* vlba track number, or -1 for diabled */
+     };
