f9e2b55116744ebff4e21911096da701f3ecaa47
Nancy Vandenberg <>
1996-02-13 00:00:00 +0000

    Missing argument for sideband in call to unplo. Format of "L" line was missing the channel assignment.

--- skdrut/frinp.f
+++ skdrut/frinp.f
@@ -4,7 +4,9 @@ C     This routine reads and decodes one line in the $CODES section.
 C     Call in a loop to get all values in freqs.ftni filled in,
 C     then call SETBA to figure out which frequency bands are there.
 C
-       INCLUDE 'skparm.ftni'
+      include '../skdrincl/skparm.ftni'
+      include '../skdrincl/freqs.ftni'
+      include '../skdrincl/statn.ftni'
 C
 C  INPUT:
       integer*2 IBUF(*)
@@ -17,16 +19,16 @@ C  OUTPUT:
       integer ierr
 C     IERR - error number
 C
-       INCLUDE 'freqs.ftni'
-       INCLUDE 'statn.ftni'
 C
 C  LOCAL:
       integer ITRK(4,max_pass),idum
 C     integer*2 IP(14)
       integer*2 LNA(4)
-      integer ii,ivc,i,icode,istn,inum,itype,is,ns,ibad,j
+      integer ic,iv,ii,ivc,i,icode,istn,inum,itype,is,ns,ibad,j
+      integer ix,iy,iscn_ch,icx,nvlist,ivlist(max_chan)
       integer*2 lc,lsg,lm(4),lid,lin,ls,lsub(4)
       integer*2 lst(4,max_stn)
+      real*8 bitden
       logical kvlba,kmk3
       character*3 cs
       real*4 f1,f2,f,vb,rbbc
@@ -40,6 +42,14 @@ C            nrv implicit none
 C     930421 nrv Re-added: store track assignments
 C 951019 nrv Add extension of LO lines to include per channel
 C 951116 nrv Change to frequency sequencey per station
+C 960124 nrv Missing argument in call to UNPLO
+C 960212 nrv Missing argument in UNPLO
+C 960213 nrv Uppercase the frequency code
+C 960220 nrv Save LNAFRSUB even if no station names on the "F" line.
+C 960221 nrv Read switching from "C" lines, also BBC index.
+C 960223 nrv Fill in "L" line info on per channel basis
+C 960228 nrv Change unplo call to include VC assignments from PC-SCHED
+C            patching info.
 C
 C
 C     1. Find out what type of entry this is.  Decode as appropriate.
@@ -49,33 +59,41 @@ C
       IF (JCHAR(IBUF,1).EQ.OCAPC) ITYPE=1 ! C frequency sequence lines
       IF (JCHAR(IBUF,1).EQ.OCAPL) ITYPE=2 ! L LO lines
       IF (ITYPE.EQ.1) CALL UNPCO(IBUF(2),ILEN-1,IERR,
-     .                LC,LSG,F1,F2,IVC,LM,VB,ITRK)
+     .                LC,LSG,F1,F2,Icx,LM,VB,ITRK,cs,ivc)
       IF (ITYPE.EQ.2) CALL UNPLO(IBUF(2),ILEN-1,IERR,
-     .                LID,LC,LSG,LIN,F,ivc,cs)
+     .                LID,LC,LSG,LIN,F,ivlist,ls,nvlist)
       IF (ITYPE.EQ.3) CALL UNPFSK(IBUF(2),ILEN-1,IERR,LNA,LC,lsub,lst,
      .                ns)
+      call hol2upper(lc,2) ! uppercase frequency code
 C
 C 1.5 If there are errors, handle them first.
 C
       IF  (IERR.NE.0) THEN
         IERR = -(IERR+100)
-        write(lu,9201) ierr,(ibuf(i),i=2,ilen/2)
+        write(lu,9201) ierr,(ibuf(i),i=2,ilen)
 9201    format('FRINP01 - Error in field ',I3,' of this line:'/40a2)
         RETURN
       END IF 
 C  Lines need not be in order, so we may encounter a new code
 C  on any line. But this is a bad practice.
       IF  (IGTFR(LC,ICODE).EQ.0) THEN !a new code
-        NCODES = NCODES + 1
-        IF  (NCODES.GT.MAX_FRQ) THEN !too many codes
-          IERR = MAX_FRQ
-          ncodes=ncodes-1
-          write(lu,9202) ierr
-9202      format('FRINP02 - Too many frequency codes.  Max is ',I3,
-     .    ' codes.')
-          RETURN
-        END IF  !too many codes
-        ICODE = NCODES
+        if (itype.eq.3) then ! "F" line
+          NCODES = NCODES + 1
+          IF  (NCODES.GT.MAX_FRQ) THEN !too many codes
+            IERR = MAX_FRQ
+            ncodes=ncodes-1
+            write(lu,9202) ierr
+9202        format('FRINP02 - Too many frequency codes.  Max is ',I3,
+     .      ' codes.')
+            RETURN
+          END IF  !too many codes
+          ICODE = NCODES
+        else ! not allowed
+          write(lu,9203) (ibuf(i),i=2,ilen)
+9203      format('FRINP03 - Warning! A new frequency code was found out',
+     .    ' of order on the following line and was ignored:'/40a2)
+          return
+        endif
       END IF  !a new code
 C
 C     2. Now decide what to do with this information.
@@ -85,18 +103,35 @@ C
         do j=1,nstsav ! apply to each station on the preceding "F" line
           is=istsav(j)
           nvcs(is,icode)=nvcs(is,icode)+1 ! count them
-          invcx(nvcs(is,icode),is,icode)=ivc ! index number from "C" line
-          LSUBVC(IVC,is,ICODE) = LSG ! sub-group, i.e. S or X
-          FREQRF(IVC,is,ICODE) = F1
-          VCBAND(ivc,is,ICODE) = VB
+          invcx(nvcs(is,icode),is,icode)=icx ! index number from "C" line
+          LSUBVC(icx,is,ICODE) = LSG ! sub-group, i.e. S or X
+          FREQRF(icx,is,ICODE) = F1
+          VCBAND(icx,is,ICODE) = VB
           LCODE(ICODE) = LC ! 2-letter code for the sequence
           idum = ichmv(LMODE(1,is,ICODE),1,LM,1,8) ! recording mode
+C         Set bit density depending on the mode
+          ix = iscn_ch(lmode(1,is,icode),1,8,'1:') 
+          iy = iscn_ch(lmode(1,is,icode),1,8,':1') 
+          if (ix.gt.0.or.iy.gt.0) then ! a VLBA mode
+            bitden=34020
+          else ! Mark III
+            bitden=33333
+          endif
+C         If specified for a station, use that value
+          if (bitden_save(is).gt.1000) bitden=bitden_save(is)
+          bitdens(is,icode)=bitden
           DO  I=1,max_pass
-            IF (ITRK(1,I).NE.-99) itras(1,1,ivc,i,is,icode) = itrk(1,i)
-            IF (ITRK(2,I).NE.-99) itras(2,1,ivc,i,is,icode) = itrk(2,i)
-            IF (ITRK(3,I).NE.-99) itras(1,2,ivc,i,is,icode) = itrk(3,i)
-            IF (ITRK(4,I).NE.-99) itras(2,2,ivc,i,is,icode) = itrk(4,i)
+            IF (ITRK(1,I).NE.-99) itras(1,1,icx,i,is,icode) = itrk(1,i)
+            IF (ITRK(2,I).NE.-99) itras(2,1,icx,i,is,icode) = itrk(2,i)
+            IF (ITRK(3,I).NE.-99) itras(1,2,icx,i,is,icode) = itrk(3,i)
+            IF (ITRK(4,I).NE.-99) itras(2,2,icx,i,is,icode) = itrk(4,i)
           END DO 
+          cset(icx,is,icode) = cs
+          if (ivc.eq.0) then
+            ibbcx(icx,is,icode) = icx
+          else
+            ibbcx(icx,is,icode) = ivc ! BBC number
+          endif
         enddo ! each station on "F" line
       END IF  !code entry
 C
@@ -107,50 +142,70 @@ C
         IF  (IGTST(LID,ISTN).EQ.0) THEN  !error
           write(lu,'("FRINP03 - Station ",a2," not selected.",
      .    " LO entry on the following line ignored:"/40a2)') lid,
-     .    (ibuf(i),i=2,ilen/2)
+     .    (ibuf(i),i=2,ilen)
           IERR = MAX_STN
           RETURN
         END IF  !error
 C
-        if (ivc.ne.0) then ! per-channel info present
-          if (lsg.ne.lsubvc(ivc,istn,icode)) 
-     .      write(lu,'("FRINP04 - Subgroup ",a2," inconsistent with ",
-     .      a2," for channel ",i3,", station ",4a2)') lsg,
-     .      lsubvc(ivc,istn,icode),ivc,(lstnna(i,istn),i=1,4)
-          if (lc.ne.lcode(icode)) 
-     .      write(lu,'("FRINP05 - Code ",a2," inconsistent with ",
-     .      a2," for channel ",i3,", station ",4a2)') lc,lcode(icode),
-     .      ivc,(lstnna(i,istn),i=1,4)
-          LIFINP(ivc,istn,ICODE) = LIN ! IF input channel
-          FREQLO(ivc,ISTN,ICODE) = F
-          ivix(ivc,istn,icode) = ivc
-          losb(ivc,istn,icode) = ls
-          cset(ivc,istn,icode) = cs
-        else ! fill in per-channel with certain assumptions
+        if (nvlist.ne.0) then ! physical BBC info present
+          if (nvlist.eq.1) then ! one BBC on this line
+          do iv=1,nvcs(istn,icode)
+            ic=invcx(iv,istn,icode)
+            if (ic.gt.0.and.ibbcx(ic,istn,icode).eq.ivlist(1)) then 
+C                                    ! this channel on this BBC
+              if (lsg.ne.lsubvc(ic,istn,icode)) 
+     .        write(lu,'("FRINP04 - Subgroup ",a2," inconsistent with "
+     .        ,a2," for channel ",i3,", station ",4a2)') lsg,
+     .        lsubvc(ic,istn,icode),ic,(lstnna(i,istn),i=1,4)
+              if (lc.ne.lcode(icode)) 
+     .        write(lu,'("FRINP05 - Code ",a2," inconsistent with ",
+     .        a2," for channel ",i3,", station ",4a2)') lc,lcode(icode)
+     .        ,ic,(lstnna(i,istn),i=1,4)
+              LIFINP(ic,istn,ICODE) = LIN ! IF input channel
+              FREQLO(ic,ISTN,ICODE) = F ! LO freq
+              losb(ic,istn,icode) = ls ! sideband
+            endif
+          enddo
+          else ! many BBCs on this line (from PC-SCHED)
+            do iv=1,nvlist
+              ic=ivlist(iv)
+              if (lsg.ne.lsubvc(ic,istn,icode)) 
+     .        write(lu,'("FRINP04 - Subgroup ",a2," inconsistent with "
+     .        ,a2," for channel ",i3,", station ",4a2)') lsg,
+     .        lsubvc(ic,istn,icode),ic,(lstnna(i,istn),i=1,4)
+              if (lc.ne.lcode(icode)) 
+     .        write(lu,'("FRINP05 - Code ",a2," inconsistent with ",
+     .        a2," for channel ",i3,", station ",4a2)') lc,lcode(icode)
+     .        ,ic,(lstnna(i,istn),i=1,4)
+              LIFINP(ic,istn,ICODE) = LIN ! IF input channel
+              FREQLO(ic,ISTN,ICODE) = F ! LO freq
+              losb(ic,istn,icode) = ls ! sideband
+            enddo
+          endif ! one/many 
+        else ! fill physical BBC/IF/LO info assuming all channels get same
           do i=1,nvcs(istn,icode)
-            if (lsg.eq.lsubvc(i,istn,icode)) then ! match sub-group
-              if (ivix(i,istn,icode).eq.0) then ! first time
-                LIFINP(i,istn,ICODE) = LIN 
-                FREQLO(i,ISTN,ICODE) = F
-                ivix(i,istn,icode) = i
-                call char2hol('U ',losb(i,istn,icode),1,2)
-                cset(i,istn,icode) = '   '
+            iv=invcx(i,istn,icode) ! channel index assumed same as BBC#
+            if (lsg.eq.lsubvc(iv,istn,icode)) then ! match sub-group
+              if (ichcm_ch(lifinp(iv,istn,icode),1,'  ').eq.0) then ! first time
+                LIFINP(iv,istn,ICODE) = LIN 
+                FREQLO(iv,ISTN,ICODE) = F
+                call char2hol('U ',losb(iv,istn,icode),1,2)
               else ! had a previous LO already
-                rbbc=abs(freqlo(i,istn,icode)-freqrf(i,istn,icode))
-                kmk3 =ichcm_ch(lifinp(i,istn,icode),1,'1N').eq.0.or.
-     .                ichcm_ch(lifinp(i,istn,icode),1,'2N').eq.0.or.
-     .                ichcm_ch(lifinp(i,istn,icode),1,'3N').eq.0.or.
-     .                ichcm_ch(lifinp(i,istn,icode),1,'1A').eq.0.or.
-     .                ichcm_ch(lifinp(i,istn,icode),1,'2A').eq.0.or.
-     .                ichcm_ch(lifinp(i,istn,icode),1,'3A').eq.0
-                kvlba=ichcm_ch(lifinp(i,istn,icode),1,'A').eq.0.or.
-     .                ichcm_ch(lifinp(i,istn,icode),1,'B').eq.0.or.
-     .                ichcm_ch(lifinp(i,istn,icode),1,'C').eq.0.or.
-     .                ichcm_ch(lifinp(i,istn,icode),1,'D').eq.0
+                rbbc=abs(freqlo(iv,istn,icode)-freqrf(i,istn,icode))
+                kmk3 =ichcm_ch(lifinp(iv,istn,icode),1,'1N').eq.0.or.
+     .                ichcm_ch(lifinp(iv,istn,icode),1,'2N').eq.0.or.
+     .                ichcm_ch(lifinp(iv,istn,icode),1,'3N').eq.0.or.
+     .                ichcm_ch(lifinp(iv,istn,icode),1,'1A').eq.0.or.
+     .                ichcm_ch(lifinp(iv,istn,icode),1,'2A').eq.0.or.
+     .                ichcm_ch(lifinp(iv,istn,icode),1,'3A').eq.0
+                kvlba=ichcm_ch(lifinp(iv,istn,icode),1,'A').eq.0.or.
+     .                ichcm_ch(lifinp(iv,istn,icode),1,'B').eq.0.or.
+     .                ichcm_ch(lifinp(iv,istn,icode),1,'C').eq.0.or.
+     .                ichcm_ch(lifinp(iv,istn,icode),1,'D').eq.0
                 if ((rbbc.gt.1000.0.and.kvlba).or.
      .              (rbbc.gt.500.0.and.kmk3)) then
-                  LIFINP(i,istn,ICODE) = LIN 
-                  FREQLO(i,ISTN,ICODE) = F
+                  LIFINP(iv,istn,ICODE) = LIN 
+                  FREQLO(iv,ISTN,ICODE) = F
                 endif
               endif ! previous LO/first time
             endif ! match sub-group
@@ -187,6 +242,7 @@ C       Check the list of station names.
           nstsav=nstatn
           do i=1,nstatn
             istsav(i)=i ! save the station index 
+            idum= ICHMV(LNAFRsub(1,i,ICODE),1,lsub,1,8)
           enddo
         endif
         idum= ICHMV(LNAFRQ(1,ICODE),1,LNA,1,8)
