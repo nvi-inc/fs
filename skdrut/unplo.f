84d298493514b68af0f856a2412e274f3e420135
Nancy Vandenberg <>
1996-02-13 00:00:00 +0000

    Missing argument for sideband in call to unplo. Format of "L" line was missing the channel assignment.

--- skdrut/unplo.f
+++ skdrut/unplo.f
@@ -1,10 +1,10 @@
       SUBROUTINE unplo(IBUF,ILEN,IERR,
      .LIDSTN,LCODE,LSUBGR,LIFINP,FREQLO,
-     .iv,cs)
+     .iv,ls,nv)
 C
 C     UNPLO unpacks the record holding information on a LO configuration
 C
-      include 'skparm.ftni'
+      include '../skdrincl/skparm.ftni'
 
 C  History:
 C  900126 NRV Changed last parameter so no error is generated
@@ -15,6 +15,10 @@ C 951019 nrv Extend line to include index number and other info
 C 951020 nrv If info is missing, send back blanks or zeros, not error.
 C            This will make it backward compatible.
 C 951116 nrv Remove "ib" from call
+C 960124 nrv Missing SB argument in call.
+C 960121 nrv Remove switching and channel from call.
+C 960228 nrv Change iv to an array, add nv as count. Decode patching
+C            from PC-SCHED lines and use as BBC/VC assignments
 C
 C  INPUT:
       integer*2 IBUF(*)
@@ -23,10 +27,9 @@ C           - buffer having the record
 C     ILEN - length of the record in IBUF, in words
 C
 C  OUTPUT:
-      integer ierr,iv,is
+      integer ierr,iv(max_chan),nv
       integer*2 lidstn,lcode,lsubgr,lifinp,ls
       real*4 freqlo
-      character*3 cs
 C     IERR - error, 0=OK, -100-n=error reading nth field in the record
 C     LIDSTN - 1-char station ID
 C     LCODE - frequency code, 2 char
@@ -37,7 +40,7 @@ C
 C  LOCAL:
       real*8 DAS2B
       real*4 d
-      integer ich,nch,ic1,ic2,i,l,idumy
+      integer ivv,ich,nch,ic1,ic2,i,l,idumy
       integer ichcm_ch,jchar,ichmv,ias2b
 C
 C
@@ -82,10 +85,11 @@ C
 C
 C     IF distributor channel and input
 C     May be: IFA,IFB,IFC,IFD or IF1N,IF2N,IF1A,IF2A,IF3A
-C         of A,B,C,D
+C         oo A,B,C,D or 1N,2N,1A,2A,3N
 C
       CALL GTFLD(IBUF,ICH,ILEN*2,IC1,IC2)
-      if (ichcm_ch(ibuf,ic1,'IF').eq.0) then 
+      i=ias2b(ibuf,ic1,1) ! decode first character
+      if (ichcm_ch(ibuf,ic1,'IF').eq.0) then ! IFnx
         I = IAS2B(IBUF,IC1+2,1)
         if (i.gt.0) then ! valid Mark III channel
           L = JCHAR(IBUF,IC1+3)
@@ -104,8 +108,16 @@ C
           endif
           IDUMY = ICHMV(LIFINP,1,ibuf,ic1+2,2)
         endif
+      else if (i.ge.1.and.i.le.3) then ! IF1,2,3
+        l=jchar(ibuf,ic1+1)
+        IF  ((I.NE.1.AND.I.NE.2.and.i.ne.3).OR.
+     .  (L.NE.OCAPA.AND.L.NE.OCAPN)) THEN
+          IERR = -104
+          RETURN
+        END IF  !
+        IDUMY = ICHMV(LIFINP,1,ibuf,ic1,2)
       else ! VLBA 1-letter channel
-        l = jchar(ibuf,ic1+2)
+        l = jchar(ibuf,ic1)
         if (l.ne.ocapa.and.l.ne.ocapb.and.
      .      l.ne.ocapc.and.l.ne.ocapd) then
           ierr=-104
@@ -125,26 +137,45 @@ C
       FREQLO = D
 C
 C From here on, older schedules may not have the info.
-C Schedules from PC-SCHED may have patching here.
-C
-C Initialize for missing information:
-      iv=0
-      call char2hol ('  ',LS,1,2)
-      cs = '   '
+C Schedules from PC-SCHED may have patching here, pick up and
+C use as physical BBC/VC assignments.
 
-C     VC# or BBC# to be recorded
-C
+C Initialize for missing information:
+      do i=1,max_chan
+        iv(i)=0
+      enddo
+      nv=0
+      call char2hol ('  ',LS,1,2)
+
+C     VC# or BBC# assigned to this channel
+
       CALL GTFLD(IBUF,ICH,ILEN*2,IC1,IC2)
       if (ic1.eq.0) return
       NCH = IC2-IC1+1
-      iv = ias2b(ibuf,ic1,nch)
-      if (iv.lt.0) then ! illegal, assume PC-SCHED line
-        iv=0
-        return
-      endif
-      IF  ((iv.lt.1.or.iv.gt.max_chan)) THEN  !
-        IERR = -106
-        RETURN
+      ivv = ias2b(ibuf,ic1,nch)
+      IF  ((ivv.lt.1.or.ivv.gt.max_chan)) THEN  ! check for patching
+        if (jchar(ibuf,ic2).eq.ocaph.or.jchar(ibuf,ic2).eq.ocapl) then
+C              trailing "L" or "H" means patching
+          nv=0
+          do while (ic1.gt.0) ! get patching fields
+            ivv = ias2b(ibuf,ic1,nch-1)
+            if (ivv.lt.1.or.ivv.gt.max_chan) then ! error
+              ierr=-106-nv
+              return
+            endif
+            nv=nv+1
+            iv(nv)=ivv
+            call gtfld(ibuf,ich,ilen*2,ic1,ic2)
+            nch=ic2-ic1+1
+          enddo
+          return ! no more on the line 
+        else
+          IERR = -106
+          RETURN
+        endif
+      else
+        nv=1
+        iv(nv)=ivv
       END IF  !
 C
 C     SB - Side Band for this BBC, either U or L
@@ -154,24 +185,11 @@ C
       NCH = ic2-ic1+1
       if ((jchar(ibuf,ic1).ne.OCAPU.and.jchar(ibuf,ic1).ne.OCAPL)
      ..or.nch.ne.1) then
-        IERR = -106
+        IERR = -107
         RETURN
         END IF  !
       call char2hol ('  ',LS,1,2)
       IDUMY = ICHMV(LS,1,ibuf,ic1,1)
-C
-C     Set number - 0 1 2 or 1,2
-C
-      CALL GTFLD(IBUF,ICH,ILEN*2,IC1,IC2)
-      if (ic1.eq.0) return
-      NCH = IC2-IC1+1
-      cs = '   '
-      call hol2char(ibuf,ic1,ic2,cs)
-      IF ((cs(1:1).ne.'1').and.(cs(1:1).ne.'2').and.
-     .    (cs(1:1).ne.'0').and.(cs.ne.'1,2')) then
-        IERR = -107
-        RETURN
-      END IF  !
 C
       RETURN
       END
