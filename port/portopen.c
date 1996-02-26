da0e42090d67ad6d292d212df6581806543a926a
Ed Himwich <ed.himwich@nviinc.com>
1996-02-26 00:00:00 +0000

    various changes
    1996-02-02 Ari Mujunen
    support 19200-115200 baud rates on Digiboard
    
    1996-02-26 Ed Himwich
    add support for 50 and 75 BAUD
    
    Co-authored-by: Ari Mujunen <>

--- port/portopen.c
+++ port/portopen.c
@@ -3,6 +3,8 @@
 #include <fcntl.h>
 #include <termio.h>
 #include <linux/serial.h>
+#include <sys/errno.h>
+#include "/usr/src/linux/drivers/char/digi.h"  /* yechh, abs. path... */
 
 int portopen_(port, name, len, baud, parity, bits, stop)
 int *port;
@@ -108,6 +110,12 @@ int *stop;
     return -7;
   else
   switch ((int)*baud){
+    case 50:
+      term.c_cflag |= B50;
+      break;
+    case 75:
+      term.c_cflag |= B75;
+      break;
     case 110:
       term.c_cflag |= B110;
       break;
@@ -144,42 +152,103 @@ int *stop;
     default:
       return -7;
   };
-  if(ioctl (*port, TCSETA, &term)==-1)
+  if (ioctl (*port, TCSETA, &term)==-1)
     return -8;
 
   /* We attempt to adjust 38400 baud into higher speeds. */
   /* Linux can give 57600 or 115200 when std Unix 'termios' call */
   /* requests for 38400. */
-  if (*baud > 19200)  /* why bother setting if it is less? */
+  /* Now we need to adjust the special bits whenever "slower" */
+  /* baud rates are requested, too: namely DigiBoard wants */
+  /* to reuse 50/75/110 baud and to get _these_ slow rates, */
+  /* we must cancel the fastbaud bit... */
+  if (*baud > 0)
   {
     struct serial_struct allSerialSettings;
     int oldBits;
 
-    if(ioctl(*port, TIOCGSERIAL, &allSerialSettings)==-1) {
-        return -9;
-    }
-    oldBits = allSerialSettings.flags & ASYNC_SPD_MASK;
+    if (ioctl(*port, TIOCGSERIAL, &allSerialSettings) == 0) {
+      /* We can do the change in Linux 'serial.c' way. */
+      oldBits = allSerialSettings.flags & ASYNC_SPD_MASK;
 
-    /* Zero the SPD bits first.  (== "normal" 38400 baud) */
-    allSerialSettings.flags &= ~ASYNC_SPD_MASK;
+      /* Zero the SPD bits first.  (== "normal" 38400 baud) */
+      allSerialSettings.flags &= ~ASYNC_SPD_MASK;
 
-    if (*baud > 86400) {
-      /* 115200 baud. */
-      allSerialSettings.flags |= ASYNC_SPD_VHI;
-    } else if (*baud > 48000) {
-      /* 57600 baud. */
-      allSerialSettings.flags |= ASYNC_SPD_HI;
-    } else {
-      /* Leave at genuine 38400 baud (or lower.) */
-    }
+      if (*baud > 86400) {
+	/* 115200 baud. */
+	allSerialSettings.flags |= ASYNC_SPD_VHI;
+      } else if (*baud > 48000) {
+	/* 57600 baud. */
+	allSerialSettings.flags |= ASYNC_SPD_HI;
+      } else {
+	/* Leave at genuine 38400 baud (or lower.) */
+      }
 
-    /* Change (all the) serial settings only if they really need changing. */
-    if ((allSerialSettings.flags & ASYNC_SPD_MASK) != oldBits) {
-      if(ioctl(*port, TIOCSSERIAL, &allSerialSettings) == -1) {
-        return -10;
-       }
-    }
-  }  /* if > 19200 baud */
+      /* Change (all the) serial settings only if they really need changing. */
+      if ((allSerialSettings.flags & ASYNC_SPD_MASK) != oldBits) {
+	if(ioctl(*port, TIOCSSERIAL, &allSerialSettings) == -1) {
+	  return -10;
+	}
+      }
+    } else {  /* couldn't use 'serial.c'-style SPD_[V]HI */
+      if (errno == EINVAL) {
+        /* 'TIOCGSERIAL' didn't work, perhaps a Digiboard special will? */
+        digi_t digiSettings;
+        digi_t oldDigiSettings;
+	struct termio oldSettings;
+
+        /* We start by re-getting the current termio + Digi settings: */
+	if (ioctl(*port, TCGETA, &term) != 0) {
+	  return -3;
+	}
+	oldSettings = term;
+	if (ioctl(*port, DIGI_GETA, &digiSettings) != 0) {
+	  return -3;  /* xxx: perhaps a new code? */
+	}
+	oldDigiSettings = digiSettings;
+
+        if (*baud > 86400) {
+          /* 115200 baud. */
+          digiSettings.digi_flags |= DIGI_FAST;
+#define SET_SPEED(tp, speed) \
+    (tp)->c_cflag &= ~CBAUD; \
+    (tp)->c_cflag |= speed
+	  SET_SPEED(&term, B110);
+        } else if (*baud > 67200) {
+          /* 76800 baud. */
+          digiSettings.digi_flags |= DIGI_FAST;
+	  SET_SPEED(&term, B75);
+        } else if (*baud > 48000) {
+          /* 57600 baud. */
+          digiSettings.digi_flags |= DIGI_FAST;
+	  SET_SPEED(&term, B50);
+#undef SET_SPEED
+        } else {
+          /* Leave at genuine 38400 baud (or lower.) */
+          digiSettings.digi_flags &= ~DIGI_FAST;
+	  /* (The baud rate has been already set in standard settings
+	      above.) */
+        }
+
+        /* Change Digi settings + termio settings (termio again), */
+	/* if either Digi or std. baud rate changed. */
+	if (digiSettings.digi_flags != oldDigiSettings.digi_flags) {
+	  if (ioctl(*port, DIGI_SETAW, &digiSettings) != 0) {
+	    return -8;  /* xxx: perhaps a new code? */
+	  }
+	}
+	if (term.c_cflag != oldSettings.c_cflag) {
+	  if (ioctl (*port, TCSETA, &term) != 0) {
+	    return -8;
+	  }
+	}
+      } else {
+	/* Getting Linux-specific serial settings failed */
+	/* in other way than EINVAL... */
+	return -9;
+      }
+    }  /* else couldn't use 'serial.c'-style high speeds */
+  }  /* if > 0 baud ie. baud rate change required */
 
   return 0;
 }
