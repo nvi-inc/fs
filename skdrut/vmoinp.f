1aa300b324409fb92395688ba50f5238f32bf8cf
Nancy Vandenberg <>
1996-08-17 00:00:00 +0000

    Add S2 record mode routine. Add S2 group_order routine.

--- skdrut/vmoinp.f
+++ skdrut/vmoinp.f
@@ -8,6 +8,7 @@ C History
 C 960518 nrv New.
 C 960522 nrv Revised.
 C 960610 nrv Move initialization of freqs.ftni arrays here
+C 960817 nrv Add S2 record mode
 
       include '../skdrincl/skparm.ftni'
       include '../skdrincl/freqs.ftni'
@@ -40,7 +41,7 @@ C  LOCAL:
       integer nsubpass
       integer nchdefs,nbbcdefs,nifdefs,nfandefs,nhdpos,npl
       integer*2 lsb(max_chan),lsg(max_chan),lm(4),lin(max_ifd),
-     .ls(max_ifd)
+     .ls(max_ifd),ls2m(8)
       double precision bitden
       character*3 cs(max_chan)
       character*6 cfrbbref(max_chan),cbbcref(max_bbc),
@@ -51,6 +52,7 @@ C  LOCAL:
       character*128 cout
       integer ib2as,numc2,ichmv,ichmv_ch,ichcm_ch ! functions
       integer ptr_ch,fvex_len,fget_mode_def
+      logical km3rack,km4rack,kvrack,km4rec,km3rec,kvrec,ks2rec 
  
  
 C 1. First get all the mode def names. Station names have already
@@ -90,6 +92,18 @@ C    Assign a code to the mode and the same to the name
 
           il=fvex_len(modedefnames(icode))
           im=fvex_len(stndefnames(istn))
+C         Recognized recorder types
+          kvrec=ichcm_ch(lstrec(1,istn),1,'VLBA').eq.0
+          km3rec=ichcm_ch(lstrec(1,istn),1,'Mark3').eq.0
+          km4rec=ichcm_ch(lstrec(1,istn),1,'Mark4').eq.0
+          ks2rec=ichcm_ch(lstrec(1,istn),1,'S2').eq.0
+C         Recognized rack types
+          kvrack=ichcm_ch(lstrack(1,istn),1,'VLBA').eq.0
+     .    .or.ichcm_ch(lstrack(1,istn),1,'VLBAG').eq.0
+          km3rack=ichcm_ch(lstrack(1,istn),1,'Mark3').eq.0
+          km4rack=ichcm_ch(lstrack(1,istn),1,'Mark4').eq.0
+C         Initialize roll to unknown
+          idum = ichmv_ch(lbarrel(1,istn,icode),1,'<un>')
 
 C         Get $FREQ statements.
           CALL vunpfrq(modedefnames(icode),stndefnames(istn),
@@ -128,9 +142,15 @@ C         Get $IF statements.
           endif
   
 C         Get $TRACKS statements (i.e. fanout).
-          call vunptrk(modedefnames(icode),stndefnames(istn),
-     .    ivexnum,iret,ierr,lu,
-     .    lm,cp,ctrchanref,csm,itrk,nfandefs,ihd,ifanfac)
+          if (ks2rec) then
+            call vunps2m(modedefnames(icode),stndefnames(istn),
+     .      ivexnum,iret,ierr,lu,ls2m,lm)
+            ifanfac=0
+          else
+            call vunptrk(modedefnames(icode),stndefnames(istn),
+     .      ivexnum,iret,ierr,lu,
+     .      lm,cp,ctrchanref,csm,itrk,nfandefs,ihd,ifanfac)
+          endif
           if (ierr.ne.0) then 
             write(lu,'("VMOINP05 - Error getting $TRACKS information",
      .      " for mode ",a," station ",a/" iret=",i5," ierr=",i5)') 
@@ -140,9 +160,14 @@ C         Get $TRACKS statements (i.e. fanout).
           endif
 
 C         Get $HEAD_POS and $PASS_ORDER statements.
-          call vunphp(modedefnames(icode),stndefnames(istn),
-     .    ivexnum,iret,ierr,lu,
-     .    indexp,pos1,pos2,nhdpos,cpassl,indexl,csubpassl,npl)
+          if (ks2rec) then
+            call vunps2g(modedefnames(icode),stndefnames(istn),
+     .      ivexnum,iret,ierr,lu,cpassl,npl)
+          else
+            call vunphp(modedefnames(icode),stndefnames(istn),
+     .      ivexnum,iret,ierr,lu,
+     .      indexp,pos1,pos2,nhdpos,cpassl,indexl,csubpassl,npl)
+          endif
           if (ierr.ne.0) then 
             write(lu,'("VMOINP06 - Error getting $HEAD_POS and",
      .      "$PASS_ORDER information",
@@ -156,24 +181,29 @@ C 3. Now decide what to do with this information. If we got to this
 C    point there were no reading or content errors for this station/mode
 C    combination. Some consistency checks are done here.
 C
-C    Count subpasses and store subpass names found in the fanout defs
-          do i=1,max_subpass
-            csubpass(i)=' '
-          enddo
-          nsubpass=0
-          do i=1,nfandefs ! go through them all
-            ix=1 
-            do while (ix.le.nsubpass.and.cp(i)(1:1).ne.
-     .                 csubpass(ix))
-              ix=ix+1
+C    Count subpasses and store subpass names found in the fanout defs.
+C    Not necessary for S2 recorders.
+          if (ks2rec) then
+          else
+            do i=1,max_subpass
+              csubpass(i)=' '
             enddo
-            if (ix.gt.nsubpass) then ! a new one
-              nsubpass=nsubpass+1
-              csubpass(nsubpass)=cp(i)(1:1)
-            endif
-          enddo
+            nsubpass=0
+            do i=1,nfandefs ! go through them all
+              ix=1 
+              do while (ix.le.nsubpass.and.cp(i)(1:1).ne.
+     .                   csubpass(ix))
+                ix=ix+1
+              enddo
+              if (ix.gt.nsubpass) then ! a new one
+                nsubpass=nsubpass+1
+                csubpass(nsubpass)=cp(i)(1:1)
+              endif
+            enddo
+          endif
 
 C    Save the chan_def info and its links.
+          if (km3rack.or.km4rack.or.kvrack) then
           nchan(istn,icode) = nchdefs
           do i=1,nchdefs ! each chan_def line
             invcx(i,istn,icode)=i ! save channel index number 
@@ -208,6 +238,7 @@ C    Save the chan_def info and its links.
      .        modedefnames(icode)(1:il),stndefnames(istn)(1:im)
             endif
 C          Track assignments
+            if (km3rec.or.km4rec.or.kvrec) then
             do ix=1,nfandefs ! check each fandef
               if (ctrchanref(ix).eq.cchanidref(i)) then ! matched link
                 ip=1 ! find subpass index
@@ -231,59 +262,71 @@ C          Track assignments
                 endif
               endif ! matched link
             enddo ! check each fandef
+            endif ! m3/4 or v rec
           enddo ! each chan_def line
+          endif ! m3/4 or v rack
 C
-C       3.2 Save the non-channel specific info.
-          samprate(icode)=srate ! sample rate
-C       Recording format, "Mark3", "Mark4", "VLBA"
+C    3.2 Save the non-channel specific info.
+C         Recording format, "Mark3", "Mark4", "VLBA"
           idum = ichmv(LMODE(1,istn,ICODE),1,lm,1,8) ! recording format
-C       Set bit density depending on the mode
-          if (ichcm_ch(lmode(1,istn,icode),1,'V').eq.0) then 
-            bitden=34020 ! VLBA non-data replacement
-          else 
-            bitden=33333 ! Mark3/4 data replacement
-          endif
-C       If "56000" was specified, for this station, use higher bit density
-          if (bitden_das.gt.55000.d0) then 
+          samprate(icode)=srate ! sample rate
+          if (ks2rec) then
+            idum = ichmv(ls2mode(1,istn,icode),1,ls2m,1,16)
+          else
+C           Set bit density depending on the mode
             if (ichcm_ch(lmode(1,istn,icode),1,'V').eq.0) then 
-              bitden=56700 ! VLBA non-data replacement
+              bitden=34020 ! VLBA non-data replacement
             else 
-              bitden=56250 ! Mark3/4 data replacement
+              bitden=33333 ! Mark3/4 data replacement
             endif
-          endif
-          if (bitden_das.ne.bitden) then 
-            write(lu,'("VMOINPxx - Bit density ",f6.0," for ",a," ",a,
-     .      " changed to ",f6.0)') bitden_das,modedefnames(icode)(1:il),
-     .      stndefnames(istn)(1:im),bitden
-          endif
-          bitdens(istn,icode)=bitden
+C           If "56000" was specified, for this station, use higher bit density
+            if (bitden_das.gt.55000.d0) then 
+              if (ichcm_ch(lmode(1,istn,icode),1,'V').eq.0) then 
+                bitden=56700 ! VLBA non-data replacement
+              else 
+                bitden=56250 ! Mark3/4 data replacement
+              endif
+            endif
+            if (bitden_das.ne.bitden) then 
+              write(lu,'("VMOINPxx - Bit density ",f6.0," for ",a," ",a,
+     .        " changed to ",f6.0)') bitden_das,
+     .        modedefnames(icode)(1:il),
+     .        stndefnames(istn)(1:im),bitden
+            endif
+            bitdens(istn,icode)=bitden
 C       Check number of passes and pass order indices
-          if (npl.ne.nhdpos*nsubpass) then
-            write(lu,'("VMOINPxx - Inconsistent pass order list")')
-          endif
-          do ip=1,npl
-            ix=1
-            do while (ix.le.nhdpos.and.indexl(ip).ne.indexp(ix))
-              ix=ix+1
-            enddo
-            if (ix.gt.nhdpos) then
-              write(lu,'("VMOINPxx - Index ",i3," in $PASS_ORDER not ",
-     .        "found in $HEAD_POS for ",a," ",a)') i,
-     .        modedefnames(icode)(1:il),stndefnames(istn)(1:im)
+            if (npl.ne.nhdpos*nsubpass) then
+              write(lu,'("VMOINPxx - Inconsistent pass order list")')
             endif
-          enddo  
-C       Store head positions and subpases
+            do ip=1,npl
+              ix=1
+              do while (ix.le.nhdpos.and.indexl(ip).ne.indexp(ix))
+                ix=ix+1
+              enddo
+              if (ix.gt.nhdpos) then
+                write(lu,'("VMOINPxx - Index ",i3," in $PASS_ORDER not ",
+     .          "found in $HEAD_POS for ",a," ",a)') i,
+     .          modedefnames(icode)(1:il),stndefnames(istn)(1:im)
+              endif
+            enddo  
+          endif ! m3/4 or v rec
+C    Store head positions and subpases
           do ip=1,npl
-            ix=1 ! find subpass number
-            do while (ix.le.nsubpass.and.csubpass(ix).ne.csubpassl(ip))
-              ix=ix+1
-            enddo
-            ihdpos(ip,istn,icode)=pos1(indexl(ip))
-            ihddir(ip,istn,icode)=ix
-            ihdpo2(ip,istn,icode)=pos2(indexl(ip))
-            ihddi2(ip,istn,icode)=ix
             cpassorderl(ip,istn) = cpassl(ip)
-          enddo 
+          enddo
+          if (km4rec.or.km3rec.or.kvrec) then
+            do ip=1,npl
+              ix=1 ! find subpass number
+              do while (ix.le.nsubpass.and.
+     .                  csubpass(ix).ne.csubpassl(ip))
+                ix=ix+1
+              enddo
+              ihdpos(ip,istn,icode)=pos1(indexl(ip))
+              ihddir(ip,istn,icode)=ix
+              ihdpo2(ip,istn,icode)=pos2(indexl(ip))
+              ihddi2(ip,istn,icode)=ix
+            enddo 
+          endif ! m3/4 or v rec
           npassl = npl
 
         enddo ! for one station at a time
