738398351ae631a497641f7acdb2fbe5b7a46018
Nancy Vandenberg <>
1996-08-19 00:00:00 +0000

    Change final check for previous pass to .LT. 0 because 0 is a valid pass number for S2.

--- drudg/knewt.f
+++ drudg/knewt.f
@@ -4,6 +4,9 @@ C
       include '../skdrincl/skparm.ftni'
       include 'drcom.ftni'
 C
+C 960819 nrv change final check for IPASP to .LT. 0 because 0 is
+C            a valid S2 pass number
+
 C Input:
       integer ift,ipas,ipasp,idir,idirp,iftold
 
@@ -11,7 +14,7 @@ C Input:
      .        (IPAS.EQ.IPASP.AND.IDIR.NE.IDIRP).OR.
      .        (IPAS.EQ.IPASP.AND.((IDIR.EQ.+1.AND.IFT.LT.(IFTOLD-10))
      .        .OR.(IDIR.EQ.-1.AND.IFT.GT.(IFTOLD+10))))
-      IF (IPASP.LE.0) KNEWT=.TRUE.
+      IF (IPASP.LT.0) KNEWT=.TRUE.
 C
       RETURN
       END
