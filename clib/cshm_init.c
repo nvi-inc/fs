23cfc89f3d1ce0151a061c40b53fd0a0ca33c7e0
Ed Himwich <ed.himwich@nviinc.com>
1996-08-22 00:00:00 +0000

    initialize S2 items

--- clib/cshm_init.c
+++ clib/cshm_init.c
@@ -26,6 +26,46 @@ void cshm_init()
   shm_addr->systracks.track[2]=34;
   shm_addr->systracks.track[3]=35;
 
+  shm_addr->user_info.labels[0][0]=0;
+  shm_addr->user_info.labels[1][0]=0;
+  shm_addr->user_info.labels[2][0]=0;
+  shm_addr->user_info.labels[3][0]=0;
+
+  shm_addr->user_info.field1[0]=0;
+  shm_addr->user_info.field2[0]=0;
+  shm_addr->user_info.field3[0]=0;
+  shm_addr->user_info.field4[0]=0;
+
+  shm_addr->s2st.dir=0;
+  shm_addr->s2st.speed=-1;
+  shm_addr->s2st.record=0;
+  shm_addr->rec_mode.mode[0]=0;
+  shm_addr->rec_mode.group=-1;
+
+  shm_addr->check.s2rec.user_info.label[0]=0;
+  shm_addr->check.s2rec.user_info.label[1]=0;
+  shm_addr->check.s2rec.user_info.label[2]=0;
+  shm_addr->check.s2rec.user_info.label[3]=0;
+
+  shm_addr->check.s2rec.user_info.field[0]=0;
+  shm_addr->check.s2rec.user_info.field[1]=0;
+  shm_addr->check.s2rec.user_info.field[2]=0;
+  shm_addr->check.s2rec.user_info.field[3]=0;
+
+  shm_addr->check.s2rec.check=0;
+  shm_addr->check.s2rec.speed=0;
+  shm_addr->check.s2rec.state=0;
+  shm_addr->check.s2rec.group=0;
+  shm_addr->check.s2rec.mode=0;
+  shm_addr->check.s2rec.roll=0;
+  shm_addr->check.s2rec.dv=0;
+  shm_addr->check.s2rec.tapeid=0;
+  shm_addr->check.s2rec.tapetype=0;
+
+  shm_addr->actual.s2rec[0].rstate_valid=FALSE;
+  shm_addr->actual.s2rec[0].position_valid=FALSE;
+  shm_addr->actual.s2rec_inuse=0;
+
   return;
 }
 
