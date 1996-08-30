287abc63dd9f8ddc5c777b23db6e962003e2e2b5
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
1996-08-30 00:00:00 +0000

    various changes
    1996-08-22
    created
    
    1996-08-30
    did strncat of who for robustnuss with FORTRAN hollerith

---
+++ poclb/logite.c
@@ -0,0 +1,56 @@
+/* logite
+
+   logite formats a special error message with predefined error string and
+   sends the buffer to ddout. It is like logit(a).c.
+*/
+
+#include <string.h>
+#include <stdio.h>
+#include "../include/fs_types.h"
+#include "../include/shm_addr.h"      /* shared memory pointer */
+#include "../include/params.h"
+#include "../include/fscom.h"
+
+void cls_snd();
+void rte_time();
+
+logite(msg,ierr,who)
+char *msg;           /* a message to be logged, NULL if none */
+int ierr;            /* error number, 0 if no error          */
+char *who;           /* 2-char string identifying the error  */
+
+{
+  char buf[512];    /* Holds the complete log entry */
+  char name[5];     /* The name of our main program */
+  int it[6],ip1,ip2,l;
+ 
+/* First get the time and put dddhhmmss into the log entry.
+*/
+  rte_time(it,&it[5]);
+  buf[0]='\0';
+  int2str(buf,it[5]%100,-2,1);
+  int2str(buf,it[4],-3,1);
+  int2str(buf,it[3],-2,1);
+  int2str(buf,it[2],-2,1);
+  int2str(buf,it[1],-2,1);
+  int2str(buf,it[0],-2,1);
+
+/* For error messages, put ?ERROR xx msg into the log entry.
+*/
+  if(ierr!=0) {
+    strcat(buf,"?ERROR ");
+    strncat(buf,who,2);
+    int2str(buf,ierr,-5,0);
+    strcat(buf," ");
+  }
+  strcat(buf,msg);
+
+/* Send the complete log entry to ddout via class.
+*/
+  memcpy(&ip1,"fs",2);
+  memcpy(&ip2,"  ",2);
+  if (ierr != 0) memcpy(&ip2,"b1",2);
+/* for testing, send to output PLUS class */
+/*  fprintf(stdout,"%s\n",buf); */
+  cls_snd(&shm_addr->iclbox,buf,strlen(buf),ip1,ip2);
+}
