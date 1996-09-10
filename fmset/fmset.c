08905ae3a2b8a8b9b8a9bf8f8ff05c8d74f0a357
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
1996-09-10 00:00:00 +0000

    various changes
    1996-08-18
    add S2 menu option and selection
    
    1996-08-26
    make S2 primary if it is present
    
    1996-09-10
    add suggestion to user to not run fmset for long

--- fmset/fmset.c
+++ fmset/fmset.c
@@ -15,6 +15,8 @@
 #define DEC_KEY		'-'
 #define SET_KEY		'='
 #define EQ_KEY          '.'
+#define TOGGLE_KEY      't'
+#define TOGGLE2_KEY     'T'
 
 /* externals */
 void initvstr();
@@ -25,6 +27,8 @@ time_t	asktime(); /* ask operator to enter a time */
 
 /* global variables */
 int rack;
+int drive;
+int source;
 unsigned char inbuf[512];      /* class i-o buffer */
 unsigned char outbuf[512];     /* class i-o buffer */
 long ip[5];           /* parameters for fs communications */
@@ -47,8 +51,10 @@ int	running=TRUE;
 time_t disptime;
 int    disphs;
 char   inc;
-int    flag;
+ int    flag;
 struct tm *disptm;
+int toggle= FALSE;
+int other,temp;
 
 
 setup_ids();         /* connect to shared memory segment */
@@ -62,15 +68,28 @@ if (nsem_test(NSEM_NAME) != 1) {
 rte_prior(CL_PRIOR); /* set our priority */
 
 rack=shm_addr->equip.rack;
+drive=shm_addr->equip.drive;
 
-if (rack & MK3) {
-  printf("fmset does not support Mark 3 racks - fmset aborting\n");
+if (drive==S2) {
+  source=S2;
+  if(rack & MK3 || rack == 0)
+    ;
+  else {
+    toggle=TRUE;
+    other=rack;
+  }
+} else if (rack & MK3 || rack == 0) {
+  if(rack & MK3)
+    printf("fmset does not support Mark 3 racks - fmset aborting\n");
+  else if(rack == 0)
+    printf("fmset requires a VLBA/Mark IV rack or S2 recorder to set - fmset aborting\n");
   rte_sleep(SLEEP_TIME);
   exit(0);
-}
+} else
+  source=rack;
 
 if(rack & VLBA)
-	initvstr();
+  initvstr();
 
 /* initialize terminal settings and curses.h data structures and variables */
 initscr ();
@@ -82,10 +101,14 @@ clear_scrn ( maindisp, 24 );
 box ( maindisp, 0, 0 );  /* use default vertical/horizontal lines */
 
 /* build display screen */
-mvwaddstr( maindisp, 4, 10, "Formatter" );
+build:
+if(source == S2)
+  mvwaddstr( maindisp, 4, 10, "S2 Recorder" );
+else
+  mvwaddstr( maindisp, 4, 10, "Formatter" );
 mvwaddstr( maindisp, 5, 10, "Field System" );
 mvwaddstr( maindisp, 6, 10, "Computer" );
-mvwaddstr( maindisp, 2, 23, "fmset - formatter time set" );
+mvwaddstr( maindisp, 2, 23, "fmset - formatter/S2 recorder time set" );
 mvwaddstr( maindisp, ROW, 10,
  "Use '+'   to increment formatter time by one second." );
 mvwaddstr( maindisp, ROW+1, 10,
@@ -94,8 +117,15 @@ mvwaddstr( maindisp,  ROW+2, 10,
  "    '='   to be prompted for a new formatter time." );
 mvwaddstr( maindisp, ROW+3, 10, 
  "    '.'   to set formatter time to Field System time.");
-mvwaddstr( maindisp, ROW+4, 10,
- "    <esc> to quit.");
+if(!toggle)
+  mvwaddstr( maindisp, ROW+4, 10,
+	     "    <esc> to quit: DON'T LEAVE FMSET RUNNING FOR LONG.");
+else {
+  mvwaddstr( maindisp, ROW+4, 10,
+	     "    't' or 'T' to toggle between S2 recorder and formatter.");
+  mvwaddstr( maindisp, ROW+5, 10,
+	     "    <esc> to quit.");
+}  
 
 leaveok ( maindisp, FALSE); /* leave cursor in place */
 wrefresh ( maindisp );
@@ -148,24 +178,32 @@ do 	{
 
 	while (ERR!=(inc=wgetch( maindisp ))) 
 	switch ( inc ) {
-		case INC_KEY :  /* Increment seconds */
-			setfmtime(formtime++,1);
-			break;
-		case DEC_KEY :  /* Decrement seconds */
-			setfmtime(formtime--,-1);
-			break;
-		case SET_KEY :  /* Get time from user */
-			formtime = asktime( maindisp,&flag, formtime);
-                        if(flag)
-                          setfmtime(formtime,0);
-			break;
-		case EQ_KEY :  /* set form time from comp time */
-			setfmtime(formtime=fstime+(fshs+50)/100,0);
-			break;
-		case ESC_KEY :  /* ESC character */
-			running = FALSE;
-			break;
-		default     :
+	case INC_KEY :  /* Increment seconds */
+	  setfmtime(formtime++,1);
+	  break;
+	case DEC_KEY :  /* Decrement seconds */
+	  setfmtime(formtime--,-1);
+	  break;
+	case SET_KEY :  /* Get time from user */
+	  formtime = asktime( maindisp,&flag, formtime);
+	  if(flag)
+	    setfmtime(formtime,0);
+	  break;
+	case EQ_KEY :  /* set form time from comp time */
+	  setfmtime(formtime=fstime+(fshs+50)/100,0);
+	  break;
+	case ESC_KEY :  /* ESC character */
+	  running = FALSE;
+	  break;
+	case TOGGLE_KEY:
+	case TOGGLE2_KEY:
+	  if(toggle) {
+	    temp=source;
+	    source=other;
+	    other=temp;
+	    goto build;
+	  }
+	default     :
 			running = TRUE;
 	}
 
