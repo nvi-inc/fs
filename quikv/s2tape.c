f0c973a5d66f3d86fb6cab19b4b7ab6bdee8053f
Ed Himwich <ed.himwich@nviinc.com>
1996-09-11 00:00:00 +0000

    various changes
    1996-08-04
    created
    
    1996-09-11
    fixed missing index for "reset"

---
+++ quikv/s2tape.c
@@ -0,0 +1,92 @@
+/* S2 recorder user_info snap command */
+
+#include <stdio.h> 
+#include <string.h> 
+#include <sys/types.h>
+
+#include "../include/params.h"
+#include "../include/fs_types.h"
+#include "../include/fscom.h"         /* shared memory definition */
+#include "../include/shm_addr.h"      /* shared memory pointer */
+
+static char device[]={"rc"};           /* device menemonics */
+
+void s2tape(command,itask,ip)
+struct cmd_ds *command;                /* parsed command structure */
+int itask;
+long ip[5];                           /* ipc parameters */
+{
+      int ilast, ierr, i, count;
+      int verr;
+      char *ptr;
+      struct rclcn_req_buf buffer;        /* rclcn request buffer */
+      long int position[8];
+
+      int s2tape_dec();                 /* parsing utilities */
+      char *arg_next();
+
+      void s2tape_dis();
+      void ini_rclcn_req(), end_rclcn_req();
+      void add_rclcn_position_read();
+      void add_rclcn_position_set();
+
+      void skd_run(), skd_par();      /* program scheduling utilities */
+
+      ini_rclcn_req(&buffer);
+
+      if (command->equal != '=') {            /* read module */
+	add_rclcn_position_read(&buffer,device,1);
+	goto rclcn;
+      } else if (command->argv[0]==NULL)   /* simple equals */
+	goto parse;
+      else if (command->argv[1]==NULL)     /* special cases */
+        if (strcmp(command->argv[0],"reset")==0) {
+	  add_rclcn_position_set(&buffer,device,2, (long int) 0);
+	  goto rclcn;
+	}
+
+      
+/* if we get this far it is a set-up command so parse it */
+
+parse:
+      ilast=0;                                      /* last argv examined */
+      count=1;
+      while( count>= 0) {
+        ptr=arg_next(command,&ilast);
+        ierr=s2tape_dec(position,&count, ptr);
+        if(ierr !=0 )
+	  goto error;
+      }
+
+/* format buffers for mcbcn */
+      
+      if (ilast==1)
+	add_rclcn_position_set(&buffer,device,2,position[0]);
+      else if(ilast==8)
+	add_rclcn_position_set_ind(&buffer,device,2,position);
+      else {
+	ierr=-301;
+	goto error;
+      }
+
+rclcn:
+      end_rclcn_req(ip,&buffer);
+      skd_run("rclcn",'w',ip);
+      skd_par(ip);
+
+      if(ip[2]<0) {
+	cls_clr(ip[0]);
+	ip[0]=ip[1]=0;
+	return;
+      }
+
+      s2tape_dis(command,ip);
+      return;
+
+error:
+      ip[0]=0;
+      ip[1]=0;
+      ip[2]=ierr;
+      memcpy(ip+3,"rt",2);
+      return;
+}
