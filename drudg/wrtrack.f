ff991dac55f7b2c95c4327a5a72909917ef65842
Nancy Vandenberg <>
1996-11-01 00:00:00 +0000

    Don't write an extra NEXT after 8 channels if there are more to come.

--- drudg/wrtrack.f
+++ drudg/wrtrack.f
@@ -10,6 +10,8 @@ C   gag   910513  Added parameter to nchanv for multiple vlba stations.
 C   nrv   930412  implicit none
 C   nrv   930709  Fixed to write correct mode A tracks
 C   nrv   950406  fixed to write correct mode E tracks
+C 961101 nrv Don't write an extra NEXT after 8 channels if there are
+C            both u/l for a total of 16 channels
 C
       include '../skdrincl/skparm.ftni'
 C
@@ -25,7 +27,7 @@ C   COMMON BLOCKS USED
       include '../skdrincl/freqs.ftni'
 C
 C  LOCAL VARIABLES
-      integer iy,ul,ichan,iassign,ierr,iprint,nch,ileft
+      integer iul,iy,ul,ichan,iassign,ierr,iprint,nch,ileft
       integer ichmv_ch,ib2as ! functions
 C
 C  INITIALIZED
@@ -44,16 +46,20 @@ C       frequency
 	call ifill(ibuf,1,iblen,32)
 	call char2hol('track=',ibuf,1,6)
 	nch = 7
+        iul=1
 	do ichan=1,nchan(istn,icod)  !channels
 	  do ul=1,2  !Upper and lower
 	    iassign = itras(ul,1,invcx(ichan,istn,icod),idx,istn,icod)
 C  Note: for mode A there is no track assignment for pass 2, so
 C  no tracks are written. But both u/l are assigned.
 C  Note: for 2-bit sampling, the track for the magnitude is automatically
-C  recorded as the next one in the stack.
+C  recorded as the next one in the stack. This track does not appear
+C  explicitly in ITRAS.
 C  Note: for fan-out, the next track(s) are automatically recorded on
-C  the next one on the stack.
+C  the next one on the stack. This track does not appear
+C  explicitly in ITRAS.
 	    if (iassign.gt.-99) then  !if number
+                if (ul.eq.2) iul=2 ! we have both u/l
 		iassign = iassign + 3 ! VLBA track numbers
                 nch = ichmv_ch(ibuf,nch,'(')
 		iprint = iprint + 1
@@ -65,7 +71,7 @@ C  the next one on the stack.
 	    end if !if number
 	  end do !Upper and lower
 	  if (iprint.eq.8) then
-	    if (iprint.eq.nchan(istn,icod)) then
+	    if (iprint.eq.iul*nchan(istn,icod)) then
 C               nch = ichmv(ibuf,nch-1,2h  ,1,2)
                 nch = ichmv_ch(ibuf,nch-1,' !NEXT!')
 	    else
