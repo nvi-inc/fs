ecaade508fa4cdce7034abe520365c09fb8cd9ec
Nancy Vandenberg <>
1996-11-07 00:00:00 +0000

    If a station is not in the observation, don't generate an error.

--- drudg/ckobs.f
+++ drudg/ckobs.f
@@ -28,6 +28,7 @@ C  930407 nrv implicit none
 C  940609 nrv Satellite names have been moved to immediately
 C             follow the celestial sources.
 C 961101 nrv Codes undefined for this station are invalid too.
+C 961107 nrv Don't check for undefined if this station isn't in this scan.
 C
       ISOR = 0
       I = 1
@@ -57,10 +58,10 @@ C
       IF (ICOD.EQ.0) THEN
         WRITE(LUSCN,9230) LCOD
 9230    FORMAT(' CKOBS02 - FREQUENCY CODE ',A2,
-     .  ' NOT IN YOUR SCHEDULE. QUITTING')
+     .  ' NOT FOUND IN YOUR SCHEDULE. QUITTING')
         RETURN
       ENDIF
-      if (nchan(istn,icod).eq.0) then
+      if (istnsk.ne.0.and.nchan(istn,icod).eq.0) then
         icod=0
         WRITE(LUSCN,9240) LCOD
 9240    FORMAT(' CKOBS03 - FREQUENCY CODE ',A2,
