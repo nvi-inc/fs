a00cfe54e84131b4093d186538fa1c4db392f115
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
1996-12-19 00:00:00 +0000

    add barrelroll rcl lib calls for real control and montior of roll

--- chekr/s2drive.f
+++ chekr/s2drive.f
@@ -11,11 +11,11 @@ C  SUBROUTINES CALLED:
 C 
 C
 C  LOCAL VARIABLES: 
-      integer icherr(17)
+      integer icherr(18)
 C
 C
 C  INITIALIZED:
-      do j=1,17
+      do j=1,18
         icherr(j)=0
       enddo
       call fs_get_ichs2(icheck(18))
@@ -24,7 +24,7 @@ C  INITIALIZED:
 
       call fs_get_ichs2(icheck(18))
       if(icheck(18).le.0.or.ichecks(18).ne.icheck(18)) return
-      do j=1,17
+      do j=1,18
         if (icherr(j).ne.0) then
           call logit7ic(0,0,0,0,-500-j,lwho,'rc')
         endif
