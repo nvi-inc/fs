2576d49cfabcf2af43600123ef2e0284534e6667
Ed Himwich <ed.himwich@nviinc.com>
1996-12-27 00:00:00 +0000

    remove extra size declaration for ibuf_cm

--- newlb/pname.f
+++ newlb/pname.f
@@ -8,7 +8,7 @@
        subroutine putpname(cbuf)
        character*(*) cbuf
        integer*2 ibuf_cm(3)
-       common/pname_com/ibuf_cm(3)
+       common/pname_com/ibuf_cm
        call char2hol(cbuf,ibuf_cm,1,5)
        return
        end
