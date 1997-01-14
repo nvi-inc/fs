1fd44a03bb5364a2f74e475002c177a6be7c8509
Nancy Vandenberg <>
1997-01-14 00:00:00 +0000

    For Vex 1.5 get IF name from def directly instead of ref name,

--- skdrut/vunpif.f
+++ skdrut/vunpif.f
@@ -1,5 +1,5 @@
       SUBROUTINE vunpif(modef,stdef,ivexnum,iret,ierr,lu,
-     .cifref,flo,ls,lin,nifdefs)
+     .cifref,flo,ls,lin,lp,nifdefs)
 C
 C     VUNPIF gets the IFD def statements 
 C     for station STDEF and mode MODEF and converts it.
@@ -13,6 +13,8 @@ C
 C
 C  History:
 C 960522 nrv New.
+C 970114 nrv For Vex 1.5 get IF name from def directly instead of ref name, 
+C            and add polarization to call.
 C
 C  INPUT:
       character*128 stdef ! station def to get
@@ -29,6 +31,7 @@ C                    <0 indicates invalid value for a field
       double precision flo(max_ifd) ! LO frequencies
       integer*2 ls(max_ifd) ! sideband of the LO
       integer*2 lin(max_ifd) ! IF input channel
+      integer*2 lp(max_ifd) ! polarization
       integer nifdefs ! number of IFDs found
 C
 C  LOCAL:
@@ -37,7 +40,7 @@ C  LOCAL:
       integer idum,id,nch
       character upper
       integer trimlen,ichmv_ch ! function
-      integer fvex_len,fvex_field,fget_mode_lowl,ptr_ch,
+      integer fvex_len,fvex_field,ptr_ch,
      .fvex_double,fvex_units,fget_all_lowl
 C
 C
@@ -49,10 +52,10 @@ C ** this should work with fget_mode_lowl but doesn't seem to
      .ptr_ch('if_def'//char(0)),
      .ptr_ch('IF'//char(0)),ivexnum)
       id=0
-      do while (id.lt.max_ifd.and.iret.eq.0) ! get all BBC defs
+      do while (id.lt.max_ifd.and.iret.eq.0) ! get all IF defs
         id=id+1
 
-C  1.1 IFD ref
+C  1.1 IF def
 
         ierr = 11
         iret = fvex_field(1,ptr_ch(cout),len(cout)) ! get IFD ref
@@ -65,71 +68,87 @@ C  1.1 IFD ref
           cifref(id)=cout(1:nch)
         endif
 
-C  1.2 LO frequency
-
+C  1.2 IF input
+  
         ierr = 12
+        idum = ichmv_ch(lin(id),1,'  ')
+        iret = fvex_field(2,ptr_ch(cout),len(cout)) ! get input
+        if (iret.ne.0) return
+        nch = fvex_len(cout)
+        if (nch.ne.1.and.nch.ne.2) then
+          ierr=-2
+          write(lu,'("VUNPIFD04 - IF input must be 1 or 2 ",
+     .    "characters")')
+        else
+          if (cout(1:nch).eq.'1N'.or.cout(1:nch).eq.'1A'.or.
+     .        cout(1:nch).eq.'2N'.or.cout(1:nch).eq.'2A'.or.
+     .        cout(1:nch).eq.'3O'.or.cout(1:nch).eq.'3I'.or.
+     .        cout(1:nch).eq.'A'.or.cout(1:nch).eq.'B'.or.
+     .        cout(1:nch).eq.'C'.or.cout(1:nch).eq.'D') then
+            idum = ichmv_ch(lin(id),1,cout(1:nch))
+          else
+            ierr=-2
+            write(lu,'("VUNPIFD05 - Invalid IF input")')
+          endif
+        endif
+C       else ! not there, use last 2 char of ref <<<<<<<<<<< kludge
+C         nch = trimlen(cifref(id))
+C         if (cifref(id)(nch-1:nch).eq.'1A'.or.
+C    .        cifref(id)(nch-1:nch).eq.'2A'.or.
+C    .        cifref(id)(nch-1:nch).eq.'1N'.or.
+C    .        cifref(id)(nch-1:nch).eq.'2N') then ! 2 char
+C           idum = ichmv_ch(lin(id),1,cifref(id)(nch-1:nch))
+C         else ! 1 char
+C           idum = ichmv_ch(lin(id),1,cifref(id)(nch:nch))
+C         endif
+C       endif
+
+C  1.3 Polarization 
+
+        ierr = 13
+        idum = ichmv_ch(lp(id),1,'  ')
+        iret = fvex_field(3,ptr_ch(cout),len(cout)) ! get IFD ref
+        if (iret.ne.0) return
+        nch = fvex_len(cout)
+        cout(1:1) = upper(cout(1:1))
+        if (nch.ne.1.or.(cout(1:1).ne.'R'.and.cout(1:1).ne.'L')) then
+          ierr=-3
+          write(lu,'("VUNPIFD06 - Polarization must be R or L")')
+        else
+          idum = ichmv_ch(lp(id),1,cout(1:1))
+        endif
+
+C  1.4 LO frequency
+
+        ierr = 14
         flo(id)=0.d0
-        iret = fvex_field(2,ptr_ch(cout),len(cout)) ! get number
+        iret = fvex_field(4,ptr_ch(cout),len(cout)) ! get number
         if (iret.ne.0) return
         iret = fvex_units(ptr_ch(cunit),len(cunit))
         if (iret.ne.0) return
         iret = fvex_double(ptr_ch(cout),ptr_ch(cunit),d)
         if (iret.ne.0.or.d.lt.0.d0) then
-          ierr=-2
+          ierr=-4
           write(lu,'("VUNPIFD02 - Invalid LO frequency")')
         else
           flo(id) = d/1.d6
         endif
 
-C  1.3 Sideband
+C  1.5 Sideband
 
-        ierr = 13
+        ierr = 15
         idum = ichmv_ch(ls(id),1,'  ')
-        iret = fvex_field(3,ptr_ch(cout),len(cout)) ! get IFD ref
+        iret = fvex_field(5,ptr_ch(cout),len(cout)) ! get IFD ref
         if (iret.ne.0) return
         nch = fvex_len(cout)
         cout(1:1) = upper(cout(1:1))
         if (nch.ne.1.or.(cout(1:1).ne.'U'.and.cout(1:1).ne.'L')) then
-          ierr=-3
+          ierr=-5
           write(lu,'("VUNPIFD03 - Sideband must be U or L")')
         else
           idum = ichmv_ch(ls(id),1,cout(1:1))
         endif
 
-C  1.4 IF input
-  
-        ierr = 14
-        idum = ichmv_ch(lin(id),1,'  ')
-        iret = fvex_field(4,ptr_ch(cout),len(cout)) ! get input
-        if (iret.eq.0) then
-          nch = fvex_len(cout)
-          if (nch.ne.1.or.nch.ne.2) then
-            ierr=-4
-            write(lu,'("VUNPIFD04 - IF input must be 1 or 2 ",
-     .      "characters")')
-          else
-            if (cout(1:nch).eq.'1N'.or.cout(1:nch).eq.'1A'.or.
-     .          cout(1:nch).eq.'2N'.or.cout(1:nch).eq.'2A'.or.
-     .          cout(1:nch).eq.'3N'.or.cout(1:nch).eq.'3A'.or.
-     .          cout(1:nch).eq.'A'.or.cout(1:nch).eq.'B'.or.
-     .          cout(1:nch).eq.'C'.or.cout(1:nch).eq.'D') then
-              idum = ichmv_ch(lin(id),1,cout(1:nch))
-            else
-              ierr=-5
-              write(lu,'("VUNPIFD05 - Invalid IF input")')
-            endif
-          endif
-        else ! not there, use last 2 char of ref <<<<<<<<<<< kludge
-          nch = trimlen(cifref(id))
-          if (cifref(id)(nch-1:nch).eq.'1A'.or.
-     .        cifref(id)(nch-1:nch).eq.'2A'.or.
-     .        cifref(id)(nch-1:nch).eq.'1N'.or.
-     .        cifref(id)(nch-1:nch).eq.'2N') then ! 2 char
-            idum = ichmv_ch(lin(id),1,cifref(id)(nch-1:nch))
-          else ! 1 char
-            idum = ichmv_ch(lin(id),1,cifref(id)(nch:nch))
-          endif
-        endif
 
 C       Get next IFD def statement
         iret = fget_all_lowl(ptr_ch(stdef),
