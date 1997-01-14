d02a00fe46a43adfaa95b2811b1f50e58efd0823
Nancy Vandenberg <>
1997-01-14 00:00:00 +0000

    Chane 8 to max_sorlen

--- skdrut/findscan.f
+++ skdrut/findscan.f
@@ -10,6 +10,7 @@ C   on the source, code, and start time.
 
 C History
 C 960527 nrv New.
+C 970114 nrv Chane 8 to max_sorlen
 
 C Input
       integer isor,icod ! indices for source and freq code
@@ -21,7 +22,7 @@ C Output
 C Local
       integer nch,is,iob,idum,ich,ic,ic1,ic2,iyr,ida,ihr,imin,isc
       integer ir,numc3,numc2
-      integer*2 ibuf(ibuf_len),lsn(4),lfrq,lstart(6)
+      integer*2 ibuf(ibuf_len),lsn(max_sorlen/2),lfrq,lstart(6)
       integer ib2as,ichcm,ias2b,ichmv,igtso,igtfr
 
 C 1. Find source name and frequency code.
@@ -29,7 +30,7 @@ C    Convert start time to ascii.
 
       numc2 = 2+o'40000'+o'400'*2
       numc3 = 3+o'40000'+o'400'*3
-      idum = ichmv(lsn,1,lsorna(1,isor),1,8)
+      idum = ichmv(lsn,1,lsorna(1,isor),1,max_sorlen)
       lfrq = lcode(icod)
       nch=1
       NCH = NCH + IB2AS(istart(1)-1900,lstart,nch,2)
@@ -54,10 +55,10 @@ C       idum = ichmv(ibuf,1,lskobs(1,iskrec(iob)),1,ibuf_len*2)
 C  Source name
 C       CALL GTFLD(lskobs(1,ir),ICH,IBUF_LEN*2,IC1,IC2)
 C       Assume source starts in char 1
-C       if (ichcm(lsn,1,lskobs(1,ir),ic1,8).eq.0) then ! continue
-        if (ichcm(lsn,1,lskobs(1,ir),1,8).eq.0) then ! continue
-C       CALL IFILL(LSN,1,8,oblank)
-C       IDUM = ICHMV(LSN,1,IBUF,IC1,MIN0(IC2-IC1+1,8))
+C       if (ichcm(lsn,1,lskobs(1,ir),ic1,max_sorlen).eq.0) then ! continue
+        if (ichcm(lsn,1,lskobs(1,ir),1,max_sorlen).eq.0) then ! continue
+C       CALL IFILL(LSN,1,max_sorlen,oblank)
+C       IDUM = ICHMV(LSN,1,IBUF,IC1,MIN0(IC2-IC1+1,max_sorlen))
 C       IF  (IGTSO(LSN,IS).eq.isor) then ! continue
 C  Skip cal time
           CALL GTFLD(lskobs(1,ir),ICH,IBUF_LEN*2,IC1,IC2)
