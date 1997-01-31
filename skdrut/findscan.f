c06ef5bf86caa954700ce7940f80151266cb9712
Nancy Vandenberg <>
1997-01-31 00:00:00 +0000

    Don't check using exact columns, but get fields.

--- skdrut/findscan.f
+++ skdrut/findscan.f
@@ -11,6 +11,7 @@ C   on the source, code, and start time.
 C History
 C 960527 nrv New.
 C 970114 nrv Chane 8 to max_sorlen
+C 970131 nrv Don't check using exact columns, but get fields.
 
 C Input
       integer isor,icod ! indices for source and freq code
@@ -26,7 +27,7 @@ C Local
       integer ib2as,ichcm,ias2b,ichmv,igtso,igtfr
 
 C 1. Find source name and frequency code.
-C    Convert start time to ascii.
+C    Convert start time to Hollerith .
 
       numc2 = 2+o'40000'+o'400'*2
       numc3 = 3+o'40000'+o'400'*3
@@ -39,12 +40,11 @@ C    Convert start time to ascii.
       NCH = NCH + IB2AS(istart(4),lstart,NCH,numc2)
       NCH = NCH + IB2AS(istart(5),lstart,NCH,numc2)
 
-C 1. Loop through all observations so far. 
+C 1. Loop through all observations so far.  Check source, code, and time.
 C     source   cal code preob start 
 C     Example:
 C     3C84      120 SX PREOB 80092120000  
 C                            yydddhhmmss
-
       irec = 0
       iob = 1
       do while (iob.le.nobs)
@@ -53,37 +53,18 @@ C       idum = ichmv(ibuf,1,lskobs(1,iskrec(iob)),1,ibuf_len*2)
         ir=iskrec(iob)
         ICH = 1
 C  Source name
-C       CALL GTFLD(lskobs(1,ir),ICH,IBUF_LEN*2,IC1,IC2)
-C       Assume source starts in char 1
-C       if (ichcm(lsn,1,lskobs(1,ir),ic1,max_sorlen).eq.0) then ! continue
-        if (ichcm(lsn,1,lskobs(1,ir),1,max_sorlen).eq.0) then ! continue
-C       CALL IFILL(LSN,1,max_sorlen,oblank)
-C       IDUM = ICHMV(LSN,1,IBUF,IC1,MIN0(IC2-IC1+1,max_sorlen))
-C       IF  (IGTSO(LSN,IS).eq.isor) then ! continue
+        CALL GTFLD(lskobs(1,ir),ICH,IBUF_LEN*2,IC1,IC2)
+        if (ichcm(lsn,1,lskobs(1,ir),ic1,max_sorlen).eq.0) then ! continue
 C  Skip cal time
           CALL GTFLD(lskobs(1,ir),ICH,IBUF_LEN*2,IC1,IC2)
 C  Freq code
-C         CALL GTFLD(lskobs(1,ir),ICH,IBUF_LEN*2,IC1,IC2)
-C         if (ichcm(lfrq,1,lskobs(1,ir),ic1,2).eq.0) then ! continue
-C         Assume freq code is in col 14-15
-          if (ichcm(lfrq,1,lskobs(1,ir),14,2).eq.0) then ! continue
-C         IDUM = ICHMV(LFRQ,1,IBUF,IC1,2)
-C         IF  (IGTFR(LFRQ,IC).EQ.icod) then ! continue
+          CALL GTFLD(lskobs(1,ir),ICH,IBUF_LEN*2,IC1,IC2)
+          if (ichcm(lfrq,1,lskobs(1,ir),ic1,2).eq.0) then ! continue
 C  Skip preob
             CALL GTFLD(lskobs(1,ir),ICH,IBUF_LEN*2,IC1,IC2)
 C  Start time
-C           CALL GTFLD(lskobs(1,ir),ICH,IBUF_LEN*2,IC1,IC2)
-C           if (ichcm(lstart,1,lskobs(1,ir),ic1,11).eq.0) then ! got it
-C           Assume time starts in col 24
-            if (ichcm(lstart,1,lskobs(1,ir),24,11).eq.0) then ! got it
-C           IYR = 1900+IAS2B(IBUF,IC1,2)
-C           IDA = IAS2B(IBUF,IC1+2,3)
-C           IHR = IAS2B(IBUF,IC1+5,2)
-C           IMIN = IAS2B(IBUF,IC1+7,2)
-C           ISC = IAS2B(IBUF,IC1+9,2)
-C           if (iyr.eq.istart(1).and.ida.eq.istart(2).and.
-C    .      ihr.eq.istart(3).and.imin.eq.istart(4).and.
-C    .      isc.eq.istart(5)) then ! got it
+            CALL GTFLD(lskobs(1,ir),ICH,IBUF_LEN*2,IC1,IC2)
+            if (ichcm(lstart,1,lskobs(1,ir),ic1,11).eq.0) then ! got it
               irec = iob
 C             write(6,'("matched ",i5)') irec
               return
