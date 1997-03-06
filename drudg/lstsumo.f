3b157a7cbf3cc28cdead4cfbb0b4118fdd481aba
Nancy Vandenberg <>
1997-03-06 00:00:00 +0000

    various changes
    1997-01-31
    Put updating of IFEET back in here, after call to LSTSUMO.
    Remove some checks for KS2 and replace with others.
    Add KET to lstsumo call. Change tape start into a string.
    
    1997-03-06
    Change S2 to print minutes, not seconds. Add ".and..not.ks2"

--- drudg/lstsumo.f
+++ drudg/lstsumo.f
@@ -1,6 +1,6 @@
       subroutine lstsumo(iline,npage,cstn,cid,cexper,maxline,idir,
-     .      speed_snap,itearl_local,kwrap,ks2,cday,kazel,ksat,kstart,
-     .      nsline,csor,cwrap,ch1,cm1,cs1,ih1,im1,is1,
+     .      speed_snap,itearl_local,kwrap,ks2,cday,kazel,ksat,ket,
+     .      kstart,nsline,csor,cwrap,ch1,cm1,cs1,ih1,im1,is1,
      .      ihd,imd,isd,ih2,im2,is2,idm,ids,cpass,ifeet,cnewtap,cdir,
      .      kskd,ncount,ntapes,
      .      rarad,dcrad,xpos,ypos,zpos,mjd,ut)
@@ -12,6 +12,10 @@
 
 C Writes an output line for LSTSUM.
 C 960917 nrv New. Removed lines from LSTSUM to make this routine.
+C 970131 nrv Remove updating of IFEET and put it back into LSTSUM.
+C 970131 nrv Add KET to call. Change tape start into a string.
+C 970306 nrv Change S2 to print minutes, not seconds. Add ".and..not.ks2" 
+C            to the first two tests for output.
 
 C Input
       double precision rarad,dcrad,xpos,ypos,zpos,ut
@@ -22,9 +26,9 @@ C Input
       character*8 csor,cexper,cstn
       character*3 cdir,cnewtap,cday
       character*2 cpass,ch1,cm1,cs1
-      character*1 cid
+      character*2 cid
       character*7 cwrap
-      logical kskd,kstart,ks2
+      logical kskd,kstart,ket,ks2
       logical kwrap, kazel,ksat
 
 C Output
@@ -33,20 +37,22 @@ C These are modified on return: iline, page,ncount,ntapes
 C Local
       integer i,idur,iaz,iel,trimlen
       double precision az,el
+      integer*2 lt
+      integer zz2,idum,ib2as
 
-
+      zz2 = 2+o'40000'+o'400'*2
       if (iline.ge.maxline) then ! new page, write header
         if (npage.gt.0) call luff(luprt)
         npage = npage + 1
         write(luprt,9300) cinname(1:trimlen(cinname)),npage
 9300    format(' Schedule file: ',a,35x,'Page ',i3)
         if (kskd) then
-          write(luprt,9302) (lstnna(i,istn),i=1,4),lstcod(istn),
+          write(luprt,9302) (lstnna(i,istn),i=1,4),lpocod(istn),
      .    (lexper(i),i=1,4)
-9302      format(' Station: ',4a2,' (',a1,')'/' Experiment: ',4a2)
+9302      format(' Station: ',4a2,' (',a2,')'/' Experiment: ',4a2)
         else
           write(luprt,9303) cstn,cid,cexper
-9303      format(' Station: ',a8,' (',a1,')'/' Experiment: ',a8)
+9303      format(' Station: ',a8,' (',a2,')'/' Experiment: ',a8)
         endif
         write(luprt,9304) itearl_local
 9304    format(' Early tape start: ',i3,' seconds'/)
@@ -65,12 +71,12 @@ C
 9312    format(22x,'        Start    Start    Stop',15x,
      .  'Tape'/
      .  ' Line#  Source   Az El Cable   Tape     Data     ',
-     .  'Data    Dur Group (sec) Change')
+     .  'Data    Dur Group (min) Change')
         if (.not.kwrap.and.ks2) write(luprt,9391)
 9391    format(22x,'     Start     Start     Stop',16x,
      .  'Tape'/
      .  ' Line#  Source    Az El    Tape      Data      ',
-     .  'Data     Dur Group (sec) Change')
+     .  'Data     Dur Group (min) Change')
         write(luprt,9311)
 9311    format('-----------------------------------------',
      .  '-------------------------------------')
@@ -90,33 +96,40 @@ C   Now write the scan line
         iaz = 0
         iel = 0
       endif
-      if (cnewtap.eq.'XXX') kstart=.true.
-      if (.not.kstart) then ! blank out start time
+C     if (cnewtap.eq.'XXX') ket=.true.
+      if (kstart) then ! convert start time to characters
+        idum=ib2as(ih1,lt,1,zz2)
+        call hol2char(lt,1,2,ch1)
+        idum=ib2as(im1,lt,1,zz2)
+        call hol2char(lt,1,2,cm1)
+        idum=ib2as(is1,lt,1,zz2)
+        call hol2char(lt,1,2,cs1)
+       else ! blank out start time
         ch1='  '
         cm1='  '
         cs1='  '
       endif
       if (kwrap.and..not.ks2) then
-        write(luprt,9330) nsline,csor,iaz,iel,cwrap,ih1,im1,is1,
+        write(luprt,9330) nsline,csor,iaz,iel,cwrap,ch1,cm1,cs1,
      .  ihd,imd,isd,ih2,im2,is2,idm,ids,cpass,cdir,ifeet,cnewtap
-9330    format(1x,i5,1x,a8,1x,i3,1x,i2,1x,a5,1x,i2.2,':',i2.2,':',
-     .  i2.2,1x,i2.2,':',i2.2,':',i2.2,1x,i2.2,':',i2.2,':',i2.2,
+9330    format(1x,i5,1x,a8,1x,i3,1x,i2,1x,a5,1x,a2,':',a2,':',
+     .  a2,1x,i2.2,':',i2.2,':',i2.2,1x,i2.2,':',i2.2,':',i2.2,
      .  1x,i2.2,':',i2.2,2x,a2,1x,a3,1x,i5,1x,a3)
       else if (.not.kwrap.and..not.ks2) then
-        write(luprt,9380) nsline,csor,iaz,iel,ih1,im1,is1,ihd,imd,
+        write(luprt,9380) nsline,csor,iaz,iel,ch1,cm1,cs1,ihd,imd,
      .  isd,ih2,im2,is2,idm,ids,cpass,cdir,ifeet,cnewtap
-9380    format(1x,i5,1x,a8,2x,i3,1x,i2,1x,1x,i2.2,':',i2.2,':',
-     .  i2.2,2x,i2.2,':',i2.2,':',i2.2,2x,i2.2,':',i2.2,':',i2.2,
+9380    format(1x,i5,1x,a8,2x,i3,1x,i2,1x,1x,a2,':',a2,':',
+     .  a2,2x,i2.2,':',i2.2,':',i2.2,2x,i2.2,':',i2.2,':',i2.2,
      .  2x,i2.2,':',i2.2,2x,a2,1x,a3,1x,i5,1x,a3)
       else if (kwrap.and.ks2) then
         write(luprt,9331) nsline,csor,iaz,iel,cwrap,ch1,cm1,cs1,
-     .  ihd,imd,isd,ih2,im2,is2,idm,ids,cpass,ifeet,cnewtap
+     .  ihd,imd,isd,ih2,im2,is2,idm,ids,cpass,ifeet/60,cnewtap
 9331    format(1x,i5,1x,a8,1x,i3,1x,i2,1x,a5,1x,a2,':',a2,':',
      .  a2,1x,i2.2,':',i2.2,':',i2.2,1x,i2.2,':',i2.2,':',i2.2,
      .  1x,i2.2,':',i2.2,2x,a2,1x,i5,1x,a3)
       else if (.not.kwrap.and.ks2) then
         write(luprt,9381) nsline,csor,iaz,iel,ch1,cm1,cs1,ihd,imd,
-     .  isd,ih2,im2,is2,idm,ids,cpass,ifeet,cnewtap
+     .  isd,ih2,im2,is2,idm,ids,cpass,ifeet/60,cnewtap
 9381    format(1x,i5,1x,a8,2x,i3,1x,i2,1x,1x,a2,':',a2,':',
      .  a2,2x,i2.2,':',i2.2,':',i2.2,2x,i2.2,':',i2.2,':',i2.2,
      .  2x,i2.2,':',i2.2,2x,a2,1x,i5,1x,a3)
@@ -125,11 +138,6 @@ C   Now write the scan line
       ncount = ncount + 1 ! count of observations
       if (cnewtap.eq.'XXX') ntapes=ntapes+1
       cnewtap = '   '
-C     Calculate footage at the end of the current scan
-      if (.not.ks2) then 
-        idur = 60*idm + ids
-        ifeet = ifeet + idir*(idur+itearl_local)*(speed_snap/12.0)
-      endif
 
       return
       end
