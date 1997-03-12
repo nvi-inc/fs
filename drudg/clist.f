ef543df0992900a179db935feefdf6a24f62bb4c
Nancy Vandenberg <>
1997-03-12 00:00:00 +0000

    various changes
    1997-03-01
    Implement font size logic and calls to setprint.
    
    1997-03-04
    Add COPTION for default formatting.
    
    1997-03-12
    Add call to READ_SNAP1 to read the first line.

--- drudg/clist.f
+++ drudg/clist.f
@@ -1,4 +1,4 @@
-	SUBROUTINE CLIST(kskd)
+      SUBROUTINE CLIST(kskd)
 C
 C This routine reads a SNAP schedule file and makes a
 C listing with all of the commands for one observation
@@ -22,33 +22,27 @@ C            Added call to SETPRINT
 C NRV 910703 Added call to PRTMP at end.
 C nrv 930407 implicit none
 c 960201 nrv check for 'source' as well as 'SOURCE'
+C 970301 nrv Implement font size logic and calls to setprint.
+C 970304 nrv Add COPTION for defaults.
+C 970312 nrv Add call to READ_SNAP1 to read the first line.
 
       include '../skdrincl/skparm.ftni'
       include 'drcom.ftni'
       include '../skdrincl/../skdrincl/statn.ftni'
 C
 C Input:
-	logical kskd
+      logical kskd
 C
 C LOCAL:
-      CHARACTER*256 BUF,LBUF
-      INTEGER IC, TRIMLEN, IL, NCHAR, MAXLIN
+      CHARACTER*256 cbuf,LBUF
+      character cs
+      INTEGER iwid,IC, TRIMLEN, IL, NCHAR, MAXLIN
       integer maxwid,ierr,nrec,iyear,i,npage,numlin,l
       character*8 cexper,cstn
+      character*1 cid1
+      character*2 cid2
       LOGICAL EX
 C     integer*4 ifbrk
-C
-C  First set up for proper width/length.
-
-      IF (IWIDTH.EQ.80) THEN
-	  MAXLIN = 46
-	  maxwid = 70
-      ELSE
-C  MAXLIN = 64
-C  maxwid = 110
-          maxlin = 52
-          maxwid = 120
-      ENDIF
 C
 C   Check for existence of SNAP file.
 
@@ -60,11 +54,11 @@ C   Check for existence of SNAP file.
         RETURN
       ENDIF
 
-	OPEN(UNIT=LU_INFILE,FILE=SNPNAME,STATUS='OLD',IOSTAT=IERR)
-	IF (IERR.EQ.0) THEN
-	  REWIND(LU_INFILE)
-	  CALL INITF(LU_INFILE,IERR)
-	ELSE
+      OPEN(UNIT=LU_INFILE,FILE=SNPNAME,STATUS='OLD',IOSTAT=IERR)
+      IF (IERR.EQ.0) THEN
+        REWIND(LU_INFILE)
+        CALL INITF(LU_INFILE,IERR)
+      ELSE
         WRITE(LUSCN,9060) IERR, SNPNAME(1:IC)
 9060    FORMAT(' CLIST02 - ERROR ',I4,' OPENING SNAP FILE ',A)
         RETURN
@@ -72,18 +66,56 @@ C   Check for existence of SNAP file.
 
 C  Read the first line to get the station name for the header
 C  in case this is a SNAP file reading only.
-	read(lu_infile,'(a)',err=990,end=990,iostat=IERR) buf
-	nrec=1
-	read(buf,9001) cexper,iyear,cstn
-9001  format(2x,a8,2x,i4,1x,a8,2x,a1)
-	if (.not.kskd) call char2hol(cstn,lstnna(1,1),1,8)
-	WRITE(LUSCN,9100) (LSTNNA(I,ISTN),I=1,4),SNPNAME(1:ic)
+      nrec=1
+      read(lu_infile,'(a)',err=990,end=990,iostat=IERR) cbuf
+      call read_snap1(cbuf(2:),cexper,iyear,cstn,cid1,cid2,ierr)
+      if (ierr.lt.0) then ! set defaults instead
+        if (ierr.ge.-1) cexper='XXX'
+        if (ierr.ge.-2) iyear=0
+        if (ierr.ge.-3) cstn='        '
+        if (ierr.ge.-4) cid1=' '
+        if (ierr.ge.-5) cid2='  '
+      endif
+      ierr=0
+C     read(cbuf,9001) cexper,iyear,cstn
+C9001  format(2x,a8,2x,i4,1x,a8,2x,a1)
+      if (.not.kskd) call char2hol(cstn,lstnna(1,1),1,8)
+      WRITE(LUSCN,9100) (LSTNNA(I,ISTN),I=1,4),SNPNAME(1:ic)
 9100  FORMAT(' SNAP COMMAND FILE LISTING FOR ',4A2,' FROM FILE ',A)
 C
 C
-C  Setup printer for either landscape or portrait depending on iwidth
+C  Set up for proper width/length, depending on font size and
+C  orientation.
+
+      cs = csize
+      if (cs.eq.'D') cs = coption(2)(2:2) ! use default
+      iwid = iwidth
+      if (iwid.eq.-1) then ! use default
+        if (coption(2)(1:1).eq.'P') iwid=80
+        if (coption(2)(1:1).eq.'L') iwid=137
+      endif
+      IF (iwid.EQ.80.or.iwid.eq.-1) THEN ! portrait
+        if (cs.eq.'L') then ! large font
+          MAXLIN = 50
+          maxwid = 70
+          call setprint(ierr,0)
+        else ! small font, default
+          MAXLIN = 70
+          maxwid = 115
+          call setprint(ierr,2)
+        endif
+      ELSE ! landscape 
+        if (cs.eq.'L') then ! large
+          call setprint(ierr,1)
+          maxlin = 35
+          maxwid = 90
+        else ! small, default
+          call setprint(ierr,3)
+          maxlin = 50
+          maxwid = 140
+        endif
+      ENDIF
 C
-	call setprint(ierr,iwidth,0)
       IF (IERR.ne.0) THEN
         WRITE(LUSCN,9061) IERR
 9061    FORMAT(' CLIST03 - ERROR ',I5,' accessing printer')
@@ -99,27 +131,27 @@ C   Loop through SNAP FILE
       NPAGE = NPAGE + 1
       NUMLIN = 0
 C
-	if (kskd) then
-	  ic=trimlen(snpname)
-	  write(luprt,9302) snpname(1:ic),(lstnna(i,istn),i=1,4),npage
+      if (kskd) then
+        ic=trimlen(snpname)
+        write(luprt,9302) snpname(1:ic),(lstnna(i,istn),i=1,4),npage
 9302    FORMAT(//'  SNAP Commands in file ',A,' for station ',
      .       4A2,10x,'Page ',I3//)
-	else
-	  write(luprt,9303) snpname(1:ic),cstn,npage
+      else
+        write(luprt,9303) snpname(1:ic),cstn,npage
 9303    format(//'  SNAP Commands in file ',A,' for station ',
      .       A8,10x,'Page ',i3//)
-	endif
+      endif
 
-	if (nrec.eq.1) then !write out first line
-	  l = trimlen(buf)
-	  WRITE(LUPRT,9904) nrec,BUF(1:l)
-	  NUMLIN = NUMLIN + 1
-	endif
+      if (nrec.eq.1) then !write out first line
+        l = trimlen(cbuf)
+        WRITE(LUPRT,9904) nrec,cbuf(1:l)
+        NUMLIN = NUMLIN + 1
+      endif
 
 120   IF (NUMLIN.GE.MAXLIN) GOTO 110
-      CALL clear_array(BUF)
-      READ(LU_INFILE,'(A)',END=108,IOSTAT=IERR) BUF
-      IL = TRIMLEN(BUF)
+      CALL clear_array(cbuf)
+      READ(LU_INFILE,'(A)',END=108,IOSTAT=IERR) cbuf
+      IL = TRIMLEN(cbuf)
       IF (IERR.NE.0) GOTO 990
       NREC = NREC + 1
       IF (IL.GE.0) GOTO 125
@@ -133,8 +165,8 @@ C END OF FILE DETECTED
 C
 C WE HAVE A COMMAND IN THE BUFFER
 
-125   IF ((BUF(1:6).NE.'SOURCE').and.(buf(1:6).ne.'source')
-     ..AND.(BUF(1:1).NE.CHAR(34)))
+125   IF ((cbuf(1:6).NE.'SOURCE').and.(cbuf(1:6).ne.'source')
+     ..AND.(cbuf(1:1).NE.CHAR(34)))
 C                             "
      .    GOTO 130
       IF (NREC.EQ.1.OR.NCHAR.LE.1) GOTO 126
@@ -142,17 +174,17 @@ C
 C SOURCE COMMAND OR COMMENT IS IN THE BUFFER
 C FIRST WRITE OUT THE PREVIOUS LINE WITH OLD COMMANDS
 
-	NCHAR = MIN0(NCHAR,128)
+      NCHAR = MIN0(NCHAR,128)
 Cif (ifbrk().lt.0) goto 999
-	WRITE(LUPRT,9902) LBUF(1:NCHAR)
+      WRITE(LUPRT,9902) LBUF(1:NCHAR)
 9902  FORMAT(1X,6X,1X,A)
       NUMLIN = NUMLIN + 1
 C
 C NOW WRITE OUT THE CURRENT LINE NUMBER AND THE NEW SOURCE COMMAND
 
-126   WRITE(LUPRT,9904) NREC, BUF(1:IL)
+126   WRITE(LUPRT,9904) NREC, cbuf(1:IL)
 9904  FORMAT(1X,I6,1X,A)
-	numlin = numlin + 1
+      numlin = numlin + 1
       NCHAR = 1
       GOTO 120
 130   CONTINUE
@@ -161,7 +193,7 @@ C
 C CHECK TO MAKE SURE THAT THE COMMAND WILL FIT ON THE PAGE,
 C IF NOT, THEN WRITE OUT BUFFER, AND CONTINUE
 
-	IF ((NCHAR+IL+1).LE.(maxwid)) GOTO 140
+      IF ((NCHAR+IL+1).LE.(maxwid)) GOTO 140
       NCHAR=MIN0(NCHAR,128)
       NUMLIN = NUMLIN + 1
       WRITE(LUPRT,9902) LBUF(1:NCHAR)
@@ -169,7 +201,7 @@ C IF NOT, THEN WRITE OUT BUFFER, AND CONTINUE
 C
 C ADD THE COMMAND TO THE END OF THE LINE AND ADD ONE BLANK
 
-140   LBUF(NCHAR:NCHAR+IL+1) = BUF(1:IL) // ' '
+140   LBUF(NCHAR:NCHAR+IL+1) = cbuf(1:IL) // ' '
       NCHAR = NCHAR+IL+1
       GOTO 120
 
@@ -180,9 +212,12 @@ C ADD THE COMMAND TO THE END OF THE LINE AND ADD ONE BLANK
       endif
 
 999   call luff(luprt)
-C     if (cprttyp.eq.'FILE') CLOSE(LUPRT)
       close(luprt)
-      call prtmp
+      if (iwid.eq.-1.or.iwid.eq.137) then
+        call prtmp(1)
+      else
+        call prtmp(0)
+      endif
 C
       RETURN
       END
