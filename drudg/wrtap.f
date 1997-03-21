5614ad62f2b81e8b7941d1a4ac8b3f9087e1357e
Nancy Vandenberg <>
1997-03-21 00:00:00 +0000

    Re-write for more flexibility.

--- drudg/wrtap.f
+++ drudg/wrtap.f
@@ -1,7 +1,6 @@
-	SUBROUTINE wrtap(lspdir,ispin,ihead,lu,iwr,ktape,irec)
+      SUBROUTINE wrtap(lspdir,ispin,ihead,lu,iwr,ktape,irec)
 C
-C  WRTAP writes the tape lines for the VLBA
-C  pointing schedules.
+C  WRTAP writes the tape lines for the VLBA pointing schedules.
 C
 C   HISTORY:
 C     WHO   WHEN   WHAT
@@ -9,63 +8,57 @@ C     gag   900724 CREATED
 c     gag   901025 added ktape logical for NEXT writing
 C     nrv   930709 Now "ihead" is the actual head position in microns,
 C                  removed the hard-coded array.
+C 970321 nrv Re-write for more flexibility.
 C
 C   COMMON BLOCKS USED
       include '../skdrincl/skparm.ftni'
       include 'drcom.ftni'
-      include '../skdrincl/statn.ftni'
-      include '../skdrincl/sourc.ftni'
 C
 C  INPUT:
         integer*2 lspdir
-	integer ispin,ihead,lu,iwr,irec
-	logical ktape
+      integer ispin,ihead,lu,iwr,irec
+      logical ktape
 C
 C     CALLED BY: vlbat
 C
 C  LOCAL VARIABLES
-        integer*2 ltape(23)
-	integer idum,iln,ierr
+      integer nch,idum,iln,ierr
       integer ib2as,ichmv,ichmv_ch ! functions
 
-C  INITIALIZED:
-	DATA ltape/'ta','pe','=(','1,','s0','00','00','0)','  ',
-     .           'wr','it',
-     .           'e=','(1',',0','00',') ','he','ad','=(','1,','00',
-     .           '00',') '/
-C
-      
-C
-C     Encode the recorder number into the string
-      idum = ib2as(irec,ltape,7,1)
-      idum = ib2as(irec,ltape,26,1)
-      idum = ib2as(irec,ltape,39,1)
+      call ifill(ibuf,1,ibuf_len,oblank)
+
+      nch = ichmv_ch(ibuf,1,'tape=(')
+      nch = nch + ib2as(irec,ibuf,nch,1) ! recorder number
+      nch = ichmv_ch(ibuf,nch,',')
       if (ispin.ne.0) then
-          idum=ichmv(ltape, 9,LSPDIR,1,1)
-C         idum = ib2as(ispin,ltape,10,3)
-	  if (ispin.eq.330) then
-	    idum = ichmv_ch(ltape,10,'REWIND)')
-	  else
-	    idum = ichmv_ch(ltape,10,'RUN)   ')
-	  endif
+        nch=ichmv(ibuf,nch,LSPDIR,1,1)
+        if (ispin.eq.330) then
+          nch = ichmv_ch(ibuf,nch,'REWIND) ')
+        else
+          nch = ichmv_ch(ibuf,nch,'RUN) ')
+        endif
       else
-	  idum = ichmv_ch(ltape,9,'STOP)   ')
+        nch = ichmv_ch(ibuf,nch,'STOP) ')
       end if
 
+      nch = ichmv_ch(ibuf,nch,' write=(')
+      nch = nch + ib2as(irec,ibuf,nch,1) ! recorder number
+      nch = ichmv_ch(ibuf,nch,',')
       if (iwr.eq.0) then
-	  idum = ichmv_ch(ltape,28,'off)')
+       nch = ichmv_ch(ibuf,nch,'off)')
       else
-	  idum = ichmv_ch(ltape,28,'on) ')
+        nch = ichmv_ch(ibuf,nch,'on) ')
       end if
 
-	Idum = ib2as(IHEAD,ltape,41,4)
-	idum = ichmv(ibuf,1,ltape,1,46)
-	iln = 23
-	if (ktape) then
-	  call char2hol('  !NEXT!',ibuf,46,54)
-	  iln = 27
-	end if
-	CALL writf_asc(LU,IERR,ibuf,iln)
+      nch = ichmv_ch(ibuf,nch,' head=(')
+      nch = nch + ib2as(irec,ibuf,nch,1) ! recorder number
+      nch = ichmv_ch(ibuf,nch,',')
+      nch = nch + ib2as(IHEAD,ibuf,nch,4)
+      nch = ichmv_ch(ibuf,nch,')')
+
+      if (ktape) nch = ichmv_ch(ibuf,nch,'  !NEXT!')
+
+      CALL writf_asc(LU,IERR,ibuf,(nch+1)/2)
 
       RETURN
       END
