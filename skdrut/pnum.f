f9fac7def16a5746547cacce2c6cea559ee064ad
Nancy Vandenberg <>
1997-05-23 00:00:00 +0000

    Passes "d" and "e" (39 and 40) were reversed in data statement.

--- skdrut/pnum.f
+++ skdrut/pnum.f
@@ -1,12 +1,13 @@
       character*1 function pnum(i)
 C  Return the character corresponding to the pass index.
 C 960527 nrv New.
+C 970530 nrv "d" and "e" reversed!
 
       integer i
       character*61 cp ! pass numbers
       character*1 cc
 
-      data cp/'123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcedfghijklmnopqrstuv
+      data cp/'123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuv
      .wxyz'/
 
       cc = cp(i:i)
