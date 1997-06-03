e394ea00cf94472a29604e052ee357677ebf9f93
Nancy Vandenberg <>
1997-06-03 00:00:00 +0000

    various changes
    1997-03-17
    Remove TAPE_MOTION and BARREL. Read $PARAM using sked's routines.
    
    1997-04-01
    Set all parameter values to defaults at the start, then if
    
    1997-05-20
    Save the location of the $PROCS section.
    
    1997-06-03
    Find the start of the cover letter in .drg files.

--- drudg/sread.f
+++ drudg/sread.f
@@ -13,24 +13,24 @@ C  Output:
 C  Local:
       integer ical,iyr,idayr,ihr,imin,isc,nstnsk,mjd,mon,ida
       double precision ut,gst
+      integer*2 ibufq(ibuf_len)
       integer*2 LSNAME(max_sorlen/2),LSTN(MAX_STN),LCABLE(MAX_STN),
      .          LMON(2),LDAY(2),LPRE(3),LPST(3),LMID(3),LDIR(MAX_STN),
      .          lfreq,itim1(6),itim2(6)
-      integer   IPAS(MAX_STN),IFT(MAX_STN),IDUR(MAX_STN)
-      integer irec,ipnt,ilen,ltype,ich,ic1,ic2,idummy,ic,
-     .icx,nch,iret,i
+      integer   IPAS(MAX_STN),IFT(MAX_STN),IDUR(MAX_STN),ioff(max_stn)
+      integer irec,ipnt,ilen,ltype,ich,ic1,ic2,idummy,
+     .nch,iret,i
       integer htype ! section 2-letter code
+      logical kearl
       logical kcod ! set to ksta when $CODES is found
       logical ksta ! set to true when $STATIONS is found
       logical kvlb ! set to true when $VLBA is found
       logical khed ! set to ksta when $HEAD is found
-      logical kearl,kpartm,ksettm,ksortm,ktaptm,khdtm
       integer Z24,hbb,hex,hpa,hso,hst,hfr,hsk,hpr,Z20,hhd,idum
-      integer iscnc,iscn_CH,ias2b,jchar,ichcm,ichmv ! functions
+      integer iscn_ch,iscnc,jchar,ichcm,ichmv ! functions
       integer ichcm_ch,trimlen
       real rdum,reio
       character*128 cbuf
-      character*128 ltape
 C Initialized:
       data    Z24/Z'24'/, hbb/2h  /, hex/2hEX/, hpa/2hPA/, hso/2hSO/
       data    hhd/2hHD/
@@ -56,6 +56,11 @@ C 970129 nrv Add a check for TAPE_MOTION and GAP
 C 970207 nrv Unpack each scan and check the parity flag
 C 970219 nrv If parameter values are not in schedule file, set to defaults
 C 970307 nrv Initialize ISKREC for SKED files, and time order it at the end.
+C 970317 nrv Remove TAPE_MOTION and BARREL
+C 970317 nrv Read $PARAM using sked's routines.
+C 970401 nrv Set all parameter values to defaults at the start, then if
+c            the key words are found in the $PARAM section they get set.
+C 970603 nrv Find the start of the cover letter in .drg files.
 C
 C
       close(unit=LU_INFILE)
@@ -72,16 +77,9 @@ C
 C
       IRECEL = -1.0
       ksta = .false.
-        kcod = .false.
-        kvlb = .false.
-        khed = .false.
-      ltape=''
-          kparity = .false.
-      ksettm = .false.
-      kpartm = .false.
-      ktaptm = .false.
-      ksortm = .false.
-      khdtm = .false.
+      kcod = .false.
+      kvlb = .false.
+      khed = .false.
 C
       kvex=.false.
       read(lu_infile,'(a)') cbuf
@@ -95,11 +93,6 @@ C       read stations, codes, sources
           write(luscn,9009) iret,ierr
 9009      format(' from VREAD iret=',i5,' ierr=',i5)
         endif
-C       call vglinp(ivexnum,luscn,ierr)
-C       if (ierr.ne.0) then
-C         write(luscn,9010) ierr
-C9010      format(' from VEXINP ierr=',i5)
-C       endif
 C       Write out experiment information now.
         write(luscn,'(/"Experiment name: ",4a2)') lexper
         i=trimlen(cexperdes)
@@ -122,8 +115,6 @@ C       Write out experiment information now.
         call char2hol('  ',LTYPE,1,2)
         IF (ichcm_ch(IBUF,1,'$EXPER').EQ.0) THEN
           call char2hol('EX',LTYPE,1,2)
-        ELSE IF (ichcm_ch(IBUF,1,'$PARAM').EQ.0) THEN
-          call char2hol('PA',LTYPE,1,2)
         ELSE IF (ichcm_ch(IBUF,1,'$SOURC').EQ.0) THEN
           call char2hol('SO',LTYPE,1,2)
         ELSE IF (ichcm_ch(IBUF,1,'$STATI').EQ.0) THEN
@@ -160,74 +151,6 @@ C         Get the next line
             call hol2char(ibuf,1,ilen*2,cbuf)
             write(luscn,'(a)') cbuf(1:trimlen(cbuf))
             CALL READS(LU_INFILE,IERR,IBUF,ISKLEN,ILEN,3)
-          END DO
-C
-        ELSE IF(ichcm(htype,1,hpa,1,2).eq.0) THEN !parameter section
-          CALL READS(LU_INFILE,IERR,IBUF,ISKLEN,ILEN,2)
-          DO WHILE (JCHAR(IBUF,1).NE.Z24.AND.ILEN.NE.-1)
-            IF (ichcm_ch(IBUF,1,'ELEVATION').EQ.0) THEN !el line
-              IF (IRECEL.EQ.-1.0) call locf(LU_INFILE,IRECEL) 
-            ELSE
-              IC = ISCN_CH(IBUF,1,ILEN*2,'PARITY')
-              IF (IC.NE.0) THEN !parity time
-                CALL GTFLD(IBUF,IC+6,ILEN*2,IC1,IC2)
-                IPARTM=IAS2B(IBUF,IC1,IC2-IC1+1)
-                kpartm=.true.
-              ENDIF !parity time
-              IC = ISCN_CH(IBUF,1,ILEN*2,'SETUP')
-              IF (IC.NE.0) THEN !setup time
-                CALL GTFLD(IBUF,IC+5,ILEN*2,IC1,IC2)
-                ISETTM=IAS2B(IBUF,IC1,IC2-IC1+1)
-                ksettm=.true.
-              ENDIF !setup time
-              IC = ISCN_CH(IBUF,1,ILEN*2,'BARREL')
-              IF (IC.NE.0) THEN !barrel roll
-                CALL GTFLD(IBUF,IC+7,ILEN*2,IC1,IC2)
-C               if (ic1.ne.0) idummy = ichmv(lbarrel,1,ibuf,ic1,ic2-ic1+1)
-              ENDIF !barrel roll
-              IC = ISCN_CH(IBUF,1,ILEN*2,'SOURCE')
-              IF (IC.NE.0) THEN !source time
-                CALL GTFLD(IBUF,IC+6,ILEN*2,IC1,IC2)
-                ISORTM=IAS2B(IBUF,IC1,IC2-IC1+1)
-                ksortm=.true.
-              ENDIF !source time
-              IC = ISCN_CH(IBUF,1,ILEN*2,'HEAD')
-              IF (IC.NE.0) THEN !head time
-                CALL GTFLD(IBUF,IC+4,ILEN*2,IC1,IC2)
-                IHDTM=IAS2B(IBUF,IC1,IC2-IC1+1)
-                khdtm=.true.
-              ENDIF !head time
-              IC = ISCN_CH(IBUF,1,ILEN*2,'EARLY')
-              IF (IC.NE.0) THEN !early time
-                CALL GTFLD(IBUF,IC+5,ILEN*2,IC1,IC2)
-                ITEARL(1)=IAS2B(IBUF,IC1,IC2-IC1+1) ! save one value
-              ENDIF !early time
-              IC = ISCN_CH(IBUF,1,ILEN*2,'TAPE ') ! older name
-              ICX= ISCN_CH(IBUF,1,ILEN*2,'TAPETM')
-              IF (IC.NE.0.or.icx.ne.0) THEN !tape time
-                if (icx.eq.0) ICH=IC+4
-                IF (IC.EQ.0) ICH=ICX+6
-                CALL GTFLD(IBUF,ICH,ILEN*2,IC1,IC2)
-                ITAPTM=IAS2B(IBUF,IC1,ic2-ic1+1)
-                ktaptm=.true.
-              endif !tape time
-              IC= ISCN_CH(IBUF,1,ILEN*2,'TAPE_MOTION')
-              IF (IC.NE.0) THEN !tape motion
-                ICH=IC+12
-                CALL GTFLD(IBUF,ICH,ILEN*2,IC1,IC2)
-                nch=ic2-ic1+1
-                if (nch.gt.0) call hol2char(ibuf,ic1,ic2,ltape)
-              endif !tape motion
-              IC= ISCN_CH(IBUF,1,ILEN*2,'GAP')
-              IF (IC.NE.0) THEN !gap time
-                ICH=IC+3
-                CALL GTFLD(IBUF,ICH,ILEN*2,IC1,IC2)
-                nch=ic2-ic1+1
-                if (nch.gt.0) itgap(1)=IAS2B(IBUF,IC1,nch)
-              endif !gap time
-            ENDIF
-C
-            CALL READS(LU_INFILE,IERR,IBUF,ISKLEN,ILEN,2)
           END DO
 C
         ELSE IF(ichcm(htype,1,hso,1,2).eq.0 
@@ -282,7 +205,7 @@ C           Store in memory
 C         Unpack the scan just to get kflg
           CALL UNPSK(IBUF,ILEN,LSNAME,ICAL,LFREQ,IPAS,LDIR,IFT,LPRE,
      .    IYR,IDAYR,IHR,iMIN,ISC,IDUR,LMID,LPST,NSTNSK,LSTN,LCABLE,
-     .    MJD,UT,GST,MON,IDA,LMON,LDAY,IERR,KFLG)
+     .    MJD,UT,GST,MON,IDA,LMON,LDAY,IERR,KFLG,ioff)
 C           If any scans have flag2 set, remember this.
             if (kflg(2)) kparity = .true.
 C
@@ -292,8 +215,7 @@ C           Read the next schedule entry
 C
         ELSE IF(ichcm(htype,1,hpr,1,2).eq.0) THEN !procedures
           rdum= reio(2,LUSCN,IBUF,-ILEN)
-C         write(luscn,'(20a2)') (ibuf(i),i=1,(ilen+1)/2)
-C
+          write(luscn,'(20a2)') (ibuf(i),i=1,(ilen+1)/2)
 C         Get the position of this section
           call locf(LU_INFILE,IRECPR)
 C         And read the first line
@@ -344,25 +266,75 @@ C           write(luscn,'(20a2)') (ibuf(i),i=1,(ilen+1)/2)
         CALL READS(LU_INFILE,IERR,IBUF,ISKLEN,ILEN,1)
         enddo 
       endif
-C
-C For SKED file, initialize early start for all stations and set 
-C other tape values to zero.
-      do i=1,max_stn
-        itearl(i)=itearl(1)
-        itlate(i)=0
-        itgap(i)=itgap(1)
-        if (ltape.ne.'') then
-          tape_motion_type(i)=ltape
-        else ! default
-          tape_motion_type(i)='start&stop'
-        endif
-      enddo
-C For .DRG files, these times will probably not be defined!
-      if (.not.ksettm) isettm = 20
-      if (.not.kpartm) ipartm = 70
-      if (.not.ktaptm) itaptm = 1
-      if (.not.ksortm) isortm = 5
-      if (.not.khdtm) ihdtm = 6
+C Go back to parameter section and read it now.
+        write(luscn,'(" Re-reading ... ",$)')
+        rewind(LU_INFILE)
+        do i=1,nstatn
+          tape_motion_type(i)='START&STOP'
+          itearl(i)=0
+          itgap(i)=0
+          itlate(i)=0
+        enddo
+        isettm = 20
+        ipartm = 70
+        itaptm = 1
+        isortm = 5
+        ihdtm = 6
+        CALL READS(LU_INFILE,IERR,IBUF,ISKLEN,ILEN,1)
+        DO WHILE (ILEN.GT.0) !read schedule file
+          IF (ichcm_ch(IBUF,1,'$PARAM').EQ.0) THEN
+            rdum= reio(2,LUSCN,IBUF,-ILEN)
+C           write(luscn,'(40a2)') (ibuf(i),i=1,(ilen+1)/2)
+            CALL READS(LU_INFILE,IERR,IBUF,ISKLEN,ILEN,2)
+            DO WHILE (JCHAR(IBUF,1).NE.Z24.AND.ILEN.NE.-1)
+              ich=1
+              CALL GTFLD(IBUF,ICH,ILEN*2,IC1,IC2)
+              nch=ilen-ic2+1
+              if (ichcm_ch(ibuf,ic1,'ELEVATION ').eq.0) then
+                ibufq(1)=nch
+                idum=ichmv(ibufq(2),1,ibuf,ic2+1,nch)
+                call selev(ibufq,luscn,luscn)
+              else if (ichcm_ch(ibuf,ic1,'TAPE_MOTION').eq.0) then
+                ibufq(1)=nch
+                idum=ichmv(ibufq(2),1,ibuf,ic2+1,nch)
+                call stape(ibufq,luscn,luscn)
+              else if (ichcm_ch(ibuf,ic1,'EARLY_START ').eq.0) then
+                ibufq(1)=nch
+                idum=ichmv(ibufq(2),1,ibuf,ic2+1,nch)
+                call searl(ibufq,luscn,luscn)
+              else if (ichcm_ch(ibuf,ic1,'SNR  ').eq.0) then
+              else if (ichcm_ch(ibuf,ic1,'SCAN  ').eq.0) then
+              else if (ichcm_ch(ibuf,ic1,'SUBNET  ').eq.0) then
+              else if (ichcm_ch(ibuf,ic1,'WEIGHT  ').eq.0) then
+              else
+                ibufq(1)=ilen
+                idum=ichmv(ibufq(2),1,ibuf,1,ilen)
+                call drset(ibufq)
+              endif
+              CALL READS(LU_INFILE,IERR,IBUF,ISKLEN,ILEN,2)
+            enddo !read $PARAM section
+          endif
+          CALL READS(LU_INFILE,IERR,IBUF,ISKLEN,ILEN,1)
+        enddo !read schedule file
+C       Look for the string "Cover Letter"
+        ireccv=0
+        if (kdrg_infile) then ! .drg file
+          rewind(lu_infile)
+          call initf(lu_infile,ierr)
+          write(luscn,'(" Re-reading to find the cover letter ... ",$)')
+          CALL READF_ASC(lu_infile,iERR,IBUF,ISKLEN,ILEN)
+          call inc(lu_infile,ierr)
+          DO WHILE (ILEN.GT.0.and.ireccv.eq.0) !read schedule file
+            IF (iscn_ch(IBUF,1,ilEN*2,'Cover Letter').gt.0) THEN
+              call locf(LU_INFILE,IRECCV)
+            endif
+            CALL READF_ASC(lu_infile,iERR,IBUF,ISKLEN,ILEN)
+            call inc(lu_infile,ierr)
+          enddo !read schedule file
+          write(luscn,'()')
+        endif ! .drg file
+C Close the schedule file.
+      close(lu_infile)
 C
 C Order the observations, in case they were not so in the $SKED section.
       if (nobs.le.0) then
