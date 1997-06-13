06fda45b6f702238dccb241d6789c6d3d3cdcfab
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
1997-06-13 00:00:00 +0000

    don't update if character is not lower

--- fslb/upper.f
+++ fslb/upper.f
@@ -3,16 +3,17 @@
       implicit none
       integer*2 ibuf(1)
       integer ifc,ilc
-      integer nchar,i,ival
+      integer nchar,i,ival,jchar
       character ch
  
       nchar = ilc - ifc + 1
       if (nchar.le.0) return
       do i=ifc,ilc
-        call hol2char(ibuf,i,i,ch)
-        ival=ichar(ch)
-        if (ival.ge.97.and.ival.le.122) ch = char(ival-32)
-        call char2hol(ch,ibuf,i,i)
+        ival=jchar(ibuf,i)
+        if (ival.ge.97.and.ival.le.122) then
+           ival = ival-32
+           call pchar(ibuf,i,ival)
+        endif
       enddo
 
       return
