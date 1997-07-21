ddac941bde2eb2a5fa301331b736d98d2f4e9a18
Nancy Vandenberg <>
1997-07-21 00:00:00 +0000

    Replace LDIR in call with idir

--- drudg/lspin.f
+++ drudg/lspin.f
@@ -1,15 +1,15 @@
-      SUBROUTINE LSPIN(LDIR,ISPM,ISPS,IBUF2,NCH)
+      SUBROUTINE LSPIN(iDIR,ISPM,ISPS,IBUF2,NCH)
 C LSPIN  forms a buffer with the SNAP command FASTx=nnMmmS
 C
       include '../skdrincl/skparm.ftni'
 C
 C Input:
-      integer*2 ldir
-      integer ispm,isps,nch
+      integer idir,ispm,isps,nch
 	integer*2 IBUF2(ibuf_len)
 C
 C MODIFICATIONS - 880411 NRV DE-COMPC'D
 C nrv 930407 implicit none
+C 970721 nrv Replace LDIR in call with idir
 
 	integer Z8000
         integer ic,idummy
@@ -17,7 +17,9 @@ C nrv 930407 implicit none
 	DATA Z8000/Z'8000'/
 C
       IDUMMY = ichmv_ch(IBUF2,1,'FAST')
-      IDUMMY = ICHMV(IBUF2,5,LDIR,1,1)
+      if (idir.eq.+1) IDUMMY = ICHMV_ch(IBUF2,5,'f')
+      if (idir.eq.-1) IDUMMY = ICHMV_ch(IBUF2,5,'r')
+      if (idir.eq.0) return
       IDUMMY = ichmv_ch(IBUF2,6,'=')
 	IC=Z8000+2
       NCH = 7+IB2AS(ISPM,IBUF2,7,ic)
