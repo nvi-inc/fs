2ffdbd26283939228f1f2a191deaaa60465c26b7
Nancy Vandenberg <>
1997-07-22 00:00:00 +0000

    New. Removed from addscan and newscan

---
+++ skdrut/gdscan.f
@@ -0,0 +1,26 @@
+      integer function gdscan(ibuf,nch,igdata)
+
+C GDSCAN add the "good data offset" to the scan line
+C NOTE: ibuf and nch are modified on return.
+C History
+C 970722 nrv New. Removed from addscan and newscan
+
+C Input AND Output
+      integer*2 ibuf(*)
+      integer nch ! character to start with in ibuf
+      integer igdata ! good data offset in seconds
+
+C Local
+      integer*2 ibufx(4)
+      integer i
+      integer ib2as ! function
+
+C  Convert the integer into a temporary buffer because
+C  ib2as can't handle array indices greater than 256.
+      i = ib2as(igdata,ibufx,1,5) ! put into temporary buffer
+C  Now move the converted value into the buffer
+      nch = ichmv(ibuf,nch,ibufx,1,5)
+      gdscan=nch
+
+      return
+      end
