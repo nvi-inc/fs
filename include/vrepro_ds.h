1822579ba460e46cdcc995d30ce77f330fc65bdc
Ed Himwich <ed.himwich@nviinc.com>
1997-10-16 00:00:00 +0000

    various changes
    1997-09-18
    add head number
    
    1997-10-16
    headstack number and bitsynch

--- include/vrepro_ds.h
+++ include/vrepro_ds.h
@@ -3,6 +3,8 @@
 struct vrepro_cmd {      /* command parameters */
                          /* indices run over channels a(0) and b(1) */
      int mode[2];        /* mode read(0) or byp(1) */
-     int track[2];       /* m3 track # 1-28 */
+     int track[2];       /* track # 0-35 */
+     int head[2];        /* head # 1 or 2 */
      int equalizer[2];   /* equalizer std(0), alt1(1), alt2(2) */
+     int bitsynch;       /* 0...5 = 16, 8, 4, 2, 1, 0.5 Mbit/sec */
      };
