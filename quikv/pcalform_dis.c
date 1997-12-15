f9185996f146430a6246c589d0702d6b952f2625
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
1997-12-15 00:00:00 +0000

    add pcald and pcalform data structures and code

---
+++ quikv/pcalform_dis.c
@@ -0,0 +1,60 @@
+/* pcalform display */
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+
+#include "../include/params.h"
+#include "../include/fs_types.h"
+#include "../include/fscom.h"
+#include "../include/shm_addr.h"
+
+#define MAX_OUT 256
+
+void pcalform_dis(command,ip)
+struct cmd_ds *command;
+long ip[5];
+{
+      struct pcalform_cmd lclc;
+      int kcom, i, ierr, count, start;
+      char output[MAX_OUT];
+
+      kcom= command->argv[0] != NULL &&
+            *command->argv[0] == '?' && command->argv[1] == NULL;
+
+      if ((!kcom) && command->equal == '=') {
+         return;
+      } else if (kcom){
+	memcpy(&lclc,&shm_addr->pcalform,sizeof(lclc));
+      } else {
+	memcpy(&lclc,&shm_addr->pcalform,sizeof(lclc));
+      }
+
+   /* format output buffer */
+
+      strcpy(output,command->name);
+      strcat(output,"/");
+      start=strlen(output);
+
+      for (i=0;i<5;i++) ip[i]=0;
+
+      count=0;
+      while( count>= 0) {
+	count++;
+        pcalform_enc(output,&count,&lclc);
+
+	if(count > 0) {
+	  cls_snd(&ip[0],output,strlen(output),0,0);
+	  ip[1]++;
+	  output[start]='\0';
+	}
+      }
+      return;
+
+error:
+      ip[0]=0;
+      ip[1]=0;
+      ip[2]=ierr;
+      memcpy(ip+3,"pf",2);
+      return;
+}
