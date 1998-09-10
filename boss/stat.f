f3051641e48bd43e246046ace52d1a5ae19293e8
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
1998-09-10 00:00:00 +0000

    adjust for four digit year

--- boss/stat.f
+++ boss/stat.f
@@ -26,7 +26,7 @@ C
       integer fmpposition,fmpreadstr,fmpsetpos,fmpsetline
       integer ichcm_ch
       integer*2 ibuf(1),ib(50)
-      dimension it(5)
+      integer it(6)
       character*100 ibc
       character cjchar
       equivalence (ib,ibc)
@@ -63,21 +63,29 @@ C    Print display heading.
       call put_cons(lm1,32)
       call ifill_ch(ibuf,1,100,' ')
       idummy = ichmv_ch(ibuf,5,'time')
-      idummy = ichmv_ch(ibuf,16,'event')
-      call put_cons(ibuf,21)
+      idummy = ichmv_ch(ibuf,23,'event')
+      call put_cons(ibuf,27)
       call ifill_ch(ibuf,5,4,'-')
-      call ifill_ch(ibuf,16,5,'-')
-      call put_cons(ibuf,21)
+      call ifill_ch(ibuf,23,5,'-')
+      call put_cons(ibuf,27)
 C
 C    Get and display current time.
       call fc_rte_time(it,idum)
-      call char2hol('   -  :  :',lm1,1,80)
-      idummy = ib2as(it(5),lm1,1,o'41403')
-      idummy = ib2as(it(4),lm1,5,o'41002')
-      idummy = ib2as(it(3),lm1,8,o'41002')
-      idummy = ib2as(it(2),lm1,11,o'41002')
-      idummy = ichmv_ch(lm1,15,'now')
-      call put_cons(lm1,18)
+      nch = 1 + ib2as(idum ,lm1,1,o'42004')
+      nch = ichmv_ch(lm1,nch,'.')
+      nch = nch + ib2as(it(5),lm1,nch,o'41403')
+      nch = ichmv_ch(lm1,nch,'.')
+      nch = nch + ib2as(it(4),lm1,nch,o'41002')
+      nch = ichmv_ch(lm1,nch,':')
+      nch = nch + ib2as(it(3),lm1,nch,o'41002')
+      nch = ichmv_ch(lm1,nch,':')
+      nch = nch + ib2as(it(2),lm1,nch,o'41002')
+      nch = ichmv_ch(lm1,nch,'.')
+      nch = nch + ib2as(it(1),lm1,nch,o'41002')
+      nch = ichmv_ch(lm1,nch,'  ')
+      nch0= nch
+      nch = ichmv_ch(lm1,nch,'now')
+      call put_cons(lm1,nch-1)
 C
 C   Save current schedule position.
       idum = fmpposition(idcbsk,ierr,irec,ioff)
@@ -95,46 +103,54 @@ C
         call putcon_ch('end of schedule')
         goto 300
 210   if(cjchar(ib,1).ne.'!') goto 220
-      if(cjchar(ib,2).lt.'0'.or.cjchar(ib,2).gt.'3') goto 200
+      if(index('0123456789',cjchar(ib,2)).eq.0) goto 200
       nc = iflch(ib,ilen)
       call gttim(ib,2,nc,0,it1,it2,it3,ierr)
-      idummy = ib2as(mod(it1,1024),lm1,1,o'40000'+o'1400'+3)
-      idummy = ib2as(it2/60,lm1,5,o'40000'+o'1000'+2)
-      idummy = ib2as(mod(it2,60),lm1,8,o'40000'+o'1000'+2)
-      idummy = ib2as(it3/100,lm1,11,o'40000'+o'01000'+2)
+      nch = 1   + ib2as(it1/1024+1970,lm1,1,o'42004')
+      nch = ichmv_ch(lm1,nch,'.')
+      nch = nch + ib2as(mod(it1,1024),lm1,nch,o'41403')
+      nch = ichmv_ch(lm1,nch,'.')
+      nch = nch + ib2as(it2/60,lm1,nch,o'41002')
+      nch = ichmv_ch(lm1,nch,':')
+      nch = nch + ib2as(mod(it2,60),lm1,nch,o'41002')
+      nch = ichmv_ch(lm1,nch,':')
+      nch = nch + ib2as(it3/100,lm1,nch,o'41002')
+      nch = ichmv_ch(lm1,nch,'.')
+      nch = nch + ib2as(mod(it3,100),lm1,nch,o'41002')
+      nch = ichmv_ch(lm1,nch,'  ')
 220   if (ichcm_ch(ib,1,'et').ne.0.or.kendo) goto 230
 C  End of observation
       if (ksto) then
-        idummy = ichmv_ch(lm1,15,'end of next observation   ')
+        nch = ichmv_ch(lm1,nch0,'end of next observation   ')
       else
-        idummy = ichmv_ch(lm1,15,'end of current observation')
+        nch = ichmv_ch(lm1,nch0,'end of current observation')
       end if
-      call put_cons(lm1,41)
+      call put_cons(lm1,nch-1)
       kendo = .true.
       goto 200
 C
 230   if (ichcm_ch(ib,1,'unlod').ne.0.or.kendtp) goto 240
 C  End of tape
       if (ksttp) then
-        idummy = ichmv_ch(lm1,15,'end of next tape   ')
+        nch = ichmv_ch(lm1,nch0,'end of next tape   ')
       else
-        idummy = ichmv_ch(lm1,15,'end of current tape')
+        nch = ichmv_ch(lm1,nch0,'end of current tape')
       end if
-      call put_cons(lm1,33)
+      call put_cons(lm1,nch-1)
       kendtp = .true.
       goto 200
 C
 240   if (ichcm_ch(ib,1,'st=').ne.0) goto 200
       if (ksto) goto 250
 C  Start of observation
-      idummy = ichmv_ch(lm1,15,'start of next observation')
-      call put_cons(lm1,40)
+      nch = ichmv_ch(lm1,nch0,'start of next observation')
+      call put_cons(lm1,nch-1)
       ksto = .true.
 C
 250   if (.not.kendtp.or.ksttp) goto 200
 C  Start of tape
-      idummy = ichmv_ch(lm1,15,'start of next tape')
-      call put_cons(lm1,32)
+      nch = ichmv_ch(lm1,nch0,'start of next tape')
+      call put_cons(lm1,nch-1)
       ksttp = .true.
       goto 200
 C
@@ -162,21 +178,28 @@ C
         ind = i
 320   continue
       if(ind.gt.0) goto 330
-        call ifill_ch(ibuf,40,9,'?')
-        ich=48
+        call ifill_ch(ibuf,ich,20,'?')
+        ich=ich+20-1
         goto 340
-330   it(5) = mod(itscb(1,ind),1024)
+ 330  continue
+      it(6) = itscb(1,ind)/1024+1970
+      it(5) = mod(itscb(1,ind),1024)
       it(4) = itscb(2,ind)/60
       it(3) = mod(itscb(2,ind),60)
       it(2) = itscb(3,ind)/100
-      idummy = ib2as(it(5),ibuf,40,o'41403')
-      idummy = ichmv_ch(ibuf,43,'-')
-      idummy = ib2as(it(4),ibuf,44,o'41002')
-      idummy = ichmv_ch(ibuf,46,':')
-      idummy = ib2as(it(3),ibuf,47,o'41002')
-      idummy = ichmv_ch(ibuf,49,':')
-      idummy = ib2as(it(2),ibuf,50,o'41002')
-      ich=51
+      it(1) = mod(itscb(3,ind),100)
+      ich = ich + ib2as(it(6),ibuf,ich,o'42004')
+      ich = ichmv_ch(ibuf,ich,'.')
+      ich = ich + ib2as(it(5),ibuf,ich,o'41403')
+      ich = ichmv_ch(ibuf,ich,'.')
+      ich = ich + ib2as(it(4),ibuf,ich,o'41002')
+      ich = ichmv_ch(ibuf,ich,':')
+      ich = ich + ib2as(it(3),ibuf,ich,o'41002')
+      ich = ichmv_ch(ibuf,ich,':')
+      ich = ich + ib2as(it(2),ibuf,ich,o'41002')
+      ich = ichmv_ch(ibuf,ich,'.')
+      ich = ich + ib2as(it(1),ibuf,ich,o'41002')
+      ich = ich - 1
 340   call put_cons(ibuf,ich)
       ich = ichmv_ch(ib,1,'operator stream is ')
       if(kopblk) goto 350
@@ -191,21 +214,28 @@ C
         ind = i
 360   continue
       if(ind.gt.0) goto 365
-        call ifill_ch(ib,40,9,'?')
-        ich = 48
+        call ifill_ch(ib,ich,20,'?')
+        ich = ich+20-1
         goto 370
-365   it(5) = mod(itscb(1,ind),1024)
+ 365  continue
+      it(6) = itscb(1,ind)/1024+1970
+      it(5) = mod(itscb(1,ind),1024)
       it(4) = itscb(2,ind)/60
       it(3) = mod(itscb(2,ind),60)
       it(2) = itscb(3,ind)/100
-      idummy = ib2as(it(5),ibuf,40,o'41403')
-      idummy = ichmv_ch(ib,43,'-')
-      idummy = ib2as(it(4),ibuf,44,o'41002')
-      idummy = ichmv_ch(ib,46,':')
-      idummy = ib2as(it(3),ibuf,47,o'41002')
-      idummy = ichmv_ch(ib,49,':')
-      idummy = ib2as(it(2),ibuf,50,o'41002')
-      ich = 51
+      it(1) = mod(itscb(3,ind),100)
+      ich = ich + ib2as(it(6),ib,ich,o'42004')
+      ich = ichmv_ch(ib,ich,'.')
+      ich = ich + ib2as(it(5),ib,ich,o'41403')
+      ich = ichmv_ch(ib,ich,'.')
+      ich = ich + ib2as(it(4),ib,ich,o'41002')
+      ich = ichmv_ch(ib,ich,':')
+      ich = ich + ib2as(it(3),ib,ich,o'41002')
+      ich = ichmv_ch(ib,ich,':')
+      ich = ich + ib2as(it(2),ib,ich,o'41002')
+      ich = ichmv_ch(ib,ich,'.')
+      ich = ich + ib2as(it(1),ib,ich,o'41002')
+      ich = ich - 1
 370   call put_cons(ib,ich)
 C
 C  5.  Restore schedule to original position.
