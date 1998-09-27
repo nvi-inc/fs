6ec401bb35693be3295fd64cda05c873edcb5ff9
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
1998-09-27 00:00:00 +0000

    various changes
    1998-09-22
    generalize for all log formats
    
    1998-09-27
    add year to "st" time
    adjust header
    add iyear to lxcom

--- logex/readl.f
+++ logex/readl.f
@@ -10,7 +10,7 @@ C    820818   KNM  COMMENTS ARE WRITTEN OUT BY CALLING LXWRT
 C 
 C INPUT VARIABLES:
 C 
-      integer*2 lnewso(4), lnewsn 
+      integer*2 lnewso(8), lnewsn 
 C       - Contains new source name. 
 C 
 C    LSTID - Station identifier 
@@ -35,11 +35,13 @@ C
       integer ichcm_ch
       character cjchar
 C 
-      integer*2 ireg(2),iparm(2),ival(2)
+      integer*2 ireg(2),iparm(2),ival(2),ibuf2(256)
 C        - Registers for reading; parameters from GTPRM 
 C        - REG, PARM - two word variables equiv 
 C 
+      save oldformat
       equivalence (reg,ireg(1)),(parm,iparm(1)),(value,ival(1)) 
+      data oldformat/-1/
 C 
 C     ICHK - Indicates whether a CHEKR error has been encountered.
 C     IET - Indicates whether an stop time has been encountered.
@@ -54,7 +56,7 @@ C
 C  ***************************************************************
 C
 C
-      call ifill_ch(lsourn,1,8,' ')
+      call ifill_ch(lsourn,1,16,' ')
       call ifill_ch(lsouon,1,8,' ')
       call ifill_ch(lsft,1,6,' ')
       call ifill_ch(lsft,1,1,'-')
@@ -79,7 +81,7 @@ C
       itcntl=-1
       islew=-1
       ichk=-1
-      if (ichcm_ch(lnewsn,1,'$$').ne.0) call ichmv(lsourn,1,lnewso,1,8)
+      if (ichcm_ch(lnewsn,1,'$$').ne.0) call ichmv(lsourn,1,lnewso,1,16)
 C
 C  *************************************************************
 C
@@ -89,6 +91,7 @@ C  *************************************************************
 C
 C
 100   call lxget
+      call mvupper(ibuf2,1,ibuf,1,nchar)
       if (ilen.eq.-1) call ichmv(ltapen,1,ltapn,1,8)
       if (icode.eq.-1.or.lstend.eq.-1.or.ilen.eq.-1) goto 1200
 C
@@ -102,11 +105,42 @@ C
 C  ************************************************************
 C
 C
-      if (cjchar(ibuf,15).ne.'"') goto 300
+      if(logformat.ne.oldformat) then
+         if(logformat.eq.1) then
+            idyps=1
+            ihrps=4
+            imnps=6
+            iscps=8
+            ildch=10
+            ifrst=11
+         else if(logformat.eq.2) then
+            idyps=3
+            ihrps=6
+            imnps=8
+            iscps=10
+            ildch=14
+            ifrst=15
+         else if(logformat.eq.3) then
+C not Y10K compliant
+            idyps=6
+C not Y10K compliant
+            ihrps=10
+C not Y10K compliant
+            imnps=13
+C not Y10K compliant
+            iscps=16
+C not Y10K compliant
+            ildch=21
+C not Y10K compliant
+            ifrst=22
+         endif
+         oldformat=logformat
+      endif
+      if (cjchar(ibuf2,ifrst).ne.'"') goto 300
       call ifill_ch(jbuf,1,100,' ')
       call ifill(jbuf,1,1,o'42')
       call ichmv(jbuf,2,lstid,1,1)
-      call ichmv(jbuf,4,ibuf,1,ilen)
+      call ichmv(jbuf,4,ibuf2,1,ilen)
       ncom=ilen+3
       call lxwrt(jbuf,ncom)
       goto 100
@@ -121,25 +155,26 @@ C  *********************************************************
 C
 C
 300   continue
-      if (ichcm_ch(ibuf,15,'source=').ne.0) goto 350
-      ich = 22
-      call gtprm(ibuf,ich,nchar,0,parm,id)
+      if (ichcm_ch(ibuf2,ifrst,'SOURCE=').ne.0) goto 350
+      ich = ifrst+7
+      call gtprm(ibuf2,ich,nchar,0,parm,id)
 C
 C  Make sure there are some characters in the source name
 C  before trying to move them.  Accept a maximum of 8
 C  characters.
 C
       if (ichcm_ch(lnewsn,1,'$$').ne.0) goto 310
-      if (ich-23.le.0) goto 100
-      call ichmv(lnewso,1,ibuf,22,min0(8,ich-23))
-      call ichmv(lsourn,1,ibuf,22,min0(8,ich-23))
+      if (ich-(ifrst+8).le.0) goto 100
+      call ichmv(lnewso,1,ibuf2,ifrst+7,min0(16,ich-(ifrst+8)))
+      call ichmv(lsourn,1,ibuf2,ifrst+7,min0(16,ich-(ifrst+8)))
       call char2hol('  ',lnewsn,1,2)
       goto 100
-310   call ifill_ch(lnewso,1,8,' ')
-      if (ich-23.gt.0) call ichmv(lnewso,1,ibuf,22,min0(8,ich-23))
+310   call ifill_ch(lnewso,1,16,' ')
+      if (ich-(ifrst+8).gt.0)
+     &     call ichmv(lnewso,1,ibuf2,ifrst+7,min0(16,ich-(ifrst+8)))
       goto 1200
 C
-350   if (ichcm_ch(lsourn,1,'        ').eq.0) goto 100
+350   if (ichcm_ch(lsourn,1,'                ').eq.0) goto 100
 C
 C
 C  ************************************************************
@@ -149,11 +184,12 @@ C
 C  ************************************************************
 C
 C
-400   if (ichcm_ch(ibuf,15,'st=').ne.0) goto 500
-      isld=ias2b(ibuf,3,3)
-      islhr=ias2b(ibuf,6,2)
-      islmin=ias2b(ibuf,8,2)
-      islsec=ias2b(ibuf,10,2)
+400   if (ichcm_ch(ibuf2,ifrst,'ST=').ne.0) goto 500
+      isly=iyear
+      isld=ias2b(ibuf2,idyps,3)
+      islhr=ias2b(ibuf2,ihrps,2)
+      islmin=ias2b(ibuf2,imnps,2)
+      islsec=ias2b(ibuf2,iscps,2)
       ist=1
       goto 100
 C
@@ -165,12 +201,13 @@ C
 C  ************************************************************
 C
 C
-500   if (ichcm_ch(ibuf,15,'onsource/').ne.0) goto 600
-      if (ichcm_ch(ibuf,24,'tracking').ne.0) goto 510
+ 500  continue
+      if (ichcm_ch(ibuf2,ifrst,'ONSOURCE/').ne.0) goto 600
+      if (ichcm_ch(ibuf2,ifrst+9,'TRACKING').ne.0) goto 510
       call ichmv_ch(lstat,1,'O')
       goto 100
 510   if (islew.eq.1.or.iet.eq.1) goto 100
-      call ichmv_ch(lstat,1,'O')
+      call ichmv_ch(lstat,1,'o')
       islew=1
       goto 100
 C
@@ -182,9 +219,9 @@ C
 C  *************************************************************
 C
 C
-600   if (ichcm_ch(ibuf,15,'error ch').ne.0) goto 700
+600   if (ichcm_ch(ibuf2,ifrst,'ERROR CH').ne.0) goto 700
       if (ichk.eq.1) goto 100
-      call ichmv_ch(lstat,2,'E')
+      call ichmv_ch(lstat,2,'e')
       ichk=1
       goto 100
 C
@@ -196,11 +233,11 @@ C
 C  ************************************************************
 C
 C
-700   if (ichcm_ch(ibuf,15,'et').ne.0) goto 800
-      ield=ias2b(ibuf,3,3)
-      ielhr=ias2b(ibuf,6,2)
-      ielmin=ias2b(ibuf,8,2)
-      ielsec=ias2b(ibuf,10,2)
+700   if (ichcm_ch(ibuf2,ifrst,'ET').ne.0) goto 800
+      ield=ias2b(ibuf2,idyps,3)
+      ielhr=ias2b(ibuf2,ihrps,2)
+      ielmin=ias2b(ibuf2,imnps,2)
+      ielsec=ias2b(ibuf2,iscps,2)
       iet=1
       goto 100
 C
@@ -211,8 +248,8 @@ C
 C  ************************************************************
 C
 C
-800   if (ichcm_ch(ibuf,15,'label=').ne.0) goto 900
-      call ichmv(ltapen,1,ibuf,21,8)
+800   if (ichcm_ch(ibuf2,ifrst,'LABEL=').ne.0) goto 900
+      call ichmv(ltapen,1,ibuf2,ifrst+6,8)
       call ichmv(ltapn,1,ltapen,1,8)
       goto 100
 C
@@ -225,7 +262,7 @@ C
 C  ************************************************************
 C
 C
-900   if (ichcm_ch(ibuf,15,'unlod').ne.0) goto 1000
+900   if (ichcm_ch(ibuf2,ifrst,'UNLOD').ne.0) goto 1000
       call ichmv(ltapn,1,ltapen,1,8)
       call ifill_ch(ltapen,1,8,' ')
       call ifill_ch(ltapen,3,1,'-')
@@ -239,10 +276,10 @@ C
 C  ************************************************************
 C
 C
-1000  if (ichcm_ch(ibuf,15,'tape/').ne.0) goto 1100
-      nch=1
-      call gtprm(ibuf,nch,nchar,0,parm,ierr)
-      call gtprm(ibuf,nch,nchar,1,parm,ierr)
+1000  if (ichcm_ch(ibuf2,ifrst,'TAPE/').ne.0) goto 1100
+      nch=ifrst+5
+      call gtprm(ibuf2,nch,nchar,0,parm,ierr)
+      call gtprm(ibuf2,nch,nchar,1,parm,ierr)
       if (ist.eq.-1) lsft = iparm(1)
       if (iet.eq.1)  left = iparm(1)
       goto 100
@@ -255,13 +292,14 @@ C
 C  ************************************************************
 C
 C
-1100  if (cjchar(ibuf,15).ne.'!') goto 100
+1100  continue
+      if (cjchar(ibuf2,ifrst).ne.'!') goto 100
       if (ist.eq.1) goto 100
-      inum=ias2b(ibuf,16,3)
-      if (inum.eq.-32768) goto 100
-      ilrday=inum
-      ilrhrs=ias2b(ibuf,19,2)
-      ilrmin=ias2b(ibuf,21,2)
+      call gttim(ibuf2,ifrst+1,nchar,0,it1,it2,it3,ierr)
+      if(ierr.ne.0) goto 100
+      ilrday=mod(it1,1024)
+      ilrhrs=it2/60
+      ilrmin=mod(it2,60)
       itcntl=1
       goto 100
 C
