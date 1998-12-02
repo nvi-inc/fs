15e26a7576dc5980f3dd9494aa5cb3ea90023311
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
1998-12-02 00:00:00 +0000

    various changes
    1997-12-12
    new LO command
    
    1998-10-19
    correctly initialize ip[2]=0
    
    1998-12-02
    removed erronous clear of command class number

---
+++ quikv/lo.c
@@ -0,0 +1,67 @@
+/* lo snap command */
+
+#include <stdio.h> 
+#include <string.h> 
+#include <sys/types.h>
+
+#include "../include/params.h"
+#include "../include/fs_types.h"
+#include "../include/fscom.h"         /* shared memory definition */
+#include "../include/shm_addr.h"      /* shared memory pointer */
+
+void lo(command,itask,ip)
+struct cmd_ds *command;                /* parsed command structure */
+int itask;
+long ip[5];                           /* ipc parameters */
+{
+      int ilast, ierr, ichold, i, count;
+      int verr;
+      char *ptr;
+      struct lo_cmd lcl;
+
+      int lo_dec();                 /* parsing utilities */
+      char *arg_next();
+
+      void lo_dis();
+
+      void skd_run(), skd_par();      /* program scheduling utilities */
+
+      if (command->equal != '=') {           /* read module */
+	  lo_dis(command,ip);
+	  return;
+      } else if (command->argv[0]==NULL)
+	goto parse;  /* simple equals */
+      else if (command->argv[1]==NULL) /* special cases */
+        if (*command->argv[0]=='?') {
+          lo_dis(command,ip);
+          return;
+	}
+    
+/* if we get this far it is a set-up command so parse it */
+
+parse:
+      ilast=0;                                      /* last argv examined */
+      memcpy(&lcl,&shm_addr->lo,sizeof(lcl));
+      count=1;
+      while( count>= 0) {
+        ptr=arg_next(command,&ilast);
+        ierr=lo_dec(&lcl,&count, ptr);
+        if(ierr !=0 ) goto error;
+      }
+
+/* all parameters parsed okay, update common */
+
+      memcpy(&shm_addr->lo,&lcl,sizeof(lcl));
+
+      ip[0]=ip[1]=ip[2]=ierr=0;
+
+      lo_dis(command,ip);
+      return;
+
+error:
+      ip[0]=0;
+      ip[1]=0;
+      ip[2]=ierr;
+      memcpy(ip+3,"q*",2);
+      return;
+}
