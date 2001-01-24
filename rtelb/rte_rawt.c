3091a0f63de902d8abce8825059f0a2e4429fac7
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
2001-01-24 00:00:00 +0000

    add NTP time option

--- rtelb/rte_rawt.c
+++ rtelb/rte_rawt.c
@@ -2,14 +2,30 @@
 
 #include <sys/types.h>
 #include <sys/times.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#include "../include/params.h"
+#include "../include/fs_types.h"
+#include "../include/fscom.h"
+#include "../include/shm_addr.h"
 
 void rte_rawt(lRawTime)
 long *lRawTime;
 {
      long times();
      struct tms buffer;
+     struct timeval tv;
 
-     *lRawTime=times(&buffer);
+     if(shm_addr->time.model!='c')
+       *lRawTime=times(&buffer);
+     else {
+       if(0!= gettimeofday(&tv, NULL)) {
+	 perror("getting timeofday, fatal\n");
+	 exit(-1);
+       }
+       *lRawTime=(tv.tv_sec-shm_addr->time.secs_off)*100+tv.tv_usec/10000;
+     }
 
      return;
 }
