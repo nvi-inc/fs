7894bb5d68e74137c8d5854dd5694428605d6c47
Ed Himwich <ed.himwich@nviinc.com>
2001-02-02 00:00:00 +0000

    various changes
    2001-01-11 Ray Gonzalez
    created and modified to automatically adjusting attenuators.
    
    2001-02-02 Ed Himwich
    default if attenuators
    
    Co-authored-by: Ray Gonzalez <>

--- quikr/ifd.f
+++ quikr/ifd.f
@@ -21,6 +21,7 @@ C        NCHAR  - number of characters in buffer
 C        ICH    - character counter
       integer*2 IBUF(20)                      ! Class buffer
       integer get_buf,ichcm_ch
+      logical kdef(2),kfirst
       character cjchar
       dimension inp(2)                        ! input indices for if1 & if2
       dimension iat(2)                        ! if attenuator settings
@@ -39,6 +40,7 @@ C
 C     1. If we have a class buffer, then we are to set the IFD. 
 C     If no class buffer, we have been requested to read the IFD. 
 C 
+      kfirst=.true.
       ichold = -99
       iclcm = ip(1) 
       if (iclcm.eq.0) then
@@ -55,7 +57,7 @@ C
       if (cjchar(ibuf,ieq+1).eq.'?') then
         ip(1) = 0
         ip(4) = o'77'
-        call ifdis(ip,iclcm)
+        call ifdis(ip,iclcm,kfirst)
         return
       endif
 C 
@@ -67,7 +69,7 @@ C     2. Step through buffer getting each parameter and decoding it.
 C     Command from user has these parameters: 
 C                   IFD=<atten1>,<atten2>,<input1>,<input2> 
 C     Choices are <input>: NOR, ALT.  Default NOR.
-C                 <atten>: attenuator setting, 0 to 63db. Default 0.
+C                 <atten>: attenuator setting, 0 to 63db. Default current.
 C                          If number is signed, interpret as a change to
 C                          the present attenuator setting.
 C                          IF atten is MAX, go to 63 and remember old value.
@@ -77,10 +79,13 @@ C     2.1 ATTEN1 AND ATTEN2, PARAMETERS 1 AND 2
 C
       ich = 1+ieq
       do i=1,2
+        kdef(i)=.false.
         ist = ich
         call gtprm2(ibuf,ich,nchar,0,parm,ierr)
         if(ichcm_ch(iparm,1,'max').eq.0) then
+          call fs_get_iat1if(iat1if)
           if(i.eq.1) iold(1)=iat1if
+          call fs_get_iat2if(iat2if)
           if(i.eq.2) iold(2)=iat2if
           iat(i)=63
         else if(ichcm_ch(iparm,1,'old').eq.0) then
@@ -90,22 +95,48 @@ C
           ich=ist
           call gtprm2(ibuf,ich,nchar,1,parm,ierr)
           if (ierr.eq.2) then
-            iat(i) = 0                          ! default
+            kdef(i)=.true.     ! default
+            iat(i)=0
           else if (ierr.eq.1) then
+            call fs_get_iat1if(iat1if)
             if (i.eq.1) iat(i) = iat1if
+            call fs_get_iat2if(iat2if)
             if (i.eq.2) iat(i) = iat2if
-        else if (iparm(1).lt.0.or.iscn_ch(ibuf,ist,ich-1,'+').ne.0) then
+          else if (iparm(1).lt.0.or.iscn_ch(ibuf,ist,ich-1,'+').ne.0)
+     $           then
+            call fs_get_iat1if(iat1if)
             if (i.eq.1) iat(i) = iat1if + iparm(1)
+            call fs_get_iat2if(iat2if)
             if (i.eq.2) iat(i) = iat2if + iparm(1)
           else
             iat(i) = iparm(1)
           endif
-          if (iat(i).lt.0.or.iat(i).gt.63) then
-            ierr = -200-i
-            goto 990
-          endif
+        endif
+        if (iat(i).lt.0.or.iat(i).gt.63) then
+           ierr = -200-i
+           goto 990
         endif
       enddo
+      call fs_get_ifd_set(ifd_set)
+      ifd_set_save=ifd_set
+      if((kdef(1).or.kdef(2)).and.ifd_set.ne.1) then
+         call get_att(iat1,iat2,ip)
+         if(ip(3).lt.0) then
+            ierr=-300
+            goto 990
+         endif
+         if(kdef(1)) iat(1)=iat1
+         if(kdef(2)) iat(2)=iat2
+      else
+         if(kdef(1)) then
+            call fs_get_iat1if(iat1if)
+            iat(1) = iat1if
+         endif
+         if(kdef(2)) then
+            call fs_get_iat2if(iat2if)
+            iat(2) = iat2if
+         endif
+      endif
 C
 C     2.2 INPUT1 AND INPUT2 - PARAMETERS 3 AND 4
 C 
@@ -142,13 +173,17 @@ C
       icheck(16)=0
       call fs_set_icheck(icheck(16),16)
       iat1if = iat(1)
+      call fs_set_iat1if(iat1if)
       iat2if = iat(2)
+      call fs_set_iat2if(iat2if)
       inp1if = inp(1)
       call fs_set_inp1if(inp1if)
       inp2if = inp(2)
       call fs_set_inp2if(inp2if)
       iol1if_fs = iold(1)
       iol2if_fs = iold(2)
+      ifd_set=1
+      call fs_set_ifd_set(ifd_set)
 C
       iclass=0
       nch = 12
@@ -206,7 +241,17 @@ C
         icheck(16)=mod(ichold,1000)+1
         call fs_set_icheck(icheck(16),16)
       endif
-      call ifdis(ip,iclcm)
+      call ifdis(ip,iclcm,kfirst)
+      if(kfirst.and.ieq.ne.0.and.(kdef(1).or.kdef(2))) then
+         kfirst=.false.
+         if(ip(1).ne.0) call clrcl(ip(1))
+         if(ifd_set_save.ne.1) then
+            call logit7ci(0,0,0,0,-301,'qi',0)
+         else
+            call logit7ci(0,0,0,0,-302,'qi',0)
+         endif
+         goto 500
+      endif
       return
 C
 990   ip(1) = 0
