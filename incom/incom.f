f922e102d807e15a01c944244a99135cd558289a
Ray Gonzalez <>
2001-05-29 00:00:00 +0000

    modified return conditions. error returns from control file are returned and handled properly. ddout and fserr were pushed to the top of execute list.

--- incom/incom.f
+++ incom/incom.f
@@ -1,10 +1,17 @@
       program incom
       implicit none
 c
+c rdg 010529 added ip and changed the call to sincom.
+c            also added conditional code to errors.
+c
       include '../include/fscom.i'
 c
+      integer*4 ip(5)
+
+      ip(3)=0
       call setup_fscom
-      call sincom
+      call sincom(ip)
+      if(ip(3).ne.0) call fc_exit(-1)
       call write_fscom
 c
       end
