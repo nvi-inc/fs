5f94b5f1fc17d110617c97db294aabcaba50abcd
Ed Himwich <ed.himwich@nviinc.com>
2002-03-21 00:00:00 +0000

    created

---
+++ poclb/logitn.c
@@ -0,0 +1,75 @@
+/* logitn
+
+   logitn formats message and errors for logging and sends the
+   buffer to ddout. It is just like logit.c, but it handles numeric what
+*/
+
+#include <string.h>
+#include <stdio.h>
+#include "../include/fs_types.h"
+#include "../include/shm_addr.h"      /* shared memory pointer */
+#include "../include/params.h"
+#include "../include/fscom.h"
+
+void cls_snd();
+void pname();
+void rte_time();
+
+logitn(msg,ierr,who, what)
+char *msg;           /* a message to be logged, NULL if none */
+int ierr;            /* error number, 0 if no error          */
+char *who;           /* 2-char string identifying the error  */
+int  what;          /* int with more info                   */
+
+{
+  char buf[513];    /* Holds the complete log entry */
+  char name[5];     /* The name of our main program */
+  int it[6],ip1,ip2,l;
+ 
+/* First get the time and put dddhhmmss into the log entry.
+*/
+  rte_time(it,&it[5]);
+  buf[0]='\0';
+  int2str(buf,it[5],-4,1);
+  strcat(buf,".");
+  int2str(buf,it[4],-3,1);
+  strcat(buf,".");
+  int2str(buf,it[3],-2,1);
+  strcat(buf,":");
+  int2str(buf,it[2],-2,1);
+  strcat(buf,":");
+  int2str(buf,it[1],-2,1);
+  strcat(buf,".");
+  int2str(buf,it[0],-2,1);
+
+/* For error messages, put ?ERROR xx (nn) into the log entry.
+*/
+  if (ierr != 0) {
+    strcat(buf,"?ERROR ");
+    strncat(buf,who,2);
+    int2str(buf,ierr,-5,0);
+    strcat(buf,"(");
+    int2str(buf,what,4,0);
+    strcat(buf,")");
+  }
+/* Get the name of our main program and append #pname# to the
+   log entry. Then append the message.
+*/
+  else {
+    pname(name);
+    strcat(buf,"#");
+    l=strlen(buf);
+    memcpy(buf+l,name,5);
+    buf[l+5]='\0';
+    strcat(buf,"#");
+    strcat(buf,msg);
+  }
+/* Send the complete log entry to ddout via class.
+*/
+  memcpy(&ip1,"fs",2);
+  memcpy(&ip2,"  ",2);
+  if (ierr != 0) memcpy(&ip2,"b1",2);
+/* for testing, send to output PLUS class */
+/*  fprintf(stdout,"%s\n",buf); */
+  cls_snd(&shm_addr->iclbox,buf,strlen(buf),ip1,ip2);
+}
---
+++ quikv/rollform_dis.c
@@ -0,0 +1,45 @@
+/* mark IV rollform function display */
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+
+#include "../include/params.h"
+#include "../include/fs_types.h"
+#include "../include/fscom.h"
+#include "../include/shm_addr.h"
+
+#define MAX_OUT 256
+
+void rollform_dis(command,lclc,ip)
+struct cmd_ds *command;
+struct form4_cmd *lclc;
+long ip[5];
+{
+      int i, count, start_len;
+      char output[MAX_OUT];
+
+      for (i=0;i<5;i++)
+	ip[i]=0;
+
+   /* format output buffer */
+
+      strcpy(output,command->name);
+      strcat(output,"/");
+      start_len=strlen(output);
+
+      count=0;
+      while( count>= 0) {
+	count++;
+        rollform_enc(output,&count,lclc);
+	if(count>=0) {
+	  cls_snd(&ip[0],output,strlen(output),0,0);
+	  ip[1]++;
+	  output[start_len]='\0';
+	}
+      }
+
+      return;
+
+}
+
