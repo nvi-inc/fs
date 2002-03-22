41c1069b33ba1a598d96648dcfde342f9909114d
Ed Himwich <ed.himwich@nviinc.com>
2002-03-22 00:00:00 +0000

    various changes
    2002-03-13
    modified to hold roll table(s)
    
    2002-03-22
    updated for new firmware and rolling

--- include/form4_ds.h
+++ include/form4_ds.h
@@ -5,11 +5,17 @@ struct form4_cmd {
   int rate;         /* (1<<rate)*0.125 MHz, 0<=rate<=8 */
   unsigned long enable[2]; /*track enables 1=enable, 0=off */
   unsigned aux[2];   /* aux data */
-  int codes[64];      /* sampler pin codes for output tracks */
+  int codes[64];     /* sampler pin codes for output tracks */
+  int bits;          /* 1 or 2 for number of bits being used */
   int fan;           /* fan-in, fan-out mode */
-  int barrel;        /* barrel-rolling mode */
-  int last;         /* =1 if form4 was last, 0 if trackform */
+  int barrel;        /* barrel-rolling mode, 0=off, 1=8, 2=16, 3=m */
+  int modulate;     /* 0=no modulation, 1=modulation on */
+  int last;         /* =1 if form was last, 0 if trackform */
   int synch;        /* synch on 0...16 or off=-1, pass=-2 fail=-3 */
+  int roll[16][64]; /* table of arbitrary barrel-rolls */
+  int start_map;
+  int end_map;
+  int a2d[16][64];  /* table of a2d assignment */
 };
 
 struct form4_mon {
