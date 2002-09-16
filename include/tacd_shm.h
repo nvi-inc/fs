bf2d5ec4eaaeb6e0da133ef5fbc48e27b5485619
Ray Gonzalez <>
2002-09-16 00:00:00 +0000

    removed parse_it, and incorporated code into tacd_srv.c allow for code changes in tacd.c. introduced code for reading TAC PC code version.

--- include/tacd_shm.h
+++ include/tacd_shm.h
@@ -1,36 +1,51 @@
 /* TAC shared memory (C data structure) layout */
 
 struct tacd_shm {
-  float day_frac;          /* The day and fraction of day. */
-  float msec_counter;      /* Raw reading on the counter in microsec. */
-  float usec_bias;         /* Amount by which the GPS receiver's 1PPS output
+  /* TIME */
+  int day,                 /* The date for time */
+    day_frac;            /* The fraction of day for time  */
+  float msec_counter,      /* Raw reading on the counter in microsec. */
+    usec_correction,       /* On the ONCORE receiver's +/- 52 sec sawtooth*/ 
+    usec_bias,             /* Amount by which the GPS receiver's 1PPS output
                             * has been intentionally biased in usec. */
-  float cooked_correction; /* The correct (cooked) counter reading after
+    cooked_correction,     /* The correct (cooked) counter reading after
                             * applying the usec_correction and usec_bias. */
-  float rms;               /* RMS scatter of the sec data points about the 
+    pc_v_utc, 
+    utc_correction_nsec;
+  int utc_correction_sec; 
+
+  /* AVERAGE */
+  int day_a,               /* The date for average. */
+    day_frac_a;          /* The fraction of day for average. */
+  float rms,               /* RMS scatter of the sec data points about the 
                             * average in usec. */
-  float usec_average;      /* sec point average of the sec cooked counter
+    usec_average,          /* sec point average of the sec cooked counter
                             * readings in usec. */
-  float max;               /* The extreme readings of the sec points.*/
-  float min;               /* (by definition) max > average > min. */ 
-  float day_frac_old;      /* old day and fraction of day value. */
+    max,                   /* The extreme readings of the sec points.*/
+    min;                   /* (by definition) max > average > min. */ 
+
+  int day_frac_old,        /* old day */
+    day_frac_old_a;        /* old day average. */
+
   /* */
-  int continuous;          /* 0 = not so often, 1 = continuous */
-  int stop_request;        /* -1=file empty,0=ok,1=reopen file,2=fs exit*/
-  int usec_correction;     /* On the ONCORE receiver's +/- 52 sec sawtooth 
-                            * correction on the GPS 1PPS in nsec. */
-  int nsec_accuracy;       /* An estimate of the accuracy of the GPS tick in 
+  int continuous,          /* 0 = not so often, 1 = continuous */
+    nsec_accuracy,         /* An estimate of the accuracy of the GPS tick in 
                             * nsec. */
-  int sec_average;         /* Number of secs. of data going into the averg. */
-  int port;                /* The port number assiged by the Sys. Admin. on
+    sec_average,           /* Number of secs. of data going into the averg. */
+    stop_request,          /* -1=file empty,0=ok,1=reopen file,2=fs exit*/
+    port,                  /* The port number assiged by the Sys. Admin. on
                             * the PC side. */
-  int check;               /* read TAC every n secs into SharedMemory */
-  int display;             /* what do you want displayed when you do a tacd.
+    check,                 /* read TAC every n secs into SharedMemory */
+    display;               /* what do you want displayed when you do a tacd.
 			    * tacd=status(0), tacd=time(1), tacd=average(2)*/
   /* */
-  char hostpc[80];         /* The Host name. */
-  char oldnew[4];              /* show old or new. -1 is old, 1 is new. */
-  char file[40];           /* Log file in the PC doing the work. */
-  char status[8];          /* The file status on the PC log file. */
+  char hostpc[80],         /* The Host name. */
+    oldnew[8],             /* show old or new. -1 is old, 1 is new. */
+    oldnew_a[11],          /* show old or new. -1 is old, 1 is new. */
+    file[40],              /* Log file in the PC doing the work. */
+    status[8],             /* The file status on the PC log file. */
+    tac_ver[20];           /* TAC PC software version.. */
 };
+
+
 
--- tacd/tacd_srv.c
+++ tacd/tacd_srv.c
@@ -16,28 +16,28 @@
                                        * structure) layout */
 #include "../include/shm_addr.h"      /* declaration of pointer to fscom */
 
-char *cmd[4] = {"$PCNSL,TICLOG,STATUS\r\n",       /* TAC filename and status */
+char *cmd[5] = {"$PCNSL,TICLOG,STATUS\r\n",       /* TAC filename and status */
 		"$PCNSL,TICDATA,TIME,ONCE\r\n",   /* TAC time */
 		"$PCNSL,TICDATA,AVERAGE,ONCE\r\n",/* TAC averages */
+		"$PCNSL,VERSION\r\n",             /* TAC version # */
 		"$PCNSL,EXIT\r\n"};               /* TAC exit */
-
 /*********************************************************************
  *  This program creates a socket and initiates a connection.
  *  Some data are sent over the connection and then the socket
  *  is closed ending the connection.
  *********************************************************************/
 int
-tacd_srv(char host_name[], int *port_num)
+tacd_srv()
 {
   /*
    * Local variables
    */
   int sock;
-  int which_item,i,k;
+  int which_item,i,k,n;
   struct sockaddr_in server;
   struct hostent *hp, *gethostbyname();
   char buf[1024];
-
+  
   /* Create the socket for reading */
   sock = socket( AF_INET, SOCK_STREAM, 0);
   if(sock == -1) {
@@ -49,7 +49,7 @@ tacd_srv(char host_name[], int *port_num)
    * the specified host.
    */
   server.sin_family = AF_INET;
-  hp = gethostbyname(host_name);
+  hp = gethostbyname(shm_addr->tacd.hostpc);
   
   if(hp == (struct hostent *) 0) {
     logit(NULL,-3,"ta");
@@ -57,7 +57,7 @@ tacd_srv(char host_name[], int *port_num)
   }
   
   memcpy((char *)&server.sin_addr, (char *)hp->h_addr, hp->h_length);
-  server.sin_port = htons((int)port_num);
+  server.sin_port = htons((int)shm_addr->tacd.port);
 
   if(connect(sock, (struct sockaddr *)&server, sizeof server) == -1) {
     logit(NULL,-4,"ta");
@@ -75,7 +75,7 @@ tacd_srv(char host_name[], int *port_num)
     return(-1);
   }
   /* Begin writing and reading from the socket. */
-  for(which_item = 0; which_item <= 3; which_item++) {
+  for(which_item = 0; which_item <= 4; which_item++) {
     k = strlen(cmd[which_item]);
     if(i=write(sock, cmd[which_item], k) == 0) {
       logit(NULL,-6,"ta");
@@ -90,10 +90,47 @@ tacd_srv(char host_name[], int *port_num)
     /* parse the string if the exit command not sent to TAC.
      * This will make a clean exit from TAC.
      */
-    if(which_item!=3) {
-      parse_it(buf,which_item);
+    if(which_item!=4) {
+      switch (which_item) {
+      case 0:
+	/*printf("%s",&buf[21]);*/
+	sscanf(&buf[21],"%s,%s",
+	       &shm_addr->tacd.file,
+	       &shm_addr->tacd.status);
+	break;
+      case 1:
+	/* printf("%s",buf);*/
+	  sscanf(&buf[20],"%d.%d,%f,%f,%d,%f,%f,%f,%d,%f",
+		 &shm_addr->tacd.day,
+		 &shm_addr->tacd.day_frac,
+		 &shm_addr->tacd.msec_counter,
+		 &shm_addr->tacd.usec_correction,
+		 &shm_addr->tacd.nsec_accuracy,
+		 &shm_addr->tacd.usec_bias,
+		 &shm_addr->tacd.cooked_correction,
+		 &shm_addr->tacd.pc_v_utc,
+		 &shm_addr->tacd.utc_correction_sec,
+		 &shm_addr->tacd.utc_correction_nsec);
+	break;
+      case 2:
+	/*printf("%s",&buf[23]);*/
+	sscanf(&buf[23],"%d.%d,%d,%f,%f,%f,%f",
+	       &shm_addr->tacd.day_a,
+	       &shm_addr->tacd.day_frac_a,
+	       &shm_addr->tacd.sec_average,
+	       &shm_addr->tacd.rms,
+	       &shm_addr->tacd.usec_average,
+	       &shm_addr->tacd.max,
+	       &shm_addr->tacd.min);
+	break;
+      case 3:
+	/*printf("%s",&buf[15]);*/
+	sscanf(&buf[15],"%s",
+	       &shm_addr->tacd.tac_ver);
+	break;
+      default:
+      }
     }
-    rte_sleep(100);
   }
   close(sock);
   return(0);
