6a5809fd1ec9c226b8abbb6c63191f12ec59751b
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
2003-04-29 00:00:00 +0000

    various changes
    2003-05-02 Calle Holmström
    incremental changes
    
    2003-05-02 Calle Holmström
    incremental changes
    
    2003-05-02 Calle Holmström
    incremental changes
    
    2003-05-02 Ed Himwich
    incremental changes
    
    2003-04-29 Ed Himwich
    incremental changes
    
    2003-04-29 Calle Holmström
    incremental changes
    
    Co-authored-by: Calle Holmström <>

--- gndat/gndat.c
+++ gndat/gndat.c
@@ -9,49 +9,77 @@
 
 #define STL(dest,source) min(sizeof((dest)),strlen((source))+1)
 
-#define MAXRX 6
+#define MAXRX MAX_RXGAIN
 #define MAXDETECTORS 32
-#define BADVALUE -600000000
+#define BADVALUE -6000000
 #define BADSTRVALUE "X"
 #define MAXLINE 256
 
-int min(int x, int y);
+int min(int x, int y); 
 
 int main(int argc, char *argv[])
 {
   char onoffFileName[64], *cptr;
-  char parsedFileName[64], parsedFileName2[64];
-  char rxgFileName[20];
+  char parsedFileName[64], tempFileName[64];
+  char rxgFileName[20], work[20];
   double azimuth, elevation, skyfreq, gainc, tsys, sefd, tcaljy, tcalk, calratio, tcal_ass;
   double flux_ass, tcalk_over_jy_ass, gcurve_ass, dpfu_gcurve_ass, LO;
   char onetime[22], time[22], source[11], dummy[31], detector[3], ifchan[2], polarization[2], sourcetype[2];
   char source_array[MAXDETECTORS][11], detector_array[MAXDETECTORS][3];
   char ifchan_array[MAXDETECTORS][2], polarization_array[MAXDETECTORS][2], sourcetype_array[MAXDETECTORS][2];
-  char namearray[MAXRX][20], typearray[MAXRX][6];
+  char namearray[MAXRX][265], typearray[MAXRX][6];
   double azimuth_array[MAXDETECTORS], elevation_array[MAXDETECTORS], skyfreq_array[MAXDETECTORS];
   double gainc_array[MAXDETECTORS], tsys_array[MAXDETECTORS], sefd_array[MAXDETECTORS];
   double tcaljy_array[MAXDETECTORS], tcalk_array[MAXDETECTORS], calratio_array[MAXDETECTORS];
   double tcal_ass_array[MAXDETECTORS], flux_ass_array[MAXDETECTORS], dpfu_ass_array[MAXDETECTORS];
   double gcurve_ass_array[MAXDETECTORS], dpfu_gcurve_ass_array[MAXDETECTORS], LO_array[MAXDETECTORS];
-  double LO1array[MAXRX], LO2array[MAXRX];
-  int detcount, valcount, i, right, firstapr, firstval, rxcount, error, work;
+  double trec_array[MAXDETECTORS], tatm_array[MAXDETECTORS], tspill_array[MAXDETECTORS], tau_array[MAXDETECTORS];
+  double LO1array[MAXRX], LO2array[MAXRX], tau0_array[MAXDETECTORS];
+  double a,b,c,elev,am;
+  int detcount, valcount, i, j, k, right, firstapr, firstval; 
+  int rxcount, error, icount, LOcount, works, somethingelse;
   char name[32], type[10];
   double LO1, LO2;
   FILE *onoffFile;
   FILE *parsedFile;
   FILE *rxgFile;
+  FILE *tempFile;
   char line[1000];     /* line buffer for fgets() */
   struct rxgain_ds rxgain[MAXRX];
+  char names[MAXRX][256];
+  int *ierr;
+  a=0.00125;
+  b=0.00291;
+  c=0.063;
   detcount=0;
   valcount=0;
   right=0;
   firstapr=1;
   firstval=1;
   rxcount=0;
+  LOcount=0;
+  somethingelse=1;
   strncpy(onoffFileName,argv[1],STL(onoffFileName,argv[1]));
   strncpy(parsedFileName,argv[2],STL(parsedFileName,argv[2]));
-  strncpy(parsedFileName2,argv[2],STL(parsedFileName2,argv[2]));
-  work = ((int) argv[3]);
+  strcpy(tempFileName,"/tmp/temp.txt");
+  if(argc >= 4 &&  1!=sscanf(argv[3],"%d",&works)) {
+    printf("Pid might be wrong. Please open the log file again.");
+    exit(0);
+  }
+  
+  if(argc >= 4)
+    cptr=argv[3];
+  else
+    cptr="";
+  
+  icount = get_rxgain_files(rxgain,names,&ierr,cptr)-1;
+  
+  if(icount < 0)
+    {
+      printf("No rxg files found\n");
+      exit(0);
+    }
+
   onoffFile = fopen(onoffFileName, "r");
   if(onoffFile == NULL)
     {
@@ -65,6 +93,10 @@ int main(int argc, char *argv[])
       printf("Error opening file: %s", parsedFileName);
       exit(0);
     }
+  tempFile = fopen(tempFileName, "w");
+  
+  fprintf(tempFile,"%-10s %-10s %-10s %-10s %-10s\n","dpfu","gain","tcalass","tcalk","skyfreq");
+  
   fputs("$ANTENNA\n", parsedFile);
   
   fputs("* LEADING STAR OR SOMETHING IS A COMMENT\n", parsedFile);
@@ -95,53 +127,14 @@ int main(int argc, char *argv[])
   fputs("Assumed DPFU*Gain Curve\n", parsedFile);
   fputs("LO\n", parsedFile);
   fputs("SourceType\n", parsedFile);
+  fputs("Tau\n", parsedFile);
+  /*fputs("Tau0\n", parsedFile);*/
   fputs("*\n", parsedFile);
   
   fputs("$DATA\n", parsedFile);
+
   while (fgets(line, MAXLINE, onoffFile) != NULL) {
-    if(strstr(line,"/calrx/") != NULL) {
-      if((NULL == (cptr = strtok(line," /,")) || (1!=sscanf(cptr, "%20s", time)))) {
-	strcpy(time,BADSTRVALUE);
-      }
-      if((NULL == (cptr = strtok(NULL," /,")) || (1!=sscanf(cptr, "%s", source)))) {
-	strcpy(source,BADSTRVALUE);
-      }
-      if((NULL == (cptr = strtok(NULL," /,")) || (1!=sscanf(cptr, "%s", name)))) {
-	strcpy(name,BADSTRVALUE);
-      }
-      if((NULL == (cptr = strtok(NULL," /,")) || (1!=sscanf(cptr, "%s", type)))) {
-	strcpy(type,BADSTRVALUE);
-      }
-      if(NULL == (cptr = strtok(NULL," /,")) || (1!=sscanf(cptr, "%lf", &LO1)) || LO1 <= 0) {
-	LO1 = BADVALUE;
-      }
-      if(NULL == (cptr = strtok(NULL," /,")) || (1!=sscanf(cptr, "%lf", &LO2)) || LO2 <= 0) {
-	LO2 = BADVALUE;
-      }
-      strncpy(namearray[rxcount],name,min(MAXDETECTORS,strlen(name))+1);
-      strncpy(typearray[rxcount],type,min(MAXDETECTORS,strlen(type))+1);
-      LO1array[rxcount] = LO1;
-      LO2array[rxcount] = LO2;
-
-      strcpy(rxgFileName,FS_ROOT);
-      strcat(rxgFileName,"/control/");
-      strcat(rxgFileName,namearray[rxcount]);
-      strcat(rxgFileName,".rxg");
-      if(work == 1) {
-	strcat(rxgFileName,".work");
-      }
-      rxgFile = fopen(rxgFileName, "r");
-      if(rxgFile == NULL)
-	{
-	  printf("-1\n");
-	  exit(0);
-	}
-      fclose(rxgFile);      
-      error = get_rxgain(rxgFileName, &rxgain[rxcount]);
-      rxcount++;
-
-    }
-    if(strstr(line,";onoff\n") != NULL) {
+    /*if(strstr(line,";onoff") != NULL) {
       for (i=0; i<=detcount; i++) {
 	strcpy(detector_array[i],"NULL");
       }
@@ -149,15 +142,23 @@ int main(int argc, char *argv[])
 	strcpy(onetime,BADSTRVALUE);
       }
       detcount=0;
-    }
+      }*/
     if(strstr(line,"APR") != NULL) {
-      if(firstapr==1) {
+      if(firstapr==1 || somethingelse==1) {
+	for (i=0; i<=detcount; i++) {
+	  strcpy(detector_array[i],"NULL");
+	}
+	if((NULL == (cptr = strtok(line," \t\n")) || (1!=sscanf(cptr, "%20s", onetime)))) {
+	  strcpy(onetime,BADSTRVALUE);
+	}
 	detcount=0;
 	firstapr=0;
 	firstval=1;
-      }
-      if((NULL == (cptr = strtok(line," \t\n")) || (1!=sscanf(cptr, "%20s", time)))) {
-	strcpy(time,BADSTRVALUE);
+	somethingelse=0;
+      } else {      
+	if((NULL == (cptr = strtok(line," \t\n")) || (1!=sscanf(cptr, "%20s", time)))) {
+	  strcpy(time,BADSTRVALUE);
+	}
       }
       if(NULL == (cptr = strtok(NULL," \t\n")) || (1!=sscanf(cptr, "%s", detector))) {
 	strcpy(detector,BADSTRVALUE);
@@ -186,6 +187,7 @@ int main(int argc, char *argv[])
       if(NULL == (cptr = strtok(NULL," \t\n")) || (1!=sscanf(cptr, "%s", sourcetype))) {
 	strcpy(sourcetype,BADSTRVALUE);
       }
+      
       strncpy(detector_array[detcount],detector,min(MAXDETECTORS,strlen(detector))+1);
       skyfreq_array[detcount] = skyfreq;
       tcal_ass_array[detcount] = tcal_ass;
@@ -194,7 +196,39 @@ int main(int argc, char *argv[])
       gcurve_ass_array[detcount] = gcurve_ass;
       dpfu_gcurve_ass_array[detcount] = dpfu_gcurve_ass;
       LO_array[detcount] = LO;
-      strncpy(sourcetype_array[detcount],sourcetype,min(MAXDETECTORS,strlen(sourcetype))+1);
+      strncpy(sourcetype_array[detcount],sourcetype,STL(MAXDETECTORS,sourcetype));
+      
+      for (i=0; i<=icount; i++) {
+	/*This allows "fuzzy" matching of LOs.*/
+	if(((rxgain[i].type=='f') && \
+	    ((LO>=(rxgain[i].lo[1] - rxgain[i].lo[1]/1000) && LO<=(rxgain[i].lo[1] + rxgain[i].lo[1]/1000)) || \
+	     (LO>=(rxgain[i].lo[0] - rxgain[i].lo[0]/1000) && LO<=(rxgain[i].lo[0] + rxgain[i].lo[0]/1000)))) || \
+	   ((rxgain[i].type=='r') && (skyfreq<=rxgain[i].lo[1] && skyfreq>=rxgain[i].lo[0]))) {
+	  k=0;
+	  for(j=0; j<=icount; j++) {
+	    if(strcmp(namearray[j],names[i])==0) {
+	      k=1;
+	      break;
+	    }
+	  }
+	  if(k==0) {
+	    strncpy(namearray[LOcount],names[i],STL(namearray[LOcount],names[i]));
+	    if(rxgain[i].type=='f')
+	      strncpy(typearray[LOcount],"fixed",STL(typearray[LOcount],"fixed"));
+	    if(rxgain[i].type=='r')
+	      strncpy(typearray[LOcount],"range",STL(typearray[LOcount],"range"));
+	    if(rxgain[i].lo[1]==-1) {
+	      LO1array[LOcount]=rxgain[i].lo[0];
+  	    } else { 
+	      LO1array[LOcount]=rxgain[i].lo[0];
+	      LO2array[LOcount]=rxgain[i].lo[1];
+  	    } 
+	    LOcount++;
+  	    break;
+  	  } 
+	}
+      }
+      
       detcount++;
     }
     if(strstr(line,"VAL") != NULL) {
@@ -239,12 +273,6 @@ int main(int argc, char *argv[])
       if(NULL == (cptr = strtok(NULL," \t\n")) || (1!=sscanf(cptr, "%lf", &tcaljy)) || tcaljy <= 0) {
 	tcaljy = BADVALUE;
       }
-      /*if(NULL == (cptr = strtok(NULL," \t\n")) || (1!=sscanf(cptr, "%lf", &tcalk)) || tcalk <= 0) {
-	tcalk = BADVALUE;
-      }
-      if(NULL == (cptr = strtok(NULL," \t\n")) || (1!=sscanf(cptr, "%lf", &calratio)) || calratio <= 0) {
-	calratio = BADVALUE;
-	}*/
       strncpy(source_array[valcount],source,min(MAXDETECTORS,strlen(source))+1);
       azimuth_array[valcount] = azimuth;
       elevation_array[valcount] = elevation;
@@ -252,30 +280,35 @@ int main(int argc, char *argv[])
       strncpy(polarization_array[valcount],polarization,min(MAXDETECTORS,strlen(polarization))+1);
       gainc_array[valcount] = gainc;
       tsys_array[valcount] = tsys;
+      trec_array[valcount] = 10;
+      tspill_array[valcount] = 20;
+      tatm_array[valcount] = 273;
+      if(tsys == BADVALUE || (tsys-trec_array[valcount]-tspill_array[valcount])/tatm_array[valcount] >= 1) {
+	tau_array[valcount] =  BADVALUE;
+      } else {
+	tau_array[valcount] = log(1/(1-(tsys-trec_array[valcount]-tspill_array[valcount])/tatm_array[valcount]));
+      }
       sefd_array[valcount] = sefd;
       tcaljy_array[valcount] = tcaljy;
-      /*tcalk_array[valcount] = tcalk;
-	calratio_array[valcount] = calratio;*/
+      
       for (i=0; i<=detcount; i++) {
 	if(strcmp(detector,detector_array[i])==0) {
 	  float fwhm,dpfu,gain,tcal;
-	  /*printf(" rxcount %d LO %f sky %f elev %f pol %c\n",
-	    rxcount,LO_array[i],skyfreq_array[i],elevation_array[valcount],polarization_array[valcount][0]);*/
-	  get_gain_par2(&rxgain,rxcount,LO_array[i],skyfreq_array[i],1.0,elevation_array[valcount],polarization_array[valcount][0],&fwhm,&dpfu,&gain,&tcal);
+	  /*printf("%d %f %f\n",rxcount,LO_array[i],skyfreq_array[i]);*/
+	  get_gain_par2(&rxgain,MAXRX,LO_array[i],skyfreq_array[i],1.0,elevation_array[valcount],polarization_array[valcount][0],&fwhm,&dpfu,&gain,&tcal);
 	  tcal_ass_array[i]=tcal;
 	  dpfu_ass_array[i]=dpfu;
 	  gcurve_ass_array[i]=gain;
+	  /*printf("Freq: %f LO: %f TCal: %f DPFU: %f Gain %f \n",skyfreq_array[i],LO_array[i],tcal,dpfu,gain);*/
 	  if(tcaljy_array[i]<=0 || tcal_ass_array[i]<=0) {
 	    tcalk_array[i]=BADVALUE;
-	    calratio_array[valcount]=BADVALUE;
+	    calratio_array[i]=BADVALUE;
 	  } else {
 	    tcalk_array[i]=dpfu*tcaljy_array[i]*gain;
-	    /*printf("%lf %lf %lf %lf\n",dpfu,tcal,fwhm,gain);*/
-	    calratio_array[valcount]=100*(tcalk_array[i]/tcal_ass_array[i]);
+	    calratio_array[i]=tcalk_array[i]/tcal_ass_array[i];
 	  }
 	  dpfu_gcurve_ass_array[i]=dpfu*gain;
 	  
-	  /*printf(" fwhm %f dpfu %f gain %f tcal %f\n",fwhm*RAD2DEG,dpfu,gain,tcal);*/
 	  fprintf(parsedFile, "%s ", onetime);
 	  fprintf(parsedFile, "%s ", source_array[valcount]);
 	  fprintf(parsedFile, "%.1f ", azimuth_array[valcount]);
@@ -284,12 +317,12 @@ int main(int argc, char *argv[])
 	  fprintf(parsedFile, "%s ", ifchan_array[valcount]);
 	  fprintf(parsedFile, "%s ", polarization_array[valcount]);
 	  fprintf(parsedFile, "%.1f ", skyfreq_array[i]);
-	  fprintf(parsedFile, "%.2f ", gainc_array[valcount]);
+	  fprintf(parsedFile, "%.4f ", gainc_array[valcount]);
 	  fprintf(parsedFile, "%.2f ", tsys_array[valcount]);
 	  fprintf(parsedFile, "%.1f ", sefd_array[valcount]);
 	  fprintf(parsedFile, "%.3f ", tcaljy_array[valcount]);
-	  fprintf(parsedFile, "%.3f ", tcalk_array[valcount]);
-	  fprintf(parsedFile, "%.2f ", calratio_array[valcount]);
+	  fprintf(parsedFile, "%.3f ", tcalk_array[i]);
+	  fprintf(parsedFile, "%.4f ", calratio_array[i]);
 	  if(tcaljy_array[i]<=0) {
 	    fprintf(parsedFile, "%.2f ", BADVALUE);
 	  } else {
@@ -301,12 +334,20 @@ int main(int argc, char *argv[])
 	  fprintf(parsedFile, "%.3f ", gcurve_ass_array[i]);
 	  fprintf(parsedFile, "%.4f ", dpfu_gcurve_ass_array[i]);
 	  fprintf(parsedFile, "%.1f ", LO_array[i]);
-	  fprintf(parsedFile, "%s\n ", sourcetype_array[i]);
+	  fprintf(parsedFile, "%s ", sourcetype_array[i]);
+	  fprintf(parsedFile, "%.4f\n", tau_array[valcount]);
+	  /*  elev=3.1415927/180*elevation_array[valcount]; */
+/*  	  am=(1/(1+a/(1+b/(1+c))))/(1/(sin(elev)+a/(sin(elev)+b/(sin(elev)+c)))); */
+/*  	  fprintf(parsedFile, "%.4f\n ", tau_array[valcount]/am); */
 	}
       }
       valcount++;
     }
+    if(strstr(line,"APR") == NULL && strstr(line,"VAL") == NULL) {
+      somethingelse=1;
+    }
   }
+
   fputs("*\n", parsedFile);
   
   fputs("$STATS\n", parsedFile);
@@ -322,8 +363,9 @@ int main(int argc, char *argv[])
   fputs("*\n", parsedFile);
 
   fputs("$LO\n", parsedFile);
-  for (i=0; i<rxcount; i++) {
-    if(LO2array[i] != BADVALUE) {
+
+  for (i=0; i<LOcount; i++) {
+    if(LO2array[i] > 0) {
       fprintf(parsedFile, "%s %s %.1f %.1f\n", namearray[i], typearray[i], LO1array[i], LO2array[i]);
     } else {
       fprintf(parsedFile, "%s %s %.1f\n", namearray[i], typearray[i], LO1array[i]);
@@ -331,6 +373,7 @@ int main(int argc, char *argv[])
   }
   fclose(onoffFile);
   fclose(parsedFile);
+  fclose(tempFile);
   printf("%s\n",parsedFileName);
   exit(0);
 }
@@ -344,6 +387,18 @@ int min(int x, int y) {
         return y; 
     } 
 } 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
