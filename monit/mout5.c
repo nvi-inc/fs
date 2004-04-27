0bb2f6ce32adf737b575995159192b4a29fd843d
Ed Himwich <ed.himwich@nviinc.com>
2004-04-27 00:00:00 +0000

    various changes
    2004-03-18
    create monit5
    
    2004-04-27
    round remaining seconds

---
+++ monit/mout5.c
@@ -0,0 +1,118 @@
+/*                                                                */
+/*  HISTORY:                                                      */
+/*  WHO  WHEN    WHAT                                             */
+/*  gag  920714  Added a check for Mark IV rack and drive to      */
+/*               to go along with Mark III rack and drive.        */
+/*  nrv  921027  Added check for special source names             */
+/*                                                                */
+#include <ncurses.h>
+#include <signal.h>
+#include <math.h>
+#include <string.h>
+#include <sys/types.h>
+#include "mparm.h"
+#include "dpi.h"
+#include "../include/params.h"
+#include "../include/fs_types.h"
+#include "../include/fscom.h"
+
+
+extern struct fscom *shm_addr;
+
+mout5()
+
+{
+  struct monit5_ping *ping;
+  char vsn[9];
+  int hours,mins;
+
+  ping=shm_addr->monit5.ping+(shm_addr->monit5.pong)%2;
+  
+  move(ROW1+1,COL1);
+  if (ping->active==0)
+    printw(">");
+  else
+    printw(" ");
+
+  move(ROW1+1,COL1+4);
+  printw("        ");
+  move(ROW1+1,COL1+4);
+  strncpy(vsn,ping->bank[0].vsn,8);
+  vsn[9]=0;
+  printw(vsn);
+
+  move(ROW1+1,COL1+12);
+  if(ping->bank[0].seconds>=0.0) {
+    hours=(ping->bank[0].seconds+0.5)/3600.0;
+    mins=((ping->bank[0].seconds+0.5)-hours*3600.0)/60.0;
+    printw("%5dh%.2dm",hours,mins);
+  } else
+    printw("         ");
+
+  move(ROW1+1,COL1+21);
+  if(ping->bank[0].gb>=0.0) {
+    printw("%10.3lf",ping->bank[0].gb);
+  } else
+    printw("          ");
+
+  move(ROW1+1,COL1+31);
+  if(ping->bank[0].percent>=0.0) {
+    printw("%6.1lf",ping->bank[0].percent);
+  } else
+    printw("      ");
+
+  move(ROW1+1,COL1+37);
+  if(ping->bank[0].itime[0]>=0) {
+    printw("%3d:%.2d:%.2d",
+	   ping->bank[0].itime[3],
+	   ping->bank[0].itime[2],
+	   ping->bank[0].itime[1]
+	   );
+  } else
+    printw("         ");
+
+  move(ROW1+2,COL1);
+  if (ping->active==1)
+    printw(">");
+  else
+    printw(" ");
+    
+  move(ROW1+2,COL1+4);
+  printw("        ");
+  move(ROW1+2,COL1+4);
+  strncpy(vsn,ping->bank[1].vsn,8);
+  vsn[9]=0;
+  printw(vsn);
+
+  move(ROW1+2,COL1+12);
+  if(ping->bank[1].seconds>=0) {
+    hours=(ping->bank[1].seconds+0.5)/3600.0;
+    mins=((ping->bank[1].seconds+0.5)-hours*3600.0)/60.0;
+    printw("%5dh%.2dm",hours,mins);
+  } else
+    printw("         ");
+
+  move(ROW1+2,COL1+21);
+  if(ping->bank[1].gb>=0.0) {
+    printw("%10.3lf",ping->bank[1].gb);
+  } else
+    printw("          ");
+
+  move(ROW1+2,COL1+31);
+  if(ping->bank[1].percent>=0.0) {
+    printw("%6.1lf",ping->bank[1].percent);
+  } else
+    printw("      ");
+
+  move(ROW1+2,COL1+37);
+  if(ping->bank[1].itime[0]>=0) {
+    printw("%3d:%.2d:%.2d",
+	   ping->bank[1].itime[3],
+	   ping->bank[1].itime[2],
+	   ping->bank[1].itime[1]
+	   );
+  } else
+    printw("         ");
+
+}  /* end mout2 */
+
