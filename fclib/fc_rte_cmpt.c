0e0bd0b99f4be6758680afa945a9f7d53136710d
Ed Himwich <ed.himwich@nviinc.com>
2004-06-08 00:00:00 +0000

    various changes
    2004-07-21 Mario Bérubé
    added
    
    2004-07-21 Mario Bérubé
    added
    
    2004-07-21 Mario Bérubé
    added
    
    2004-07-21 Mario Bérubé
    added
    
    2004-07-21 Mario Bérubé
    added
    
    2004-07-21 Mario Bérubé
    added
    
    2004-07-21 Mario Bérubé
    added
    
    2004-07-21 Mario Bérubé
    added
    
    2004-07-21 Mario Bérubé
    added
    
    2004-07-21 Mario Bérubé
    added
    
    2004-07-21 Mario Bérubé
    added
    
    2004-07-21 Mario Bérubé
    added
    
    2004-07-21 Mario Bérubé
    added
    
    2004-07-21 Mario Bérubé
    added
    
    2004-07-21 Mario Bérubé
    added
    
    2004-06-08 Ed Himwich
    added
    
    Co-authored-by: Mario Bérubé <>

---
+++ fclib/fc_rte_cmpt.c
@@ -0,0 +1,14 @@
+#include <sys/types.h>
+#include <sys/times.h>
+#include <time.h>
+
+void fc_rte_cmpt__(poClock,plCentiSec)
+time_t *poClock;
+long *plCentiSec;
+{
+     void rte_cmpt();
+
+     rte_cmpt(poClock,plCentiSec);
+
+     return;
+}
