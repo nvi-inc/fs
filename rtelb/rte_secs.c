b78693237f1b99de5c7ef3e49af3f5c14ad27d26
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
2004-06-23 00:00:00 +0000

    simplify

--- rtelb/rte_secs.c
+++ rtelb/rte_secs.c
@@ -11,14 +11,9 @@ long rte_secs()
      time_t clock1, clock2;
      long centisec;
 
-     clock1=0;
-     centisec=1;
-     clock2=1;
-     while(clock1!=clock2 && ((centisec%100) <2)) {
-	clock1=time(&clock1);       /* bracket the centi-seconds */
-        rte_rawt(&centisec);
-	clock2=time(&clock2);
-	rte_sleep((unsigned) 1);
-	}
+     rte_ticks(&centisec);
+     clock2=time(&clock2);
+
      return (clock2-(centisec/100));
+
 }
