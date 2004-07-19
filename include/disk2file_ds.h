3f4bc0965d6dfa8c604505b9ecd0976963f6de36
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
2004-07-19 00:00:00 +0000

    various changes
    2004-03-20
    created disk2file command
    
    2004-07-19
    change start and end to be floats

---
+++ include/disk2file_ds.h
@@ -0,0 +1,51 @@
+/* mk5 disk2file data structures */
+
+struct disk2file_cmd {
+  struct {
+    char scan_name[17];
+    struct m5state state;
+  } scan_name;
+  struct {
+    char destination[65];
+    struct m5state state;
+  } destination ;
+  struct {
+    float start;
+    struct m5state state;
+  } start ;
+  struct {
+    float end;
+    struct m5state state;
+  } end ;
+  struct {
+    char options[33];
+    struct m5state state;
+  } options ;  
+};
+
+struct disk2file_mon {
+  struct {
+    int scan_number;
+    struct m5state state;
+  } scan_number;
+  struct {
+    char option[33];
+    struct m5state state;
+  } option;
+  struct {
+    long long start_byte;
+    struct m5state state;
+  } start_byte;
+  struct {
+    long long end_byte;
+    struct m5state state;
+  } end_byte;
+  struct {
+    char status[65];
+    struct m5state state;
+  } status;
+  struct {
+    long long current;
+    struct m5state state;
+  } current ;
+};
