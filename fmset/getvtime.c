70ba97f649040519f7909d3bfa23eec662660217
Ed Himwich <ed.himwich@nviinc.com>
2004-07-28 00:00:00 +0000

    various changes
    2004-06-22
    trap schedule time-outs
    
    2004-07-28
    use "before" time for VLBA formatter as more accurate

--- fmset/getvtime.c
+++ fmset/getvtime.c
@@ -18,30 +18,43 @@ int    *fshs;
 time_t *formtime; /* formatter time received from mcbcn */
 int    *formhs;
 {
-	long centisec[2], centiavg, centidiff;
+	long centisec[6], centiavg, centidiff, hsdiff;
         int it[6];
 
         nsem_take("fsctl",0);
-        get_vtime(centisec,it,ip);
+        if(get_vtime(centisec,it,ip,1)!=0) {
+	  endwin();
+	  fprintf(stderr,"Field System not running - fmset aborting\n");
+	  exit(0);
+	}
         nsem_put("fsctl");
 	if( ip[2] != 1 )
 		{
 		endwin();
-		printf("Error %d from formatter\n",ip[2]);
+		fprintf(stderr,"Error %d from formatter\n",ip[2]);
                 logita(NULL,ip[2],ip+3,ip+4);
                 rte_sleep(SLEEP_TIME);
 		exit(0);
 		}
 
+	/* time before is more accurate */
+
+	centisec[1]=centisec[0];
+	centisec[3]=centisec[2];
+	centisec[5]=centisec[4];
+	
         centidiff =centisec[1]-centisec[0];
-        centiavg= centisec[0]+centidiff/2;
-
-        rte_cmpt(unixtime,&centiavg);
-        *unixhs=centiavg;
-
         centiavg= centisec[0]+centidiff/2;
         rte_fixt(fstime,&centiavg);
         *fshs=centiavg;
+
+	hsdiff=(centisec[3]-centisec[2])*100+centisec[5]-centisec[4];
+	*unixhs=centisec[4]+(hsdiff/2)/100;
+	*unixtime=centisec[2];
+	if(*unixhs>=100) {
+	  *unixtime+=*unixhs/100;
+	  *unixhs=*unixhs%100;
+	}
 
         rte2secs(it,formtime);
         *formhs=0;
