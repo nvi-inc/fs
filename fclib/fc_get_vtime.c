49a0180e195e7fa9412b4b58a7ed518641db821d
Ed Himwich <ed.himwich@nviinc.com>
2004-07-28 00:00:00 +0000

    various changes
    2004-06-22
    trap schedule time-outs
    
    2004-07-28
    updated to get ticks and computer time when checking VLBA time

--- fclib/fc_get_vtime.c
+++ fclib/fc_get_vtime.c
@@ -1,11 +1,10 @@
-void fc_get_vtime__(centisec,fm_tim,ip)
-long centisec[2];
+int fc_get_vtime__(centisec,fm_tim,ip,to)
+long centisec[6];
 int fm_tim[6];
 long ip[5];
+int *to;
 {
-  void get_vtime();
 
-  get_vtime(centisec,fm_tim,ip);
+  return get_vtime(centisec,fm_tim,ip,*to);
 
-  return;
 }
