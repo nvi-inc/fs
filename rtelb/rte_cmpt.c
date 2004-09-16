5011fe4143f181d9e7b72645ca895c3a8726001a
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
2004-09-16 00:00:00 +0000

    round time when it is available

--- rtelb/rte_cmpt.c
+++ rtelb/rte_cmpt.c
@@ -22,7 +22,11 @@ long *plCentiSec;
        exit(-1);
      }
      *poClock=tv.tv_sec;
-     *plCentiSec=tv.tv_usec/10000;
+     *plCentiSec=(tv.tv_usec+5000)/10000;
+     if(*plCentiSec>99) {
+       *plCentiSec-=100;
+       (*poClock)++;
+     }
 
      return;
 }
