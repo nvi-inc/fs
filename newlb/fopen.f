389f0f78b71ae91be1653316a72b95a93106b206
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
2006-04-27 00:00:00 +0000

    only set permissions if this is a new file

--- newlb/fopen.f
+++ newlb/fopen.f
@@ -13,15 +13,23 @@ C  IERR: Error value, returns negative if an error is detected
       integer IERR
       integer permissions
       integer ilen, trimlen
+      logical kexist
      
+      INQUIRE(FILE=filename,EXIST=kexist,IOSTAT=IERR)
+      IF (IERR.GT.0) THEN
+        IERR=-1                   !  Indicates that an error has occured
+        return
+      END IF
       OPEN(IDCB,FILE=filename,IOSTAT=IERR)
       IF (IERR.GT.0) THEN
         IERR=-1                   !  Indicates that an error has occured
         return
       END IF
-      permissions = o'0666'
-      ilen=trimlen(filename)
-      call fc_chmod(filename,permissions,ilen,ierr)
+      IF(.not.kexist) then
+         permissions = o'0666'
+         ilen=trimlen(filename)
+         call fc_chmod(filename,permissions,ilen,ierr)
+      endif
 
       return
       end
