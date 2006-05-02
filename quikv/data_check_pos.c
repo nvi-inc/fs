3e7559486affe7fd670e08e0dad64f83d0399ede
Ed Himwich <ed.himwich@nviinc.com>
2006-05-02 00:00:00 +0000

    various changes
    2006-04-26
    updated to make more general for mark5b
    
    2006-05-02
    added missing \n

--- quikv/data_check_pos.c
+++ quikv/data_check_pos.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "../include/m5state_ds.h"
-#include "../include/disk_pos_ds.h"
-
 #define BUFSIZE 512
 
 int data_check_pos(ip)
@@ -21,59 +18,13 @@ long ip[5];
   int save=0;    /* argument for cls_rcv - unused */
   int nchars;
   char *ptr;
-  struct disk_pos_mon lclm;
   long class, nrecs;
   int i;
 
   out_recs=0;
   out_class=0;
-  
-  strcpy(outbuf,"position?\n");
-  cls_snd(&out_class, outbuf, strlen(outbuf) , 0, 0);
-  out_recs++;
-  
-  ip[0]=1;
-  ip[1]=out_class;
-  ip[2]=out_recs;
-  skd_run("mk5cn",'w',ip);
-  skd_par(ip);
-  
-  if(ip[2]<0) {
-    if(ip[0]!=0) {
-      cls_clr(ip[0]);
-      ip[0]=ip[1]=0;
-    }
-    return -1;
-  }
-
-  class=ip[0];
-  nrecs=ip[1];
-
-  for (i=0;i<nrecs;i++) {
-    char *ptr;
-    if ((nchars =
-	 cls_rcv(class,inbuf,BUFSIZE,&rtn1,&rtn2,msgflg,save)) <= 0) {
-      ip[3] = -401;
-      goto error;
-    }
-    if(i==0)
-      if(0!=m5_2_disk_pos(inbuf,&lclm,ip)) {
-	cls_clr(class);
-	return -1;
-      }
-  }
-
-  pos=lclm.record.record-1e6;
-
-  if(pos<0) {
-    ierr=-402;
-    goto error;
-  }
-
-  out_recs=0;
-  out_class=0;
     
-  sprintf(outbuf,"play = off : %16.0lf\n",pos);
+  strcpy(outbuf,"scan_set = : -1000000 ;\n");
   cls_snd(&out_class, outbuf, strlen(outbuf) , 0, 0);
   out_recs++;
   
