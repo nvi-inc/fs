e97729f655ea0b82e7d365d3b229ec4a5617090f
Ed Himwich <ed.himwich@nviinc.com>
2012-11-01 00:00:00 +0000

    require confirmation before sync/time-set if 5b is recording

--- fmset/asksure.c
+++ fmset/asksure.c
@@ -2,6 +2,8 @@
 #include <sys/types.h>   /* data type definition header file */
 #include <time.h>
 
+#include "../include/params.h"
+
 #include "fmset.h"
 
 #define ROWA  ROW+6
@@ -11,11 +13,16 @@
 void rte2secs();
 
 extern char *form;
+extern int dbbc;
+extern int dbbc_sync;
+extern int rack;
 
-int asksure( maindisp)  /* ask if sure */
+int asksure( maindisp,m5rec,sync)  /* ask if sure */
 WINDOW	* maindisp;  /* main display WINDOW data structure pointer */
+int m5rec;
+int sync;
 {
-char	answer[4];
+  char	answer[4], answer2[4];
 int kfirst;
 int i,j;
 char buffer[80];
@@ -23,16 +30,51 @@ char buffer[80];
 nodelay ( maindisp, FALSE );
 echo ();
 
- sprintf(buffer, "Are you sure you want to re-synch the %9s (y/n) ?      ",
+ if( sync) {
+ sprintf(buffer,
+	 "Are you sure you want to sync the %9s (y/n) ?      ",
+       /* 0123456789012345678901234567890123456789012345678901234567890 */
 	 form);
  mvwprintw( maindisp, ROWA+1, COL0, buffer);
-	  /* 0123456789012345678901234567890123456789012345678901234567890 */
- mvwscanw(  maindisp, ROWA+1, COL0+56, "%1s", answer );
+ mvwscanw(  maindisp, ROWA+1, COL0+52, "%1s", answer );
+ }
+ if ( m5rec && (answer[0] == 'Y' || answer[0] == 'y' || !sync)) {
+   if(sync)
+     sprintf(buffer,
+	"The Mark5B is recording. It should not be sync'd while recording.");
+   else
+     sprintf(buffer,
+"The Mark5B is recording. It should not have its time set while recording.");
+   mvwprintw( maindisp, ROWA+3, COL0, buffer);
+   if (sync)
+   sprintf(buffer,
+    "Are you *REALLY* sure you want to sync (y/n) ?       ");
+  /* 0123456789012345678901234567890123456789012345678901234567890 */
+   else
+   sprintf(buffer,
+    "Are you *REALLY* sure you want to set the time (y/n) ?       ");
+  /* 0123456789012345678901234567890123456789012345678901234567890 */
+ mvwprintw( maindisp, ROWA+4, COL0, buffer);
+ if(sync)
+ mvwscanw(  maindisp, ROWA+4, COL0+47, "%1s", answer );
+ else
+ mvwscanw(  maindisp, ROWA+4, COL0+55, "%1s", answer );
+ }
+
+ if ( rack == DBBC && sync && (answer[0] == 'Y' || answer[0] == 'y') ) {
+ sprintf(buffer,
+	 "Do you also want to sync the DBBC first (recommended) (y/n) ?      ",
+       /* 0123456789012345678901234567890123456789012345678901234567890 */
+	 form);
+ mvwprintw( maindisp, ROWA+6, COL0, buffer);
+ mvwscanw(  maindisp, ROWA+6, COL0+60, "%1s", answer2 );
+ dbbc_sync= answer2[0] == 'Y' || answer2[0] == 'y';
+ }
 
  nodelay ( maindisp, TRUE );
  noecho ();
 
- for (i=0; i<2;i++)
+ for (i=0; i<7;i++)
    for(j=0;j<78-COL0;j++)
      mvwprintw(maindisp,ROWA+i,COL0+j," ");
 
