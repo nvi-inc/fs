e4ea6faa933e9319459bde9055d4824dc42e8358
Ed Himwich <ed.himwich@nviinc.com>
2012-11-06 00:00:00 +0000

    don't complain if metserver returned NULLS, that's its business to report

--- st.default/st-0.0.0/metclient/src/metclient.c
+++ st.default/st-0.0.0/metclient/src/metclient.c
@@ -2,6 +2,7 @@
 ** metclient.c -- a stream socket client for MET.
 */
 
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,9 +15,8 @@
 #include <sys/time.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <time.h>
 
-#define PORT 30384 /* the port client will be connecting to */
+#define PORT 50001 /* the port client will be connecting to */
 
 #define MAXLEN 90
 #define TRUE 1
@@ -32,23 +32,26 @@ int main(int argc, char *argv[])
   char log_str[MAXLEN];
   char temp_str[MAXLEN];
   char buff[MAXLEN];
-  int loopcnt,dwx;
+ int loopcnt,dwx;
   char fsloc_str[MAXLEN];
   float twx,pwx,hwx,swx;
   struct tm *ptr;
-  time_t t;
+  time_t t, tnew;
+  int diff,phase;
   struct hostent *he;
-  struct timeval to;
+  struct timeval to, tv;
   fd_set ready;
   struct sockaddr_in their_addr; /* connector's address information */ 
   FILE *fp;
+  unsigned int port;
 
+  errno=0;
 
-  if(argc!=2) {
-  param_err:
-    err_report("metclient: No valid file. Follow /usr2/st/metclient/INSTALL instructions", NULL,0,0);
-    sleep(10);
-    goto param_err;
+arg_err:
+  if(argc<2) {
+    err_report("No valid file. Follow INSTALL instructions", NULL,0,0);
+    sleep(60);
+    goto arg_err;
   }
 
  file_err:
@@ -56,36 +59,36 @@ int main(int argc, char *argv[])
   /* Opening met file. */
   if ((fp=fopen(argv[1],"r")) == 0) {
     /* If the file does not exist complain only once. */
-    err_report("metclient: metlog.ctl does not exist", NULL,0,0);
+    err_report("metlog.ctl does not exist", NULL,errno,0);
     fclose(fp);
-    sleep(10);
+    sleep(60);
     goto file_err;
   } else {
     /* Read the contents of the file then close  */
-    k=0;
+
     cnt=0;
-    while(fgets(buff,MAXLEN-1,fp) != NULL) {
-      if(buff[0]!='*') {
+    while(cnt <= 7 && fgets(buff,MAXLEN-1,fp) != NULL) {
+      if(buff[0]!='*' && buff[0]!= '\n') {
         /* station code,windsample,logging,logfile */
-        for(j=0; buff[j]!='\n'; j++) {
-          temp_str[j]=buff[j];
-          i++;
-          if(buff[0]=='\n') {
-	    err_report("metlog.ctl:Syntax error-commas NOT allowed as delimeters", NULL,0,0);
-	    fclose(fp);
-	    sleep(10);
-	    goto file_err;
-          }
-        }
-        sscanf(temp_str," %s ",&location_str[cnt]);
-	/*if you add a parameter to the control file add to the if statment.*/
-        if(k==8) break;
+        sscanf(buff," %s ",&location_str[cnt]);
         cnt++;
       }
-      k++;
+      /*if you add a parameter to the control file add to the if statment.*/
     }
   }
   fclose(fp);
+
+  if(argc<3)
+    port=PORT;
+  else
+    port=atoi(argv[2]);
+
+port_err:
+  if(port <= 0) {
+    err_report("invalid socket. Follow INSTALL instructions", NULL,0,0);
+    sleep(60);
+    goto port_err;
+  }
 
   /* Build the header string */
   sprintf(fsloc_str,":location,%s,%s,%s,%s",
@@ -101,79 +104,87 @@ int main(int argc, char *argv[])
   sleep(10);
 
   do {
+    if(0!=gettimeofday(&tv,NULL))
+       err_report("Error getting time",NULL,errno,0);
+    phase=tv.tv_sec%loopcnt;
+
+    if(phase > 0 || tv.tv_usec > 0)
+      usleep( (loopcnt-phase)*1000000L-tv.tv_usec);
+
+    if(0!=gettimeofday(&tv,NULL))
+       err_report("Error getting time",NULL,errno,0);
+    t=tv.tv_sec;
 
   hostname_err:
   if ((he=gethostbyname("localhost")) == NULL) {  /* get the host info */
-    err_report("metclient gethostbyname error", NULL,0,0);
-    sleep(10);
+    err_report("gethostbyname error", NULL,errno,0);
+    sleep(60);
     goto hostname_err;
   }
 
   if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-    err_report("metclient socket error", NULL,0,0);
-    sleep(10);
+    err_report("socket error", NULL,errno,0);
+    sleep(60);
     goto hostname_err;
   }
 
   their_addr.sin_family = AF_INET;    /* host byte order */
-  their_addr.sin_port = htons(PORT);  /* short, network byte order */
+  their_addr.sin_port = htons(port);  /* short, network byte order */
   their_addr.sin_addr = *((struct in_addr *)he->h_addr);
   memset(&(their_addr.sin_zero), '\0', 8);  /* zero the rest of the struct */
 
   /* Set socket nonblocking  */
   if ((flags = fcntl (sockfd, F_GETFL, 0)) < 0) 
-    err_report("metclient f_getfl error", NULL,0,0);
+    err_report("f_getfl error", NULL,errno,0);
   
   flags |= O_NONBLOCK; 
   
   if (( fcntl (sockfd, F_SETFL, flags )) < 0) 
-    err_report("metclient f_setfl error", NULL,0,0);
+    err_report("f_setfl error", NULL,errno,0);
   
-  to.tv_sec = 4;
+  to.tv_sec = 10;
   to.tv_usec = 0;
   if (connect(sockfd, (struct sockaddr *)&their_addr,
 	      sizeof(struct sockaddr)) < 0) {
     if(errno != EAGAIN && errno != EINPROGRESS) {
-      perror("connect");
-      err_report("metclient client connect error", NULL,0,0);
-      sleep(10);
+      err_report("client connect error", NULL,errno,0);
+      sleep(60);
       goto hostname_err;
     }
   } 
    
-  sleep(1);
   FD_ZERO(&ready);
   FD_SET(sockfd, &ready);
   
   select(sockfd+1, &ready, NULL, NULL, &to);
 
-  sleep(1);
+  twx=-51.0;
+  pwx=hwx=-1.;
+  swx=dwx=-1.;
   if ((numbytes=recv(sockfd, buf, 29, 0)) < 0) {
-    err_report("metclient recv error", NULL,0,0);
-    strcpy(buf,"Client Timed Out");
-    numbytes=34;
-  }
+    err_report("recv error, metserver not responding", NULL,errno,0);
+  } else {
+    int icount;
     buf[numbytes]='\0';
-
-    sscanf(buf,"%f,%f,%f,%f,%d",&twx,&pwx,&hwx,&swx,&dwx);
-    buff[0]='\0';
-    if(twx >= -50.0) sprintf(&buff[0],"%.1f",twx);
-    if(swx >= 0.0 || hwx >= 0.0 || pwx >= 0.0) strcpy(&buff[strlen(buff)],",");
-    if(pwx >= 0.0) sprintf(&buff[strlen(buff)],"%.1f",pwx);
-    if(swx >= 0.0 || hwx >= 0.0) strcat(buff,",");
-    if(hwx >= 0.0) sprintf(&buff[strlen(buff)],"%.1f",hwx);
-    if(swx >= 0.0) {
+    icount=sscanf(buf,"%f,%f,%f,%f,%d",&twx,&pwx,&hwx,&swx,&dwx);
+    buff[0]=0;
+    if(icount != 5)
+      err_report("error decoding met string", buf,0,0);
+    else {
+      if(twx >= -50.0)
+	sprintf(buff,"%.1f",twx);
       strcat(buff,",");
-      sprintf(&buff[strlen(buff)],"%.1f",swx);
-      strncat(buff,",",1);
-      sprintf(&buff[strlen(buff)],"%d",dwx);
+      if(pwx >= 0.0) sprintf(buff+strlen(buff),"%.1f",pwx);
+      strcat(buff,",");
+      if(hwx >= 0.0) sprintf(buff+strlen(buff),"%.1f",hwx);
+      if(swx >= 0.0) {
+	strcat(buff,",");
+	sprintf(buff+strlen(buff),"%.1f",swx);
+	strncat(buff,",",1);
+	sprintf(buff+strlen(buff),"%d",dwx);
+      }
     }
-
-    t=time(NULL);
-    if(((time_t) -1) == t) {
-      err_report("Error getting time in metclient",NULL,1,0);
-    }
-
+  }
     /* Year, Day, and UT time */
     ptr=gmtime(&t);
 
@@ -190,8 +201,6 @@ int main(int argc, char *argv[])
     logwx(loc_stamp,log_str,sn,logfile);
     close(sockfd);
 
-    /* Wait the requested time period. */
-    sleep(loopcnt-4);
   } while(TRUE);
   exit(0);
 }
