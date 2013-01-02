37e4c54d1a33fe7dd30e6cc93fc18f51c93ec852
Ed Himwich <ed.himwich@nviinc.com>
2013-01-02 00:00:00 +0000

    various changes
    2012-12-10
    adapted for continuous cal
    
    2013-01-02
    WEH fivpt/dbbcn.f
    130103 WEH clib/dbbcn_v.c
    fivpt/volts.f
    adjust Tsys samples if continuous cal device

---
+++ fclib/fc_dbbcn_v.c
@@ -0,0 +1,28 @@
+void fc_dbbcn_d__(device,ierr,ip)
+char device[2];
+int *ierr;
+long ip[5];
+{
+    void dbbcn_d();
+
+    dbbcn_d(device,ierr,ip);
+    return;
+}
+void fc_dbbcn_v__(dtpi,dtpi2,ip,icont,isamples)
+double *dtpi,*dtpi2;
+long ip[5];
+int *icont, *isamples;
+{
+    void dbbcn_v();
+
+    dbbcn_v(dtpi,dtpi2,ip,icont,isamples);
+    return;
+}
+void fc_dbbcn_r__(ip)
+long ip[5];
+{
+    void dbbcn_r();
+
+    dbbcn_r(ip);
+    return;
+}
