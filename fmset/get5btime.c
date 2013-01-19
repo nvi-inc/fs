6fc6312752e9242428a8dc7495f7e7abf7fc8835
Ed Himwich <ed.himwich@nviinc.com>
2013-01-19 00:00:00 +0000

    various changes
    2012-11-01
    sync DBBC if rack type and 5B
    
    2013-01-19
    increase delay between dbbc sync and 5B sync to improve reliability

--- fmset/get5btime.c
+++ fmset/get5btime.c
@@ -1,4 +1,4 @@
-/* getvtime.c - get vlba formatter time */
+/* get5btime.c - get vlba formatter time */
 
 #include <ncurses.h>      /* ETI curses standard I/O header file */
 #include <memory.h>      /* for memcpy */
@@ -11,7 +11,9 @@
 
 extern long ip[5];           /* parameters for fs communications */
 extern int synch;
-int rack, rack_type;
+extern int rack, rack_type;
+
+extern dbbc_sync;
 
 void rte2secs();
 
@@ -41,30 +43,67 @@ int sz_m5clock;
 	long out_class;
 
 	if(synch) {
+	  if(rack == DBBC && dbbc_sync) {
+	    dbbc_sync=0;
+	    out_recs=0;
+	    out_class=0;
+
+	    str="pps_sync";
+	    cls_snd(&out_class, str, strlen(str) , 0, 0);
+	    out_recs++;
+	    logit("DBBC sync command sent.",0,NULL);
+
+	    ip[0]=1;
+	    ip[1]=out_class;
+	    ip[2]=out_recs;
+	  
+	    nsem_take("fsctl",0);
+	    name="dbbcn";
+	    while(skd_run_to(name,'w',ip,120)==1) {
+	      if (nsem_test("fs   ") != 1) {
+		endwin();
+		fprintf(stderr,"Field System not running - fmset aborting\n");
+		exit(0);
+	      }
+	      name=NULL;
+	    }
+	    skd_par(ip);
+	    nsem_put("fsctl");
+	    if(ip[2] != 0)
+	      {
+		logita(NULL,ip[2],ip+3,ip+4);
+	      }
+	    if(ip[1]!=0)
+	      cls_clr(ip[0]);
+	    rte_sleep(226);  /*wait for 2nd next 1 PPS before continuing */
+	  }
+
 	  out_recs=0;
 	  out_class=0;
 
+	  str="1pps_source = vsi;\n";
+	  cls_snd(&out_class, str, strlen(str) , 0, 0);
+	  out_recs++;
+	  logit("Mark 5B 1pps_source command sent.",0,NULL);
+	    
 	  str="tvr = 0;\n";
 	  cls_snd(&out_class, str, strlen(str) , 0, 0);
 	  out_recs++;
 
 	  if((rack == VLBA4 && rack_type == VLBA45) ||
-	     (rack == MK4   && rack_type == MK45  )) {
+	     (rack == MK4   && rack_type == MK45  ) ||
+	     (rack == DBBC) ) {
 
-	    str="1pps_source = vsi;\n";
-	    cls_snd(&out_class, str, strlen(str) , 0, 0);
-	    out_recs++;
-	    
 	    str="clock_set = 32 : ext;\n";
 	    cls_snd(&out_class, str, strlen(str) , 0, 0);
 	    out_recs++;
-	    logit("Mark 5B 1pps_source and clock_set commands sent.",0,NULL);
+	    logit("Mark 5B clock_set command sent.",0,NULL);
 	  }
 	  str="dot_set= : force;\n";
 	  cls_snd(&out_class, str, strlen(str) , 0, 0);
 	  out_recs++;
 	     
-	  logit("Mark 5B re-synch command sent.",0,NULL);
+	  logit("Mark 5B sync command sent.",0,NULL);
 	  ip[0]=1;
 	  ip[1]=out_class;
 	  ip[2]=out_recs;
@@ -84,12 +123,13 @@ int sz_m5clock;
 	  if(ip[2] != 0)
 	    {
 	      endwin();
-	      fprintf(stderr,"Error %d from formatter\n",ip[2]);
+	      fprintf(stderr,"Error %d from Mark5B\n",ip[2]);
 	      logita(NULL,ip[2],ip+3,ip+4);
 	      rte_sleep(SLEEP_TIME);
 	      exit(0);
 	    }
-	  cls_clr(ip[0]);
+	  if(ip[1]!=0)
+	    cls_clr(ip[0]);
 	  synch=0;
 	}
 
@@ -105,7 +145,7 @@ int sz_m5clock;
 	if( ip[2] != 0 )
 		{
 		endwin();
-		fprintf(stderr,"Error %d from formatter\n",ip[2]);
+		fprintf(stderr,"Error %d from Mark5B\n",ip[2]);
                 logita(NULL,ip[2],ip+3,ip+4);
                 rte_sleep(SLEEP_TIME);
 		exit(0);
