6e75e8750901a91ace4c00405abf32c42b9b778e
Ed Himwich <ed.himwich@nviinc.com>
2014-01-29 00:00:00 +0000

    updated for FSL9

--- misc/rc.fs
+++ misc/rc.fs
@@ -1,4 +1,15 @@
 #!/bin/sh
+### BEGIN INIT INFO
+# Provides:          fs
+# Required-Start:    $remote_fs
+# Required-Stop:     $remote_fs
+# Default-Start:     S
+# Default-Stop:      
+# Short-Description: Allocate FS shared memory
+# Description:       Run /usr2/fs/bin/fsalloc and /usr2/st/bin/stalloc if they exist
+#                    to allocate the shared memory blocks used by the Field System.
+### END INIT INFO
+
 file=/usr2/fs/bin/fsalloc
 if [ -f ${file} ]
 then
