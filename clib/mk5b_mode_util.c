105966ae81261d73303ecfd905eccab392f4acd3
Ed Himwich <ed.himwich@nviinc.com>
2015-05-20 00:00:00 +0000

    rework mk5X_mode command to use better work with existing data

--- clib/mk5b_mode_util.c
+++ clib/mk5b_mode_util.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <strings.h>
 #include <sys/types.h>
 #include <limits.h>
 #include <math.h>
@@ -13,7 +14,7 @@
 #include "../include/fscom.h"         /* shared memory definition */
 #include "../include/shm_addr.h"      /* shared memory pointer */
 
-static char *source_key[ ]=         { "ext", "tvg","ramp" }; 
+static char *source_key[ ]=         { "ext", "tvg","ramp","vdif"}; 
 static char *disk_key[ ]=         { "disk_record_ok" }; 
 
 #define NSOURCE_KEY sizeof(source_key)/sizeof( char *)
@@ -21,14 +22,16 @@ static char *disk_key[ ]=         { "disk_record_ok" };
 
 char *m5trim();
 
-int mk5b_mode_dec(lcl,count,ptr)
+int mk5b_mode_dec(lcl,count,ptr, itask)
 struct mk5b_mode_cmd *lcl;
 int *count;
 char *ptr;
+int itask;
 {
     int ierr, i, arg_key();
     float sample;
-    
+    int decimate;
+
     ierr=0;
     if(ptr == NULL) ptr="";
 
@@ -37,6 +40,15 @@ char *ptr;
       ierr=arg_key(ptr,source_key,NSOURCE_KEY,&lcl->source.source,0,TRUE);
       m5state_init(&lcl->source.state);
       if(ierr==0) {
+	if(3==lcl->source.source) {
+	  if(13 != itask) {
+	    strcpy(lcl->source.magic,"vdif");
+	  } else {
+	    ierr=-220;
+	  } 
+	}
+      }
+      if(ierr==0) {
 	lcl->source.state.known=1;
       } else {
 	lcl->source.state.error=1;
@@ -44,6 +56,8 @@ char *ptr;
       break;
     case 2:
       ierr=arg_int(ptr,&lcl->mask.mask ,0xffffffff,TRUE);
+      if(0==lcl->mask.mask)
+	ierr=-200;
       m5state_init(&lcl->mask.state);
       if(ierr==0) {
 	lcl->mask.state.known=1;
@@ -71,6 +85,7 @@ char *ptr;
       break;
     case 4:
       ierr=arg_float(ptr,&sample,0.0,FALSE);
+      m5state_init(&lcl->samplerate.state);
       if(lcl->decimate.state.known != 0) {
 	if(ierr != -100)
 	  ierr=-300;
@@ -90,39 +105,48 @@ char *ptr;
 	if(sample <= 0.124) {
 	  ierr=-200;
 	} else {
-	  lcl->decimate.decimate=(shm_addr->m5b_crate/sample)+0.5;
-	  if( fabs(lcl->decimate.decimate*sample-shm_addr->m5b_crate)/
+	  decimate=(shm_addr->m5b_crate/sample)+0.1;
+	  if( fabs(decimate*sample-shm_addr->m5b_crate)/
 	      shm_addr->m5b_crate > 0.001)
 	    ierr=-210;
-	  else if( lcl->decimate.decimate!=1 &&
-		   lcl->decimate.decimate!=2 &&
-		   lcl->decimate.decimate!=4 &&
-		   lcl->decimate.decimate!=8 &&
-		   lcl->decimate.decimate!=16) {
+	  else if( decimate!=1 &&
+		   decimate!=2 &&
+		   decimate!=4 &&
+		   decimate!=8 &&
+		   decimate!=16) {
 	    ierr=-210;
 	  }
 	}
       }
       if(ierr==0) {
-	lcl->decimate.state.known=1;
+	lcl->samplerate.samplerate=sample;
+	lcl->samplerate.decimate=decimate;
+	lcl->samplerate.state.known=1;
       } else {
-	lcl->decimate.state.error=1;
+	lcl->samplerate.state.error=1;
       } 
       break;
     case 5:
-      ierr=arg_int(ptr,&lcl->fpdp.fpdp ,0,FALSE);
       m5state_init(&lcl->fpdp.state);
-      if(ierr==0 && lcl->fpdp.fpdp != 1 && lcl->fpdp.fpdp != 2)
-	ierr=-200;
-      if(ierr==0) {
-	lcl->fpdp.state.known=1;
-      } else if(ierr==-100){
-	ierr=0;
-      } else{
-	lcl->fpdp.state.error=1;
+      if(13==itask) {
+	ierr=arg_int(ptr,&lcl->fpdp.fpdp ,0,FALSE);
+	if(ierr==0 && lcl->fpdp.fpdp != 1 && lcl->fpdp.fpdp != 2)
+	  ierr=-200;
+	if(ierr==0) {
+	  lcl->fpdp.state.known=1;
+	} else if(ierr==-100){
+	  ierr=0;
+	} else{
+	  lcl->fpdp.state.error=1;
+	}
+	break;
       }
-      break;
     case 6:
+      m5state_init(&lcl->disk.state);
+      if(14==itask)
+	break;
+      else if(13!=itask)
+	(*count)++;
       ierr=arg_key(ptr,disk_key,NDISK_KEY,&lcl->disk.disk,-1,TRUE);
       m5state_init(&lcl->disk.state);
       if(ierr==0) {
@@ -140,10 +164,11 @@ char *ptr;
    return ierr;
 }
 
-void mk5b_mode_enc(output,count,lclc,itask)
+void mk5b_mode_enc(output,count,lclc,lclm,itask)
 char *output;
 int *count;
 struct mk5b_mode_cmd *lclc;
+struct mk5b_mode_mon *lclm;
 int itask;
 {
 
@@ -153,30 +178,66 @@ int itask;
 
     switch (*count) {
     case 1:
-      m5key_encode(output,source_key,NSOURCE_KEY,
+      if(lclc->source.state.known && lclc->source.source == 3)
+	m5sprintf(output,"%s",lclc->source.magic,&lclc->source.state);
+      else
+	m5key_encode(output,source_key,NSOURCE_KEY,
 		     lclc->source.source,&lclc->source.state);
       break;
     case 2:
-      sprintf(output,"0x%x",lclc->mask.mask);
+      if(lclc->mask.state.known)
+	sprintf(output,"0x%x",lclc->mask.mask);
       m5state_encode(output,&lclc->mask.state);
       break;
     case 3:
-      sprintf(output,"%d",lclc->decimate.decimate);
-      m5state_encode(output,&lclc->decimate.state);
+      m5sprintf(output,"%d",&lclc->decimate.decimate,&lclc->decimate.state);
       break;
     case 4:
-      if(13 == itask || 14 == itask)
-	sprintf(output,"(%.3f)",
-		(float) (shm_addr->m5b_crate/lclc->decimate.decimate) );
-      else if(15==itask)
-	sprintf(output,"%.3f",lclc->fpdp.fpdp/1000000. );
-      m5state_encode(output,&lclc->decimate.state);
+      if(lclc->samplerate.state.known) {
+	sprintf(output,"%.3f",lclc->samplerate.samplerate);
+	m5state_encode(output,&lclc->samplerate.state);
+      } else if(shm_addr->mk5b_mode.samplerate.state.known &&
+		!lclm->tbitrate.state.known) {
+	  sprintf(output,"(%.3f)",shm_addr->mk5b_mode.samplerate.samplerate);
+	  m5state_encode(output,&shm_addr->mk5b_mode.samplerate.state);
+      }
       break;
     case 5:
-      if(15==itask)
-	break;
-      sprintf(output,"%d",lclc->fpdp.fpdp);
-      m5state_encode(output,&lclc->fpdp.state);
+      if(13==itask) {
+	 m5sprintf(output,"%d",&lclc->fpdp.fpdp,&lclc->fpdp.state);
+	 break;
+      }
+    default:
+      *count=-1;
+   }
+
+   if(*count>0) *count++;
+   return;
+}
+void mk5b_mode_mon(output,count,lclm)
+char *output;
+int *count;
+struct mk5b_mode_mon *lclm;
+{
+
+  int ivalue;
+
+    output=output+strlen(output);
+
+    switch (*count) {
+    case 1:
+      m5sprintf(output,"%s",lclm->format.format,&lclm->format.state);
+      break;
+    case 2:
+      m5sprintf(output,"%d",&lclm->tracks.tracks,&lclm->tracks.state);
+      break;
+    case 3:
+      if(lclm->tbitrate.state.known)
+	sprintf(output,"%.3f",lclm->tbitrate.tbitrate/1.0e6);
+      m5state_encode(output,&lclm->tbitrate.state);
+      break;
+    case 4:
+      m5sprintf(output,"%d",&lclm->framesize.framesize,&lclm->framesize.state);
       break;
     default:
       *count=-1;
@@ -191,25 +252,61 @@ char *ptr;
 struct mk5b_mode_cmd *lclc;
 int itask;
 {
-  strcpy(ptr,"mode = ");
+  if(lclc->source.source!=3) {
+    strcpy(ptr,"mode = ");
 
-  strcat(ptr,source_key[lclc->source.source]);
-  strcat(ptr," : ");
-
-  sprintf(ptr+strlen(ptr),"0x%x",lclc->mask.mask);
-  strcat(ptr," : ");
-
-  if(15 != itask) {
-    sprintf(ptr+strlen(ptr),"%d",lclc->decimate.decimate);
+    strcat(ptr,source_key[lclc->source.source]);
     strcat(ptr," : ");
-  } else
-    strcat(ptr," 1 : ");
 
-  if(lclc->fpdp.state.known==1) {
-    sprintf(ptr+strlen(ptr),"%d ;\n",lclc->fpdp.fpdp);
-  } else
-    strcat(ptr+strlen(ptr)," ; \n ");
+    sprintf(ptr+strlen(ptr),"0x%x",lclc->mask.mask);
+    strcat(ptr," : ");
 
+    if(15 != itask) {
+      if(lclc->decimate.state.known)
+	sprintf(ptr+strlen(ptr),"%d",lclc->decimate.decimate);
+      else
+	sprintf(ptr+strlen(ptr),"%d",lclc->samplerate.decimate);
+      strcat(ptr," : ");
+    } else
+      strcat(ptr," 1 : ");
+    
+    if(lclc->fpdp.state.known==1) {
+      sprintf(ptr+strlen(ptr),"%d ;\n",lclc->fpdp.fpdp);
+    } else
+      strcat(ptr+strlen(ptr)," ; \n ");
+
+  } else {
+    int bits=0;
+    int bitmask=lclc->mask.mask;
+    int bits_p_chan = 0 ;
+    int data_rate = 0;
+    int channels = 0;
+    int i;
+    
+    for(i=0;i<32;i++) 
+      if(bitmask & 0x1<<i)
+	bits++;
+    
+    if((0xaaaaaaaa & bitmask) && (0x5555555 & bitmask))
+      bits_p_chan = 2 ;
+    else if(bitmask)
+      bits_p_chan = 1 ;  
+    
+    if(bits_p_chan > 0)
+      channels = bits/bits_p_chan;
+    
+    if(lclc->decimate.state.known && lclc->decimate.decimate > 0)
+      data_rate = bits*shm_addr->m5b_crate/lclc->decimate.decimate;
+    else
+      data_rate = bits*shm_addr->m5b_crate/lclc->samplerate.decimate;
+    
+    snprintf(lclc->source.magic,sizeof(lclc->source.magic),
+	     "VDIF_8000-%d-%d-%d",data_rate,channels,bits_p_chan);
+    strncpy(shm_addr->mk5b_mode.source.magic,lclc->source.magic,
+	    sizeof(shm_addr->mk5b_mode.source.magic));
+    sprintf(ptr,"mode = %s ; \n",lclc->source.magic);
+    
+  }
   return;
 }
 mk5c_clock_set_2_m5(ptr,lclc)
@@ -218,19 +315,25 @@ struct mk5b_mode_cmd *lclc;
 {
   strcpy(ptr,"clock_set = ");
 
-  sprintf(ptr+strlen(ptr),"%d : ext ; \n",
-	  shm_addr->m5b_crate/lclc->decimate.decimate);
+  if(lclc->samplerate.state.known)
+    sprintf(ptr+strlen(ptr),"%.3f : ext ; \n",
+	    lclc->samplerate.samplerate+0.0001);
+  else
+    sprintf(ptr+strlen(ptr),"%d : ext ; \n",
+	    shm_addr->m5b_crate/lclc->decimate.decimate);
 
   return;
 }
 
-m5_2_mk5b_mode(ptr_in,lclc,ip) /* return values:
+m5_2_mk5b_mode(ptr_in,lclc,lclm,itask,ip) /* return values:
 				  *  0 == no error
 				  *  0 != error
 				  */
      char *ptr_in;           /* input buffer to be parsed */
 
      struct mk5b_mode_cmd *lclc;  /* result structure with parameters */
+     struct mk5b_mode_mon *lclm;  /* result structure with parameters */
+     int itask;
      long ip[5];   /* standard parameter array */
 {
   char *new_str, *ptr, *ptr2, *ptr_save;
@@ -247,7 +350,12 @@ m5_2_mk5b_mode(ptr_in,lclc,ip) /* return values:
   m5state_init(&lclc->source.state);
   m5state_init(&lclc->mask.state);
   m5state_init(&lclc->decimate.state);
+  m5state_init(&lclc->samplerate.state);
   m5state_init(&lclc->fpdp.state);
+  m5state_init(&lclm->format.state);
+  m5state_init(&lclm->tracks.state);
+  m5state_init(&lclm->tbitrate.state);
+  m5state_init(&lclm->framesize.state);
 
   ptr=strchr(ptr+1,':');
   if(ptr!=NULL) {
@@ -271,28 +379,59 @@ m5_2_mk5b_mode(ptr_in,lclc,ip) /* return values:
       case 1:
 	if(m5key_decode(ptr,&lclc->source,source_key,NSOURCE_KEY,
 			&lclc->source.state)) {
-	  ierr=-501;
-	  goto error2;
+	  if(m5sscanf(ptr,"%s",lclc->source.magic,&lclc->source.state)) {
+	    ierr=-501;
+	    goto error2;
+	  } else
+	    lclc->source.source = 3;
 	}
 	break;
       case 2:
-	if(m5sscanf(ptr,"%lx",&lclc->mask.mask,&lclc->mask.state)) {
+	if(lclc->source.state.known && 3 == lclc->source.source) {
+	  if(m5sscanf(ptr,"%s",lclm->format.format,&lclm->format.state)) {
+	    ierr=-512;
+	    goto error2;
+	  } 
+	} else if(m5sscanf(ptr,"%lx",&lclc->mask.mask,&lclc->mask.state)) {
 	  ierr=-502;
 	  goto error2;
 	}
 	break;
       case 3:
-	if(m5sscanf(ptr,"%d",&lclc->decimate.decimate,&lclc->decimate.state)) {
+	if(lclc->source.state.known && 3 == lclc->source.source) {
+	  if(m5sscanf(ptr,"%d",&lclm->tracks.tracks,&lclm->tracks.state)) {
+	  ierr=-513;
+	  goto error2;
+	  }
+	} else if(m5sscanf(ptr,"%d",
+			   &lclc->decimate.decimate,&lclc->decimate.state)) {
 	  ierr=-503;
 	  goto error2;
 	}
 	break;
       case 4:
-	if(m5sscanf(ptr,"%d",&lclc->fpdp.fpdp,&lclc->fpdp.state)) {
-	  ierr=-504;
-	  goto error2;
+	if(lclc->source.state.known && 3 == lclc->source.source || itask == 15) {
+	  if(m5sscanf(ptr,"%f",&lclm->tbitrate.tbitrate,&lclm->tbitrate.state)) {
+	    ierr=-514;
+	    goto error2;
+	  }
+	} else if(13 == itask) {
+	  if(m5sscanf(ptr,"%d",&lclc->fpdp.fpdp,&lclc->fpdp.state)) {
+	    ierr=-504;
+	    goto error2;
+	  }
 	}
 	break;
+      case 5:
+	if(lclc->source.state.known && 3 == lclc->source.source &&
+	   !strncasecmp(lclc->source.magic,"VDIF",4)) {
+	  if(m5sscanf(ptr,"%d",
+		      &lclm->framesize.framesize,&lclm->framesize.state)) {
+	    ierr=-505;
+	    goto error2;
+	  }
+	  break;
+	}
       default:
 	goto done;
 	break;
