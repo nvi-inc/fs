2ee2e16709b19aa33db09e63568a567394d69ddf
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
2015-05-20 00:00:00 +0000

    rework mk5X_mode command to use better work with existing data

--- include/mk5b_mode_ds.h
+++ include/mk5b_mode_ds.h
@@ -3,6 +3,7 @@
 struct mk5b_mode_cmd {
   struct {
     int source;
+    char magic[33];
     struct m5state state;
   } source;
   struct {
@@ -14,6 +15,11 @@ struct mk5b_mode_cmd {
     struct m5state state;
   } decimate;
   struct {
+    float samplerate;
+    int decimate;
+    struct m5state state;
+  } samplerate;
+  struct {
     int fpdp;
     struct m5state state;
   } fpdp;
@@ -21,4 +27,23 @@ struct mk5b_mode_cmd {
     int disk;
     struct m5state state;
   } disk;
+};
+
+struct mk5b_mode_mon {
+  struct {
+    char format[33];
+    struct m5state state;
+  } format;
+  struct {
+    int tracks;
+    struct m5state state;
+  } tracks;
+  struct {
+    float tbitrate;
+    struct m5state state;
+  } tbitrate;
+  struct {
+    int framesize;
+    struct m5state state;
+  } framesize;
 };
