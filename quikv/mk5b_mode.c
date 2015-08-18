5d5bc92544d024673a9df73d0f1f8e3e09080025
Ed Himwich <ed.himwich@nviinc.com>
2015-08-18 00:00:00 +0000

    various changes
    2015-05-20
    rework mk5X_mode command to use better work with existing data
    
    2015-05-23
    remove net_protocol= command for m5c_mode=vdif for Mark 5C recorders
    
    2015-08-18
    rearrange buffers sent depending on device
    to minimze code and messages

--- quikv/mk5b_mode.c
+++ quikv/mk5b_mode.c
@@ -36,7 +36,8 @@ long ip[5];                           /* ipc parameters */
       } else if(15 == itask && !
 		(shm_addr->equip.drive[shm_addr->select] == MK5 &&
 		 (shm_addr->equip.drive_type[shm_addr->select] == MK5C ||
-		  shm_addr->equip.drive_type[shm_addr->select] == MK5C_BS))
+		  shm_addr->equip.drive_type[shm_addr->select] == MK5C_BS ||
+		  shm_addr->equip.drive_type[shm_addr->select] == FLEXBUFF))
 		) {
 	ierr=-403;
 	goto error;
@@ -67,19 +68,13 @@ long ip[5];                           /* ipc parameters */
 /* if we get this far it is a set-up command so parse it */
 
 parse:
-      m5state_init(&lcl.source.state);
-      m5state_init(&lcl.mask.state);
-      m5state_init(&lcl.decimate.state);
-      m5state_init(&lcl.fpdp.state);
-      m5state_init(&lcl.disk.state);
-
       ilast=0;                                      /* last argv examined */
       memcpy(&lcl,&shm_addr->mk5b_mode,sizeof(lcl));
 
       count=1;
       while( count>= 0) {
         ptr=arg_next(command,&ilast);
-        ierr=mk5b_mode_dec(&lcl,&count, ptr);
+        ierr=mk5b_mode_dec(&lcl,&count, ptr, itask);
         if(ierr !=0 ) goto error;
       }
 
@@ -100,15 +95,36 @@ parse:
       out_recs=0;
       out_class=0;
 
-      if(15 == itask && shm_addr->m5b_crate != 0) {
-	mk5c_clock_set_2_m5(outbuf,&lcl);
-	cls_snd(&out_class, outbuf, strlen(outbuf) , 0, 0);
-	out_recs++;
-      }
-
       mk5b_mode_2_m5(outbuf,&lcl,itask);
       cls_snd(&out_class, outbuf, strlen(outbuf) , 0, 0);
       out_recs++;
+
+      if(lcl.source.state.known && lcl.source.source != 3) {
+	if(15 == itask && shm_addr->m5b_crate != 0) {
+	  mk5c_clock_set_2_m5(outbuf,&lcl);
+	  cls_snd(&out_class, outbuf, strlen(outbuf) , 0, 0);
+	  out_recs++;
+	}
+      } else { /* VDIF */
+	if(shm_addr->equip.drive[shm_addr->select] == MK5 &&
+	   shm_addr->equip.drive_type[shm_addr->select] == FLEXBUFF) {
+	  strcpy(outbuf,"mtu = 8192 ; \n");
+	  cls_snd(&out_class, outbuf, strlen(outbuf) , 0, 0);
+	  out_recs++;
+
+	  strcpy(outbuf,"net_protocol = udps : 32000000 : 256000000 : 4 ;\n");
+	  cls_snd(&out_class, outbuf, strlen(outbuf) , 0, 0);
+	  out_recs++;
+	}
+
+	if(shm_addr->equip.drive[shm_addr->select] == MK5 &&
+	   (shm_addr->equip.drive_type[shm_addr->select] == MK5C ||
+	    shm_addr->equip.drive_type[shm_addr->select] == MK5C_BS)) {
+	  strcpy(outbuf,"packet = 36 : 0 : 8032 : 0 : 0 ;\n");
+	  cls_snd(&out_class, outbuf, strlen(outbuf) , 0, 0);
+	  out_recs++;
+	}
+      }
 
 mk5cn:
       ip[0]=1;
