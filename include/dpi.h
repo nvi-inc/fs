9f0a668d3c96010ea4dfbb788888a811a20b1abc
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
2015-09-28 00:00:00 +0000

    parenthesises (for safety) around all calculated constant expressions

--- include/dpi.h
+++ include/dpi.h
@@ -6,9 +6,9 @@
 */
 
 #define DPI M_PI
-#define DEG2RAD  DPI/180.0
-#define RAD2DEG  180.0/DPI
-#define DTWOPI   2.0*DPI
-#define SEC2RAD  DPI/43200.0
-#define RAD2SEC  43200.0/DPI
+#define DEG2RAD  (DPI/180.0)
+#define RAD2DEG  (180.0/DPI)
+#define DTWOPI   (2.0*DPI)
+#define SEC2RAD  (DPI/43200.0)
+#define RAD2SEC  (43200.0/DPI)
 
