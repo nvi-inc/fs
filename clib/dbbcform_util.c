7f59c1f651417bcb6878fab0d4d8a7b75aa42f21
Ed Himwich <ed.himwich@nviinc.com>
2016-01-16 00:00:00 +0000

    add flex for DBBC PFB and new rack_types

--- clib/dbbcform_util.c
+++ clib/dbbcform_util.c
@@ -11,9 +11,11 @@
 #include "../include/shm_addr.h"      /* shared memory pointer */
 
 static char *mode_key[ ]={"astro","geo","wastro","test","lba","astro2","astro3"};
+static char *pfb_key[ ]={"flex"};
 static char *test_key[ ]={"0","1","bin","tvg"};
 
 #define NMODE_KEY sizeof(mode_key)/sizeof( char *)
+#define NPFB_KEY sizeof(pfb_key)/sizeof( char *)
 #define NTEST_KEY sizeof(test_key)/sizeof( char *)
 
 int dbbcform_dec(lcl,count,ptr)
@@ -30,23 +32,33 @@ char *ptr;
 
     switch (*count) {
       case 1:
-	ierr=arg_key(ptr,mode_key,NMODE_KEY,&lcl->mode,0,FALSE);
-	if(0 == ierr)
-	  if(5==lcl->mode && shm_addr->dbbcddcv < 104)
-	    ierr=-210;
-	  else if(6==lcl->mode && shm_addr->dbbcddcvl[0] == ' ')
-	    ierr=-220;
-	  else if(NULL != index("ef",shm_addr->dbbcddcvl[0]) &&
-		  3!=lcl->mode && 6!=lcl->mode)
-	    ierr=-230;
+	if(DBBC_DDC == shm_addr->equip.rack_type ||
+	   DBBC_DDC_FILA10G == shm_addr->equip.rack_type) {
+	  ierr=arg_key(ptr,mode_key,NMODE_KEY,&lcl->mode,0,FALSE);
+	  if(0 == ierr)
+	    if(5==lcl->mode && shm_addr->dbbcddcv < 104)
+	      ierr=-210;
+	    else if(6==lcl->mode && shm_addr->dbbcddcvl[0] == ' ')
+	      ierr=-220;
+	    else if(NULL != index("ef",shm_addr->dbbcddcvl[0]) &&
+		    3!=lcl->mode && 6!=lcl->mode)
+	      ierr=-230;
+	} else if(DBBC_PFB == shm_addr->equip.rack_type ||
+		  DBBC_PFB_FILA10G == shm_addr->equip.rack_type) {
+	  ierr=arg_key(ptr,pfb_key,NPFB_KEY,&lcl->mode,0,FALSE);
+	}
         break;
       case 2:
-	if(lcl->mode == 3)
-	  ierr=arg_key(ptr,test_key,NTEST_KEY,&lcl->test,0,FALSE);
-	else {
-	  lcl->test=-1;
-	  ierr=0;
-	}
+	if(DBBC_DDC == shm_addr->equip.rack_type ||
+	   DBBC_DDC_FILA10G == shm_addr->equip.rack_type) {
+	  if(lcl->mode == 3)
+	    ierr=arg_key(ptr,test_key,NTEST_KEY,&lcl->test,0,FALSE);
+	  else {
+	    lcl->test=-1;
+	    ierr=0;
+	  }
+	} else
+	  *count=-1;
         break;
       default:
        *count=-1;
@@ -69,17 +81,29 @@ struct dbbcform_cmd *lcl;
     switch (*count) {
       case 1:
         ivalue = lcl->mode;
-        if (ivalue >=0 && ivalue <NMODE_KEY)
-          strcpy(output,mode_key[ivalue]);
-        else
-          strcpy(output,BAD_VALUE);
+	if(DBBC_DDC == shm_addr->equip.rack_type ||
+	   DBBC_DDC_FILA10G == shm_addr->equip.rack_type) {
+	  if (ivalue >=0 && ivalue <NMODE_KEY)
+	    strcpy(output,mode_key[ivalue]);
+	  else
+	    strcpy(output,BAD_VALUE);
+	} else {
+	  if (ivalue >=0 && ivalue <NPFB_KEY)
+	    strcpy(output,pfb_key[ivalue]);
+	  else
+	    strcpy(output,BAD_VALUE);
+	} 
         break;
       case 2:
-	if(lcl->mode == 3) {
-	  ivalue = lcl->test;
-	  if (ivalue >=0 && ivalue <NTEST_KEY)
-	    strcpy(output,test_key[ivalue]);
-	} else 
+	if(DBBC_DDC == shm_addr->equip.rack_type ||
+	   DBBC_DDC_FILA10G == shm_addr->equip.rack_type) {
+	  if(lcl->mode == 3) {
+	    ivalue = lcl->test;
+	    if (ivalue >=0 && ivalue <NTEST_KEY)
+	      strcpy(output,test_key[ivalue]);
+	  } else 
+	    *count=-1;
+	} else
 	  *count=-1;
         break;
       default:
@@ -99,13 +123,19 @@ struct dbbcform_cmd *lcl;
 
   sprintf(buff,"dbbcform=");
 
-  if(lcl->mode >= 0 && lcl->mode < NMODE_KEY)
-    strcat(buff,mode_key[lcl->mode]);
-      
-  if(lcl->mode == 3) {
-    strcat(buff,",");
-    if(lcl->test >= 0 && lcl->test < NTEST_KEY) 
-      strcat(buff,test_key[lcl->test]);
+  if(DBBC_DDC == shm_addr->equip.rack_type ||
+     DBBC_DDC_FILA10G == shm_addr->equip.rack_type) {
+    if(lcl->mode >= 0 && lcl->mode < NMODE_KEY)
+      strcat(buff,mode_key[lcl->mode]);      
+
+    if(lcl->mode == 3) {
+      strcat(buff,",");
+      if(lcl->test >= 0 && lcl->test < NTEST_KEY) 
+	strcat(buff,test_key[lcl->test]);
+    }
+  } else {
+    if(lcl->mode >= 0 && lcl->mode < NPFB_KEY)
+      strcat(buff,pfb_key[lcl->mode]);      
   }
 
   return;
@@ -123,15 +153,23 @@ char *buff;
     return -1;
 
   ptr=strtok(NULL,",");
-  ierr=arg_key(ptr,mode_key,NMODE_KEY,&lclc->mode,-1,TRUE);
-  if(ierr!=0)
-    return -1;
 
-  if(lclc->mode == 3) {
-    ptr=strtok(NULL,",");
-    ierr=arg_key(ptr,test_key,NTEST_KEY,&lclc->test,-1,TRUE);
+  if(DBBC_DDC == shm_addr->equip.rack_type ||
+     DBBC_DDC_FILA10G == shm_addr->equip.rack_type) {
+    ierr=arg_key(ptr,mode_key,NMODE_KEY,&lclc->mode,-1,TRUE);
     if(ierr!=0)
-    return -1;
+      return -1;
+    
+    if(lclc->mode == 3) {
+      ptr=strtok(NULL,",");
+      ierr=arg_key(ptr,test_key,NTEST_KEY,&lclc->test,-1,TRUE);
+      if(ierr!=0)
+	return -1;
+    }
+  } else {
+    ierr=arg_key(ptr,pfb_key,NPFB_KEY,&lclc->mode,-1,TRUE);
+    if(ierr!=0)
+      return -1;
   }
 
   return 0;
