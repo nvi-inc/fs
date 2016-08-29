e70b23a1928deb4c992a306b19d46086b0f23060
Ed Himwich <ed.himwich@nviinc.com>
2016-08-29 00:00:00 +0000

    add az steps without cos(el) correction, single pass on either, and furrowing in el

--- holog/holog.c
+++ holog/holog.c
@@ -23,7 +23,8 @@ main()
   int i,it[6], ierr, j, ierr1, nwt, koff;
   double az,el, xoff, yoff, azoff, eloff, haoff, decoff;
   char buff[256];
-  float rut,rut1,astep,estep,astep1;
+  float rut,rut1,astep,estep,astep1,estep1;
+  int azp,elp;
 
 /* connect to the FS */
 
@@ -57,14 +58,9 @@ main()
 
   /* wait for onsource */
 
-#ifdef WEH
-  printf("WEH test version\n");
-  rte_sleep(500);
-#else
   nwt=holog.wait;
   if(onsor(nwt,&ierr)) 
     goto error_recover;
-#endif
 
   if(local(&az,&el,"azel",&ierr))
     goto error_recover;
@@ -109,9 +105,15 @@ main()
   jr2as((float) decoff*RAD2DEG,buff,-9,5,sizeof(buff));
   logit(buff,0,NULL);
 
+  azp=abs(holog.azp);
+  elp=abs(holog.elp);
 
-  astep=(holog.az/cos(el))/(abs(holog.azp)-1);
-  estep=holog.el/(abs(holog.elp)-1);
+  if(holog.elp >= 1)
+    astep=(holog.az/cos(el))/(azp-1);
+  else
+    astep=holog.az/(azp-1);
+
+  estep=holog.el/(elp-1);
 
   if( holog.ical > 0) {
     rte_time(it,it+5);
@@ -121,21 +123,25 @@ main()
     sprintf(buff,"First %7.1lf",rut1);
     logit(buff,0,NULL);
     next(buff,0.0,0.0,sizeof(buff));
-#ifdef WEH
-    rte_sleep(500);
-#else
     if(gooff(azoff,eloff,"azel",holog.wait,&ierr))
       goto error_recover;
-#endif
+
     scmds(holog.proc,0.0,0.0);
     rte_time(it,it+5);
     rut=it[3]*3600.0+it[2]*60.0+it[1]+((double)it[0])/100.;
   }
 
-  if(holog.elp > 1 && holog.azp > 1 ) {
-    for(j=-holog.elp/2;j<=holog.elp/2;j++) {
-      astep1=astep*(1-2*abs((j+holog.elp/2)%2));
-      for(i=-holog.azp/2;i<=holog.azp/2;i++) {
+  if((holog.azp > 1)) { /*furrow in az */
+    for(j=-elp/2;j<=elp/2;j++) {
+      if(elp == 1) {           /* one row */
+	astep1=astep;
+	estep1=0.0;
+      } else {                /* multiple rows */
+	astep1=astep*(1-2*abs((j+elp/2)%2)); /* alternate directions */
+	estep1=estep;
+      }
+
+      for(i=-azp/2;i<=azp/2;i++) {
 	if(holog.ical > 0) {
 	  if(i==0 && j==0)
 	    continue;
@@ -147,98 +153,62 @@ main()
 	    sprintf(buff,"Recalibrate %7.1lf",rut1);
 	    logit(buff,0,NULL);
 	    next(buff,0.0,0.0,sizeof(buff));
-#ifdef WEH
-	    rte_sleep(500);
-#else
 	    if(gooff(azoff,eloff,"azel",holog.wait,&ierr))
 	      goto error_recover;
-#endif
+
 	    koff=FALSE;
 	    scmds(holog.proc,0.0,0.0);
 	    rut=rut1;
 	  }
 	}
-	next(buff,i*astep1,j*estep,sizeof(buff));
+	next(buff,i*astep1,j*estep1,sizeof(buff));
 	koff=TRUE;
-#ifdef WEH
-	rte_sleep(500);
-#else
-	if(gooff(azoff+i*astep1,eloff+j*estep,"azel",
+	if(gooff(azoff+i*astep1,eloff+j*estep1,"azel",
 		 shm_addr->holog.wait,&ierr))
 	  goto error_recover;
-#endif
-	scmds(holog.proc,i*astep1,j*estep);
+	scmds(holog.proc,i*astep1,j*estep1);
       }
     }
-  } else {
-    astep1=astep;
-    for(i=-abs(holog.azp)/2;i<=abs(holog.azp)/2;i++) {
-      if(holog.ical > 0) {
-	if(i==0)
-	  continue;
-	rte_time(it,it+5);
-	rut1=it[3]*3600.0+it[2]*60.0+it[1]+((double)it[0])/100.;
-	if(rut1 < rut)
-	  rut1 += 86400.;
-	if(rut1-rut >= holog.ical) {
-	  sprintf(buff,"Recalibrate %7.1lf",rut1);
-	  logit(buff,0,NULL);
-	  next(buff,0.0,0.0,sizeof(buff));
-#ifdef WEH
-	  rte_sleep(500);
-#else
-	  if(gooff(azoff,eloff,"azel",holog.wait,&ierr))
-	    goto error_recover;
-#endif
-	  koff=FALSE;
-	  scmds(holog.proc,0.0,0.0);
-	  rut=rut1;
-	}
+  } else { /* furrow in elevation */
+    for(i=-azp/2;i<=azp/2;i++) {
+      if(elp == 1 && azp == 1) { /* one point */
+	astep1=0.0;
+	estep1=0.0;
+      } else if(azp == 1) {  /* one column */
+	astep1=0.0;
+	estep1=estep;
+      } else  {                 /* multiple columns */
+	astep1=astep;
+	estep1=estep*(1-2*abs((i+azp/2)%2)); /* alternate directions */
       }
-      next(buff,i*astep1,0.0,sizeof(buff));
-      koff=TRUE;
-#ifdef WEH
-      rte_sleep(500);
-#else
-      if(gooff(azoff+i*astep1,eloff+0.0,"azel",
-	       shm_addr->holog.wait,&ierr))
-	goto error_recover;
-#endif
-      scmds(holog.proc,i*astep1,0.0);
-    }
-    for(j=-abs(holog.elp)/2;j<=abs(holog.elp)/2;j++) {
-      if(holog.ical > 0) {
-	if(j==0)
-	  continue;
-	rte_time(it,it+5);
-	rut1=it[3]*3600.0+it[2]*60.0+it[1]+((double)it[0])/100.;
-	if(rut1 < rut)
-	  rut1 += 86400.;
-	if(rut1-rut >= holog.ical) {
-	  sprintf(buff,"Recalibrate %7.1lf",rut1);
-	  logit(buff,0,NULL);
-	  next(buff,0.0,0.0,sizeof(buff));
-#ifdef WEH
-	  rte_sleep(500);
-#else
-	  if(gooff(azoff,eloff,"azel",holog.wait,&ierr))
-	    goto error_recover;
-#endif
-	  koff=FALSE;
-	  scmds(holog.proc,0.0,0.0);
-	  rut=rut1;
+
+      for(j=-elp/2;j<=elp/2;j++) {
+	if(holog.ical > 0) {
+	  if(i==0 && j==0)
+	    continue;
+	  rte_time(it,it+5);
+	  rut1=it[3]*3600.0+it[2]*60.0+it[1]+((double)it[0])/100.;
+	  if(rut1 < rut)
+	    rut1 += 86400.;
+	  if(rut1-rut >= holog.ical) {
+	    sprintf(buff,"Recalibrate %7.1lf",rut1);
+	    logit(buff,0,NULL);
+	    next(buff,0.0,0.0,sizeof(buff));
+	    if(gooff(azoff,eloff,"azel",holog.wait,&ierr))
+	      goto error_recover;
+
+	    koff=FALSE;
+	    scmds(holog.proc,0.0,0.0);
+	    rut=rut1;
+	  }
 	}
+	next(buff,i*astep1,j*estep1,sizeof(buff));
+	koff=TRUE;
+	if(gooff(azoff+i*astep1,eloff+j*estep1,"azel",
+		 shm_addr->holog.wait,&ierr))
+	  goto error_recover;
+	scmds(holog.proc,i*astep1,j*estep1);
       }
-      next(buff,0.0,j*estep,sizeof(buff));
-      koff=TRUE;
-#ifdef WEH
-      rte_sleep(500);
-#else
-      if(gooff(azoff+0.0,eloff+j*estep,"azel",
-	       shm_addr->holog.wait,&ierr))
-	goto error_recover;
-#endif
-      scmds(holog.proc,0.0,j*estep);
     }
   }
 
