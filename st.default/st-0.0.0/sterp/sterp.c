9864aa2a83d5e7e255eee125bcf89f8adae0c2fe
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
2016-09-10 00:00:00 +0000

    add comments about not using class-I/O to avoid deadlock

--- st.default/st-0.0.0/sterp/sterp.c
+++ st.default/st-0.0.0/sterp/sterp.c
@@ -7,9 +7,20 @@
  * stpgm.ctl file: "sterp n xterm -g +75+75 -e sterp &"
  *
  * Input:
- *       IP(0) = class number to retrieve message from
+ *       The error message to be processed is transmitted by ddout and
+ *       retrieved with the get_err() call.
  *
  * Output: None 
+ *
+ * VERY IMPORTANT:
+ *
+ *   It is mandatory that this program _not_ use the FS class-I/O system,
+ *   particularly cls_snd() and any other calls that use it, including the
+ *   logit*() family of calls. Not following this rule could cause a
+ *   deadlock situation.
+ *
+ *   If this program encounters any error internally, it should use its
+ *   own independent error reporting system and not report via the FS.
  */
 
 #include <stdio.h>
