079cc3eb7572f35043c67ac015910e63e5076db0
David Horsley <3401668+dehorsley@users.noreply.github.com>
2018-05-01 00:00:00 +0000

    added client commands

--- oprin/oprin.c
+++ oprin/oprin.c
@@ -29,6 +29,8 @@
 /* For malloc, free. */
 #include <stdlib.h>
 
+#include <errno.h>
+
 /* For GNU Readline. */
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -63,7 +65,121 @@ static char prompt[] = ">";
 static char termination_command[] = "end_oprin";
 
 
-/* The dynamically allocated SNAP command table. */
+/* Set by command line flag. Determines if oprin is started with the field system or 
+ * later.
+ * 
+ * Always == 0 when FS build with server/client
+ */
+int fs_internal = 0;
+
+
+/* sent to boss */
+void run_snap(char *cmd) {
+	static int kfirst = 1;
+	while (kfirst && shm_addr->iclopr == -1)
+		rte_sleep(2);
+	kfirst = 0;
+
+	if (nsem_test("fs   ") != 1 && !fs_internal) {
+		fprintf(stderr, "FS no longer running.\n");
+		exit(0);
+	}
+	int len = strlen(cmd);
+	cls_snd(&(shm_addr->iclopr), cmd, len, 0, 0);
+	skd_run("boss ", 'n', ipr);
+	return;
+}
+
+/* Oprin commands */
+
+void client_command(const char *cmd) {
+    static FILE* f = NULL;
+
+    if (!f) {
+        char* fdstr = getenv("FS_CLIENT_PIPE_FD");
+
+        if (!fdstr || !*fdstr ) {
+            fprintf(stderr, "oprin: connected to fsclient");
+            return;
+        }
+        f = fdopen(atoi(fdstr), "w");
+        if (!f) {
+            perror("oprin: error opening pipe");
+            return;
+        }
+    }
+
+    fprintf(f, "%s\n", cmd);
+    fflush(f);
+}
+
+void end_oprin(const char *arg) {
+	if (fs_internal) {
+		fprintf(stderr,
+		        "The '%s' command is only available from a stand-alone "
+		        "'oprin'.\nUse 'terminate' to stop the field system.\n",
+		        termination_command);
+		return;
+	}
+	exit(EXIT_SUCCESS);
+}
+
+void terminate(const char *arg) {
+    /* without the display server, run this command as usual */
+#ifdef FS_NO_DISPLAY_SERVER
+        run_snap("terminate");
+#endif
+
+	if (!arg || !arg[0]) {
+		fprintf(stderr,
+		        "You are running FS client. Use \"terminate=fs\" or \"terminate=client\"\n");
+		return;
+	}
+
+	if (strcmp(arg, "client") == 0) {
+        /* TODO: actually kill the client here */
+		exit(EXIT_SUCCESS);
+	}
+
+	if (strcmp(arg, "fs") == 0) {
+            run_snap("terminate");
+	}
+}
+
+/* contents of cmd after first '=' are handed to the function in arg */
+
+static const struct cmd local_commands[] = {
+    {"end_oprin", end_oprin},
+    {"client", client_command},
+/*    {"terminate", terminate}, */
+    {NULL, NULL},
+};
+
+/* Tries to run a local command. Returns true on if the command exists */
+int try_local_command(const char *s) {
+	char *str = strdup(s);
+
+	char *cmd = str;
+	while (isspace(*cmd)) cmd++;
+
+	char *arg = strchr(cmd, '=');
+	if (arg) *arg++ = '\0';
+
+	int ret = 0;
+	unsigned int i;
+
+	const struct cmd* lcmd = local_commands;
+
+	for (lcmd = local_commands; lcmd->name; lcmd++) {
+		if (strcmp(lcmd->name, cmd) == 0) {
+			lcmd->cmd(arg);
+			ret = 1;
+			break;
+		}
+	}
+	free(str);
+	return ret;
+}
 
 /* 'oprin' main. */
 
@@ -74,20 +190,21 @@ main(int argc, char **argv)
   char *previous_input;
   int length;
   int kfirst=1;
-  int fs_internal;
-  int start, termination;
 
   setup_ids();
   sig_ignore();
 
+#ifdef FS_NO_DISPLAY_SERVER
   fs_internal = argc == 2 && 0 == strcmp("-fs_internal", argv[1]);
+#endif
 
   if(nsem_test("fs   ") != 1 && !fs_internal) {
     fprintf(stderr, "FS not running.\n");
     exit(0);
   }
 
-  initialize_readline(fs_internal);
+  initialize_readline(local_commands);
+  /* TODO: add local commands to readline tab completetion */
 
   previous_input = NULL;
   while (1) {
@@ -134,28 +251,6 @@ main(int argc, char **argv)
       continue;  /* no further actions (ie. 'free()') required */
     }
     
-    termination = 0;
-  
-    for (start=0; input[start] != 0 && isspace(input[start]); start++)
-      ;
-    termination = 0 == strncmp(input+start,termination_command,strlen(termination_command));
-
-    if(termination && strlen(input+start) > strlen(termination_command)) {
-      for(start=strlen(termination_command); input[start] != 0 && isspace(input[start]); start++)
-	;
-      if(input[start]!=0) {
-	fprintf(stderr,"The '%s' command was rejected because it had\nextraneous trailing non-blank characters.\n",
-		termination_command);
-	continue;
-      }
-    }
-      
-    if(termination)
-      if(fs_internal) {
-      fprintf(stderr,"The '%s' command is only available from a stand-alone 'oprin'.\nUse 'terminate' to stop the field system.\n",termination_command);
-      continue;
-      } else
-	exit(0);
     
     /* Now we have got something that's not EOF,
        perhaps an empty line or a real command. */
@@ -166,6 +261,12 @@ main(int argc, char **argv)
       /* We have at least one character. Let's add the line to the history. */
 
       add_history(input);
+
+      /* oprin commands override fs commands */
+      if (try_local_command(input)) {
+          previous_input = input;
+          continue;
+      }
 
       /* Execute this SNAP command via "boss". */
       /* wait for iclopr to be defined on the first time through */
