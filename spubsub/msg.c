3d3819a56e0ef9323eb7083efd84ade7726cc62e
David Horsley <david.horsley@nviinc.com>
2018-07-30 00:00:00 +0000

    add wait feature to help fix problems around shutdown
    converted ssub to use new library

--- spubsub/msg.c
+++ spubsub/msg.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <nanomsg/nn.h>
+#include <nng/compat/nanomsg/nn.h>
 #include "msg.h"
 
 // Output little-endian encoded uint64 to buffer,
--- spubsub/ssub.c
+++ spubsub/ssub.c
@@ -7,55 +7,84 @@
 
 #include <getopt.h>
 
-#include <nanomsg/nn.h>
-#include <nanomsg/pubsub.h>
-#include <nanomsg/reqrep.h>
+#include <nng/nng.h>
+#include <nng/protocol/pubsub0/sub.h>
+#include <nng/protocol/reqrep0/req.h>
 
 #include "msg.h"
 
-void nn_perror(const char *msg) {
-	fprintf(stderr, "ssub: %s: %s\n", msg, nn_strerror(nn_errno()));
+void nng_perror(const char *msg, int rv) {
+	fprintf(stderr, "ssub: %s: %s\n", msg, nng_strerror(rv));
 	fflush(stderr);
 }
 
-long long sync_msgs(int seq, char *addr) {
-	//  unsigned long long last_seq = 0;
-	int fd = nn_socket(AF_SP, NN_REQ);
+#define error(msg, rv)                                                                             \
+	fprintf(stderr, "ssub (%s:%d %s) error %s: %s\n", __FILE__, __LINE__, __FUNCTION__, msg,   \
+	        nng_strerror(rv))
 
-	int maxrcv = -1; // Unlimited
-	if (nn_setsockopt(fd, NN_SOL_SOCKET, NN_RCVMAXSIZE, &maxrcv,
-	                  sizeof(maxrcv)) < 0) {
+long long sync_msgs(int seq, char *addr, nng_duration timeout) {
+	//  unsigned long long last_seq = 0;
+	nng_socket sock;
+
+	int rv;
+	rv = nng_req0_open(&sock);
+	if (rv != 0) {
+		error("opening req socket", rv);
+		return -1;
+	}
+
+	rv = nng_setopt_size(sock, NNG_OPT_RECVMAXSZ, 0);
+	if (rv != 0) {
+		error("setting recv size", rv);
 		return -1;
 	};
 
-	if (nn_connect(fd, addr) < 0) {
-		nn_perror("error on connect");
+	rv = nng_setopt_ms(sock, NNG_OPT_RECVTIMEO, timeout);
+	if (rv != 0) {
+		error("setting timeout", rv);
+		return -1;
+	};
+
+	rv = nng_setopt_ms(sock, NNG_OPT_SENDTIMEO, timeout);
+	if (rv != 0) {
+		error("setting timeout", rv);
+		return -1;
+	};
+
+	rv = nng_dial(sock, addr, NULL, 0);
+	if (rv != 0) {
+		error("connecting to server", rv);
 		return -1;
 	}
 
 	uint8_t outbuf[8];
 
-	int rc = uint64_marshal_le(seq, outbuf, 8);
-	if (rc < 0)
+	int outsize = uint64_marshal_le(seq, outbuf, 8);
+	if (outsize < 0)
 		return -1;
 
-	if (nn_send(fd, outbuf, rc, 0) < 0)
+	rv = nng_send(sock, outbuf, outsize, 0);
+	if (rv != 0) {
+		error("sending syncing message to server", rv);
 		return -1;
+	}
 
-	uint8_t *buf;
-	rc = nn_recv(fd, &buf, NN_MSG, 0);
-	if (rc < 0) {
+	void *buf = NULL;
+	size_t size;
+	rv = nng_recv(sock, &buf, &size, NNG_FLAG_ALLOC);
+	if (rv != 0) {
+		error("connecting syncing with server", rv);
 		return -1;
 	}
 
 	uint8_t *bptr = buf;
 	msg_t m       = {DATA, 0, 0, NULL};
-	int rem       = rc;
+	size_t rem    = size;
 
 	while (rem > 0) {
-		rc = msg_unmarshal(&m, bptr, rem);
+		int rcv_msg_size = msg_unmarshal(&m, bptr, rem);
 
-		if (rc < 0)
+		if (rcv_msg_size < 0)
 			break;
 
 		write(STDOUT_FILENO, m.data, m.len);
@@ -65,11 +94,12 @@ long long sync_msgs(int seq, char *addr) {
 			m.data = NULL;
 		}
 
-		bptr += rc;
-		rem -= rc;
+		bptr += rcv_msg_size;
+		rem -= rcv_msg_size;
 	};
-	nn_freemsg(buf);
-	nn_close(fd);
+
+	nng_free(buf, size);
+	nng_close(sock);
 
 	if (m.data != NULL) {
 		free(m.data);
@@ -79,35 +109,43 @@ long long sync_msgs(int seq, char *addr) {
 	return m.seq;
 }
 
-const char *usage_short_str = "Usage: %s [-swh] [pubaddr repaddr]\n";
-const char *usage_long_str =  "Usage: %s [-swh] [pubaddr repaddr]\n"
+// clang-format off
+const char *usage_short_str = 
+"Usage: %s [-swhp] [pubaddr syncaddr]\n";
+const char *usage_long_str =  
+"Usage: %s [-swhp] [pubaddr syncaddr]\n"
 "subscribe to a reliable pubsub stream\n"
 "  pubaddr, syncaddr   nanomsg addresses of publish and resync sockets\n"
 "  -s, --scrollback    print full scrollback buffer on connection\n"
 "  -w, --wait          wait for new connections if server shuts down\n"
-"  -h, --help          print this message\n";
+"  -h, --help          print this message\n"
+"  -p, --print-timeout print a message to stderr when waiting for connection \n";
+
+
+const static struct option long_options[] = {
+    {"scrollback",    no_argument, NULL, 's'},
+    {"wait",          no_argument, NULL, 'w'},
+    {"help",          no_argument, NULL, 'h'},
+    {"print-timeout", no_argument, NULL, 'p'},
+
+    {NULL, 0, NULL, 0}
+};
+
+// clang-format on
 
 int main(int argc, char **argv) {
 
 	// Default config
-	char *pubaddr = "tcp://localhost:4444";
-	char *repaddr = "tcp://localhost:4445";
-	static int perform_first_sync    = false;
-	static int exit_on_terminate_msg = true;
-
-	static struct option long_options[] = {
-	    {"scrollback", no_argument, NULL, 's'},
-	    {"wait",       no_argument, NULL, 'w'},
-	    {"help",       no_argument, NULL, 'h'},
-
-	    {NULL, 0, NULL, 0}
-	};
+	char *pubaddr             = "tcp://localhost:4444";
+	char *repaddr             = "tcp://localhost:4445";
+	int perform_first_sync    = false;
+	int exit_on_terminate_msg = true;
+	int print_timeout         = false;
 
 	// Hande flags
 	int opt;
 	int option_index;
-	while ((opt = getopt_long(argc, argv, "swh", long_options,
-	                          &option_index)) != -1) {
+	while ((opt = getopt_long(argc, argv, "swhp", long_options, &option_index)) != -1) {
 		switch (opt) {
 		case 0:
 			// All long options are handled by their short form
@@ -118,8 +156,11 @@ int main(int argc, char **argv) {
 		case 'w':
 			exit_on_terminate_msg = false;
 			break;
+		case 'p':
+			print_timeout = true;
+			break;
 		case 'h':
-			fprintf(stderr, usage_long_str, argv[0]);
+			printf(usage_long_str, argv[0]);
 			exit(EXIT_SUCCESS);
 			break;
 		default: /* '?' */
@@ -141,40 +182,51 @@ int main(int argc, char **argv) {
 
 	// Now we're configured, let's get down to business...
 	//
-	// The strategy is to connect to the `pub` socket and read `msg`s
-	// If we notice we're out of sync by inspecting `seq`, we connect to the
-	// rep socket and request a resync.
+	// The strategy is to connect to the `pub` socket and read `msg`s If we notice
+	// we're out of sync by inspecting `seq`, we connect to the rep socket and request
+	// a resync.
 	//
-	// HEARTBEAT type `msg`s have the `seq` set to indicate the *next* valid
-	// DATA msg that will be transmitted. If this is not what the client is
-	// expecting, it should preform an out of band resync as with regular
-	// DATA msgs
+	// HEARTBEAT type `msg`s have the `seq` set to indicate the *next* valid DATA msg
+	// that will be transmitted. If this is not what the client is expecting, it
+	// should preform an out of band resync as with regular DATA msgs
 	//
-	// END_OF_SESSION type `msg`s indicate the server is shutting down
-	// gracefully. The client can stay online or not (in this client
-	// determinted by the `exit_on_terminate_msg` variable)
+	// END_OF_SESSION type `msg`s indicate the server is shutting down gracefully. The
+	// client can stay online or not (in this client determinted by the
+	// `exit_on_terminate_msg` variable)
 
-	int sub = nn_socket(AF_SP, NN_SUB);
+	int rv;
+	nng_socket sock;
+
+	if ((rv = nng_sub0_open(&sock)) != 0) {
+		error("nng_socket", rv);
+		exit(1);
+	}
+	rv = nng_setopt(sock, NNG_OPT_SUB_SUBSCRIBE, NULL, 0);
+
+	if (rv != 0) {
+		error("nng_setopt", rv);
+		exit(1);
+	}
 
 	int timeout = 2000;
-	if (nn_setsockopt(sub, NN_SUB, NN_SUB_SUBSCRIBE, NULL, 0) <
-	        0 || // Subscribe to anything
-	    nn_setsockopt(sub, NN_SOL_SOCKET, NN_RCVTIMEO, &timeout,
-	                  sizeof(timeout)) < 0) {
-		nn_perror("error setting socket options");
+	rv          = nng_setopt_ms(sock, NNG_OPT_RECVTIMEO, timeout);
+	if (rv != 0) {
+		error("seting sub timeout", rv);
 		exit(1);
 	}
 
-	if (nn_connect(sub, pubaddr) < 0) {
-		nn_perror("error connecting to pub socket");
+	if ((rv = nng_dial(sock, pubaddr, NULL, NNG_FLAG_NONBLOCK)) != 0) {
+		error("nng_dial", rv);
 		exit(1);
 	}
 
-	int nbytes;
-	void *buf = NULL; // nanomsg buffer msgs
-	msg_t m   = {DATA, 0, 0, NULL};
-	uint64_t seq =
-	    0; // sequence id of the next msg we're expecting to receive
+	void *buf = NULL; // nng buffer msgs
+	size_t sz;
+
+	msg_t m = {DATA, 0, 0, NULL};
+
+	// sequence id of the next msg we're expecting to receive
+	uint64_t seq = 0;
 
 	// Indicate if the client has printed to the terminal to inform the user
 	// of a timeout
@@ -184,15 +236,17 @@ int main(int argc, char **argv) {
 	bool synced = false; // state of client
 	for (;;) {
 		if (buf != NULL) {
-			nn_freemsg(buf);
+			nng_free(buf, sz);
 			buf = NULL;
 		}
-		nbytes = nn_recv(sub, &buf, NN_MSG, 0);
+		rv = nng_recv(sock, &buf, &sz, NNG_FLAG_ALLOC);
 
-		if (nbytes < 0) {
-			if (nn_errno() == ETIMEDOUT) {
-				if (!printed_timeout) {
-					// TODO: make portable and save style
+		if (rv != 0) {
+			if (rv == NNG_ETIMEDOUT) {
+				if (print_timeout && !printed_timeout) {
+					// TODO: make portable
+					// TODO: save terminal style
+					// TODO: make customizable
 					fprintf(stderr, "\e[0;31mWaiting for "
 					                "connection...\e[0m");
 					fflush(stderr);
@@ -200,13 +254,11 @@ int main(int argc, char **argv) {
 				}
 				continue;
 			}
-			nn_perror("error on recv");
+			error("error on recv", rv);
 			exit(1);
 		}
 
-		if (printed_timeout) {
-			// Clear line (assuming ANSI compatible terminal)
-			// TODO: use ncurses
+		if (print_timeout && printed_timeout) {
 			fprintf(stderr, "\e[2K\r");
 			fflush(stderr);
 			printed_timeout = false;
@@ -216,44 +268,53 @@ int main(int argc, char **argv) {
 			free(m.data);
 			m.data = NULL;
 		}
-		int rc = msg_unmarshal(&m, buf, nbytes);
+
+		int rc = msg_unmarshal(&m, buf, sz);
 		if (rc < 0) {
-			nn_perror("error unmarshaling");
+			perror("error unmarshaling");
+			exit(1);
 		}
 
-		if (m.type == END_OF_SESSION && exit_on_terminate_msg) {
-			break;
-		}
-
-		// If we're not performing first sync, just start from
-		// the first message we receive
+		// If we're not performing first sync, just start from the first message we receive
 		if (!perform_first_sync) {
 			perform_first_sync = true;
 			synced             = true;
 			seq                = m.seq;
 		}
 
+		// we could try a resync, but can't guarantee server will be around long enough to
+		// reply, so we timeout after 1 second.
+		if (m.type == END_OF_SESSION) {
+			if (m.seq > seq) {
+				sync_msgs(seq, repaddr, 1000);
+			}
+			if (exit_on_terminate_msg)
+				break;
+			continue;
+		}
+
 		if (m.seq < seq) {
 			if (!synced) {
-				// Ignore msgs we've already seen. This may
-				// happen during re-sync as msgs received by oob
-				// sync may also arrive at sub socket
+				// Ignore msgs we've already seen. This may happen during re-sync as
+				// msgs received by OOB sync may also arrive at sub socket
 				continue;
 			}
-			// If we're not rsyncing and we see a message with lower
-			// seq than we expect, the server may have restarted,
-			// and we need to resync right from the start.
+			// If we're not rsyncing and we see a message with lower seq than we expect,
+			// the server may have restarted, and we need to resync right from the
+			// start.
 			seq = 0;
 		}
 
 		if (m.seq > seq) {
 			// We've missed a message, perform out-of-band sync
 			synced      = false;
-			long long s = sync_msgs(seq, repaddr);
+			long long s = sync_msgs(seq, repaddr, -1);
 			if (s < 0) {
-				nn_perror("error in sync_msgs");
 				continue;
 			}
+			// We may have gotten fewer or more messages than we were expecting.
+			// If we got more next, we ensure
+			// TODO: we should check if we got less here and do another resync
 			seq = s + 1;
 			continue;
 		}
@@ -263,7 +324,6 @@ int main(int argc, char **argv) {
 		}
 
 		if (m.type == HEARTBEAT) {
-			// heartbeat msg, ignore
 			continue;
 		}
 
@@ -276,10 +336,9 @@ int main(int argc, char **argv) {
 		m.data = NULL;
 	}
 	if (buf != NULL) {
-		nn_freemsg(buf);
+		nng_free(buf, sz);
 		buf = NULL;
 	}
-	nn_close(sub);
-	nn_term();
+	nng_close(sock);
 	return 0;
 }
