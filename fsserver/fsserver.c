a33090ac11545a43a6c0ff032b3de1609afafd4d
David Horsley <david.e.horsley@gmail.com>
2020-05-05 05:43:12 +0000

    default to local websocket port

--- fsserver/fsserver.c
+++ fsserver/fsserver.c
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#define _GNU_SOURCE
 #include <errno.h>
 #include <fcntl.h>
 #include <pthread.h>
