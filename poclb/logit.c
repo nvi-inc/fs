nvi-inc/fs

commit 651701a6cd948be6c4ebe3fa7a4dc377fbd6dc1b

Adding logging of uname info
Add logits() call in logit.c to allow control of source char
 in log entry for C.
Improve ibuf declaration in binit.f

poclb/logit.c
@@ -35,13 +35,22 @@ void pname();
 void rte_time();
 static logit0();
 
+logits(msg,ierr,who,lsor)
+char *msg;           /* a message to be logged, NULL if none */
+int ierr;            /* error number, 0 if no error          */
+char *who;           /* 2-char string identifying the error  */
+char lsor;           /* char identifying source usually ':' or '/' */
+
+{
+  logit0(msg,ierr,who,NULL,lsor);
+}
 logit(msg,ierr,who)
 char *msg;           /* a message to be logged, NULL if none */
 int ierr;            /* error number, 0 if no error          */
 char *who;           /* 2-char string identifying the error  */
 
 {
-  logit0(msg,ierr,who,NULL);
+  logit0(msg,ierr,who,NULL,'/');
 }
 logit_nd(msg,ierr,who)
 char *msg;           /* a message to be logged, NULL if none */
@@ -49,18 +58,20 @@ int ierr;            /* error number, 0 if no error          */
 char *who;           /* 2-char string identifying the error  */
 
 {
-  logit0(msg,ierr,who,"nd");
+  logit0(msg,ierr,who,"nd",'/');
 }
-static logit0(msg,ierr,who,type)
+static logit0(msg,ierr,who,type,lsor)
 char *msg;           /* a message to be logged, NULL if none */
 int ierr;            /* error number, 0 if no error          */
 char *who;           /* 2-char string identifying the error  */
 char *type;          /* data type NULL = "fs", "nd" = no display */
+char lsor;           /* char identifying source usually ':' or '/' */
 
 {
   char buf[1025];    /* Holds the complete log entry */
   char name[5];     /* The name of our main program */
   int it[6],ip1,ip2,l;
+  char ssor[2];
  
 /* First get the time and put dddhhmmss into the log entry.
 */
@@ -97,7 +108,9 @@ char *type;          /* data type NULL = "fs", "nd" = no display */
       buf[l+5]='\0';
       strcat(buf,"#");
     } else
-      strcat(buf,"/");
+      ssor[0]=lsor;
+      ssor[1]=0;
+      strcat(buf,ssor);
     if(msg!=NULL) {
       int n;
       int bufl=strlen(buf);

boss/binit.f
@@ -35,7 +35,7 @@ C
       integer idcbsk(1)                 !  dcss for schedule
       dimension lnames(13,1)            !  list of command names
       integer*4 itscb(13,1)             !  time-scheduling control block
-      integer ibuf(50)                  !  input buffer containing command
+      integer*2 ibuf(100)               !  input buffer containing command
       integer itime(9)                  !  time array returned from spars
       integer lseg(2)                   !  segment name (obsolete)
       integer*4 icloprx

boss/newlg.f
@@ -96,6 +96,10 @@ C
       nch = nch-1
       call logit3(ibuf,nch,lsor)
 C
+C   add OS info to log
+C
+      call log_uname
+C
 C     Send configuration info from control files to log
 C
       nch = 1

control/fserr.ctl
@@ -1289,6 +1289,9 @@ Error initiliazing mk6cb, internal error ?WWW
 BO -198
 More than ?WWW Mark6s requires mods to binit.f (plus fspgm.ctl, fserr.ctl, and mk6cn.c).
 ""
+BO -199
+Error getting uname info, UNIX ?FFF
+""
 BO -200
 File name too long in caccess(), while checking .prc permissions/existence
 ""

newlb/Makefile
@@ -2,7 +2,8 @@ OBJECTS= char2low.o f_readstring.o f_rewind.o f_writestrng.o\
 		 fclose.o fopen.o ftn_purge.o lower.o prog.o trimlen.o ymday.o\
 		 iday0.o rcpar.o pname.o crcc.o igetb.o putb.o\
 		 bitest.o bjtest.o iibset.o jibset.o iibclr.o jibclr.o iishftc.o\
-		 jibits.o jmvbits.o iishft.o jishft.o put_stderr.o append_safe.o
+		 jibits.o jmvbits.o iishft.o jishft.o put_stderr.o append_safe.o\
+         log_uname.o
 
 newlb.a: $(OBJECTS)
 

newlb/log_uname.c
@@ -1,0 +1,45 @@
+/*
+ * Copyright (c) 2020 NVI, Inc.
+ *
+ * This file is part of VLBI Field System
+ * (see http://github.com/nvi-inc/fs).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <sys/utsname.h>
+#include <errno.h>
+#include <string.h>
+
+#include "../include/params.h"
+
+void log_uname__()
+{
+    char buff[MAX_CLS_MSG_BYTES];
+    struct utsname uts;
+
+    if(uname(&uts)) {
+        logitn(NULL,-199,"bo",errno);
+        return;
+    }
+
+    strcpy(buff,"uname,");
+    strcat(buff,uts.sysname);
+    strcat(buff,",");
+    strcat(buff,uts.release);
+    strcat(buff,",");
+    strcat(buff,uts.version);
+    strcat(buff,",");
+    strcat(buff,uts.machine);
+    logits(buff,0," ",':');
+}

commit 1d9137bce6c236f75df50dee8795e1a9304fcb01

Correct bad construction of external log entries
Bug introduced in 651701a6cd948be6c4ebe3fa7a4dc377fbd6dc1b

Close #53

poclb/logit.c
@@ -107,10 +107,11 @@ char lsor;           /* char identifying source usually ':' or '/' */
       memcpy(buf+l,name,5);
       buf[l+5]='\0';
       strcat(buf,"#");
-    } else
+    } else {
       ssor[0]=lsor;
       ssor[1]=0;
       strcat(buf,ssor);
+    }
     if(msg!=NULL) {
       int n;
       int bufl=strlen(buf);