nvi-inc/fs

commit f3ee6ea64be008b2bb0ebcbf3f54223b43de8b23

finalize 9.11.0

quikv/antcn.c
@@ -1,0 +1,32 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h> 
+#include <string.h> 
+#include <sys/types.h>
+
+#include "../include/dpi.h"
+#include "../include/params.h"
+#include "../include/fs_types.h"
+#include "../include/fscom.h"         /* shared memory definition */
+#include "../include/shm_addr.h"      /* shared memory pointer */
+
+int antcn(long ip[5])
+{
+
+  if(0==strncmp(shm_addr->idevant,"/dev/null ",10)) {
+    ip[0]=0;
+    ip[1]=0;
+    ip[2]=-400;
+    memcpy(ip+3,"q2",2);
+    return;
+  }
+
+  ip[2]=0;
+  skd_run("antcn",'w',ip);
+
+  if(ip[2] >= 0) {
+    ip[2]=0;
+    skd_run("flagr",'n',ip);
+  }
+
+}

antcn/antcn.c
@@ -43,7 +43,7 @@
 
 /* Defined variables */
 #define MINMODE 0  /* min,max modes for our operation */
-#define MAXMODE 7
+#define MAXMODE 9
 
 /* Include files */
 
@@ -160,6 +160,8 @@ Continue:
 
     case 6:            /* reserved */
       ierr = -1;
+      strcpy(buf,"TBD focus control");
+      logit(buf,0,NULL);
       goto End;
       break;
 
@@ -168,7 +170,25 @@ Continue:
       strcpy(buf,"Checking onsource status, log tracking data");
       logit(buf,0,NULL);
       fs->ionsor = 1;
+      break;
 
+  case 8:
+      ierr = 0;
+      strcpy(buf,"Station dependent detectors access");
+      logit(buf,0,NULL);
+      break;
+
+  case 9:
+      ierr = 0;
+      strcpy(buf,"Satellite tracking mode");
+      logit(buf,0,NULL);
+      break;
+
+  default:
+      ierr = -1;
+      strcpy(buf,"Impossible to reach");
+      logit(buf,0,NULL);
+      break;
   }  /* end of switch */
 
 End:

holog/antcn.c
@@ -1,0 +1,25 @@
+#include <stdio.h>
+
+int antcn(ip1,ierr)
+     long ip1;
+     int *ierr;
+{
+  long ip[5] = {0,0,0,0,0};
+  int i;
+
+  ip[0]=ip1;
+
+  for(i=0;i<2;i++) {
+    if(brk_chk("holog")!=0) {
+      *ierr=-1;
+      return -1;
+    }
+    skd_run("antcn",'w',ip);
+    if(ip[2]>=0)
+      return 0;
+    logita(NULL,ip[2],ip+3,ip+4);
+  }
+
+  *ierr=-30;
+  return -1;
+}

commit 002089f588ba982e01bf41386b855514a5a3f914

Fix not getting return ip in quikv/antcn.c
Fixes error introduced in f3ee6ea64be008, released as 9.11.0, Jan 2013

Closes #82

quikv/antcn.c
@@ -45,6 +45,7 @@ int antcn(int ip[5])
   ip[2]=0;
   skd_run("antcn",'w',ip);
 
+  skd_par(ip);
   if(ip[2] >= 0 && (ip0 == 1 || ip0 == 9)) {
     ip[0]=1;
     ip[2]=0;