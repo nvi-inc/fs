a5c4408021c9a9dc943d125c3c92bbf9e9d0f1e4
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
2022-05-13 13:01:07 +0000

    Run udceth0.py through 2to3

--- udceth0/udceth0.py
+++ udceth0/udceth0.py
@@ -38,26 +38,26 @@ if len(sys.argv) == 2:
                 sock.sendall(msg)
  		time.sleep(2)
                 data = sock.recv(4096)
-                print data
+                print(data)
                 sock.close()
 
         except KeyboardInterrupt:
-                print ''
-                print 'Client Terminated by User - closing open sockets'
+                print('')
+                print('Client Terminated by User - closing open sockets')
 
 
         except IndexError:
-                print 'Server address not specified'
+                print('Server address not specified')
 
         except socket.error:
-                print 'Server not reachable'
+                print('Server not reachable')
 
         finally:
                 try:
                         sock.close()
                         #print >>sys.stderr, 'socket closed'
                 except NameError:
-			print 'socket not open'
+			print('socket not open')
 elif len(sys.argv) == 5:
 
 	ip = sys.argv[1]
@@ -77,25 +77,25 @@ elif len(sys.argv) == 5:
 		sock.sendall(msg)
 		time.sleep(2)
 		data = sock.recv(4096)
-		print data
+		print(data)
 		sock.close()
 
 	except KeyboardInterrupt:
-		print ''
-		print 'Client Terminated by User - closing open sockets'	
+		print('')
+		print('Client Terminated by User - closing open sockets')	
 	
 
 	except IndexError:
-		print 'Server address not specified'
+		print('Server address not specified')
 
 	except socket.error:
-		print 'Server not reachable'
+		print('Server not reachable')
 
 	finally:
 		try:
 			sock.close()
 			#print >>sys.stderr, 'socket closed'
 		except NameError:
-			print 'socket not open'
+			print('socket not open')
 else:
-	print "\n usage: udceth0.py <ip address> <freq> <ga> <gb>\n"
+	print("\n usage: udceth0.py <ip address> <freq> <ga> <gb>\n")
