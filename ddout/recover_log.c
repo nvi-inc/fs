74678d35bba768ff1acec842dd240d013b23a0c0
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
2024-01-27 11:18:11 +0000

    Simplify log recovery logic

--- ddout/recover_log.c
+++ ddout/recover_log.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021, 2023 NVI, Inc.
+ * Copyright (c) 2020-2021, 2023-2024 NVI, Inc.
  *
  * This file is part of VLBI Field System
  * (see http://github.com/nvi-inc/fs).
@@ -107,95 +107,98 @@ int fd;
   off_t size, offset;
   static char buf_copy[BUFFSIZE];
 
-  fail=FALSE;
   fd2=open(lnamef,O_RDONLY);  /* check to see if the file exists */
-  if(fd2<0 && errno == ENOENT) {
-    shm_addr->abend.other_error=1;
-    fprintf(stderr,"\007!! help! ** log file '%s' doesn't exist, attempting to recover by copying.\n",
-	    lnamef);
-
-    fd2 = open(lnamef, O_RDWR|O_SYNC|O_CREAT,PERMISSIONS); /* try to create it */
-    if (fd2 < 0) {
-      fprintf(stderr,
-	      "\007!! help! ** can't create file '%s', giving up\n",
-	      lnamef);
-      fail=TRUE;
-    } else {
-
-      /* now try to make a copy */
-      size=lseek(fd,0L,SEEK_CUR);
-      if(size < 0)
-	perror("\007!! help! ** determining size of old file to copy: no progress meter");
-      offset=lseek(fd, 0L, SEEK_SET);
-      if(offset < 0) {
-	perror("\007!! help! ** rewinding old file to copy");
-	fprintf(stderr,"\007!! help! ** can't rewind original file, giving up\n");
-	fail=TRUE;
-      } else {
-	count=0;
-	countw=0;
-	cum=0;
-	rte_rawt(&before);
-	seconds=2;
-	fprintf(stderr,
-		"\007!! help! ** copying to recover log file '%s', please wait ... starting.\n",
-		lnamef);
-	while(count==countw && 0 < (count= read(fd,buf_copy,BUFFSIZE))) {
-	  countw= write(fd2,buf_copy,count);
-	  if(size >0) {
-	    cum+=count;
-	    rte_rawt(&after);
-	    if((after-before)>seconds*100) {
-	      fprintf(stderr,
-		      "\007!! help! ** copying to recover log file '%s', please wait ... %2d%%\n",
-		      lnamef, (int) (cum*100./size));
-	      seconds=seconds+2;
-	    }
-	  }
-	}
-	if(count < 0) {
-	  fprintf(stderr,"\007!! help! ** failed, error reading original file, giving up\n",lnamef);
-	  perror("\007!! help! ** reading original file");
-	  fail=TRUE;
-	} else if (count!=0 && count!=countw) {
-	  fprintf(stderr,"\007!! help! ** failed, error writing to '%s', giving up\n",lnamef);
-	  perror("\007!! help! ** writing recovered log");
-	  fail=TRUE;
-	} else
-	  fprintf(stderr,
-		"!! help! ** copying to recover log file '%s', done.\n",
-		lnamef);
-      }
-    }
-
-    if(fail) {
-      fprintf(stderr,"\007!! help! ** You can attempt to recover by unmounting the file system and\n");
-      fprintf(stderr,"\007!! help! ** grep-ing the file system for lines starting with the date\n");
-      fprintf(stderr,"\007!! help! ** portion of time tag for the date(s) of the session. Try to\n");
-      fprintf(stderr,"\007!! help! ** do as little as possible to the file system until you\n");
-      fprintf(stderr,"\007!! help! ** dismount it. Please see /usr2/fs/misc/logrecovery.txt for details.\n");
-    } else {
-      int ierr;
-
-      fprintf(stderr,"!! help! ** good news, log file '%s' seems to be recovered, please check it.\n",lnamef);
-      ierr=write_comment(lnamef,fd2,buf_copy);
-      if(ierr < -5) {
-        fprintf(stderr,"\007!! help! ** problem adding comment at end of recovered file, see above, may be benign\n");
-        shm_addr->abend.other_error=1;
-      } else if (ierr < -3) {
-        fprintf(stderr,"\007!! help! ** problem adding pre-comment new-line at end of recovered file, see above, may be benign\n");
-        shm_addr->abend.other_error=1;
-      } else if (ierr < 0) {
-        fprintf(stderr,"\007!! help! ** problem checking for new-line at end of recovered file, see above, may be benign\n");
-        shm_addr->abend.other_error=1;
-      } else
-        fprintf(stderr,"!! help! ** recovery comment successfully added to recovered log.\n");
-    }
-  } else if (fd2 < 0) {
-    shm_addr->abend.other_error=1;
-    perror("\007!! help! ** checking for existence of log file");
+  if (fd2 >= 0) {
+    goto done;
   }
 
+  shm_addr->abend.other_error=1;
+  if(errno != ENOENT) {
+    perror("\007!! help! ** checking for existence of log file");
+    return fd;
+  }
+
+  fail=FALSE;
+  fprintf(stderr,"\007!! help! ** log file '%s' doesn't exist, attempting to recover by copying.\n",
+          lnamef);
+
+  fd2 = open(lnamef, O_RDWR|O_SYNC|O_CREAT,PERMISSIONS); /* try to create it */
+  if (fd2 < 0) {
+    fprintf(stderr,
+            "\007!! help! ** can't create file '%s', giving up\n",
+            lnamef);
+    fail=TRUE;
+    goto fail;
+  }
+
+  /* now try to make a copy */
+  size=lseek(fd,0L,SEEK_CUR);
+  if(size < 0)
+    perror("\007!! help! ** determining size of old file to copy: no progress meter");
+  offset=lseek(fd, 0L, SEEK_SET);
+  if(offset < 0) {
+    perror("\007!! help! ** rewinding old file to copy");
+    fprintf(stderr,"\007!! help! ** can't rewind original file, giving up\n");
+    fail=TRUE;
+    goto fail;
+  }
+  count=0;
+  countw=0;
+  cum=0;
+  rte_rawt(&before);
+  seconds=2;
+  fprintf(stderr,
+          "\007!! help! ** copying to recover log file '%s', please wait ... starting.\n",
+          lnamef);
+  while(count==countw && 0 < (count= read(fd,buf_copy,BUFFSIZE))) {
+    countw= write(fd2,buf_copy,count);
+    if(size >0) {
+      cum+=count;
+      rte_rawt(&after);
+      if((after-before)>seconds*100) {
+        fprintf(stderr,
+                "\007!! help! ** copying to recover log file '%s', please wait ... %2d%%\n",
+                lnamef, (int) (cum*100./size));
+        seconds=seconds+2;
+      }
+    }
+  }
+  if(count < 0) {
+    fprintf(stderr,"\007!! help! ** failed, error reading original file, giving up\n",lnamef);
+    perror("\007!! help! ** reading original file");
+    fail=TRUE;
+  } else if (count!=0 && count!=countw) {
+    fprintf(stderr,"\007!! help! ** failed, error writing to '%s', giving up\n",lnamef);
+    perror("\007!! help! ** writing recovered log");
+    fail=TRUE;
+  } else
+    fprintf(stderr,
+            "!! help! ** copying to recover log file '%s', done.\n",
+            lnamef);
+
+fail:
+  if(fail) {
+    fprintf(stderr,"\007!! help! ** You can attempt to recover by unmounting the file system and\n");
+    fprintf(stderr,"\007!! help! ** grep-ing the file system for lines starting with the date\n");
+    fprintf(stderr,"\007!! help! ** portion of time tag for the date(s) of the session. Try to\n");
+    fprintf(stderr,"\007!! help! ** do as little as possible to the file system until you\n");
+    fprintf(stderr,"\007!! help! ** dismount it. Please see /usr2/fs/misc/logrecovery.txt for details.\n");
+  } else {
+    int ierr;
+
+    fprintf(stderr,"!! help! ** good news, log file '%s' seems to be recovered, please check it.\n",lnamef);
+    ierr=write_comment(lnamef,fd2,buf_copy);
+    if(ierr < -5)
+      fprintf(stderr,"\007!! help! ** problem adding comment at end of recovered file, see above, may be benign\n");
+    else if (ierr < -3)
+      fprintf(stderr,"\007!! help! ** problem adding pre-comment new-line at end of recovered file, see above, may be benign\n");
+    else if (ierr < 0)
+      fprintf(stderr,"\007!! help! ** problem checking for new-line at end of recovered file, see above, may be benign\n");
+    else
+      fprintf(stderr,"!! help! ** recovery comment successfully added to recovered log.\n");
+  }
+
+done:
   if(fd2 >= 0 && close(fd2) < 0) {
     shm_addr->abend.other_error=1;
     perror("\007!! help! ** closing fd2");
