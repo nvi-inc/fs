31601504fb3cb613303193d20f8530e8f4539bfb
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
2024-04-09 22:46:28 +0000

    Change rdbeX=... command to rdbe=X,...

--- quikv/rdbe.c
+++ quikv/rdbe.c
@@ -38,108 +38,131 @@ int ip[5];                           /* ipc parameters */
 {
   int ilast, ierr, ind, i, count, j;
   char *ptr;
-      char *arg_next();
-      int out_recs[MAX_RDBE], out_class[MAX_RDBE];
-      char outbuf[BUFSIZE];
-      int iplast[5];
-      int iwhich;
-      int rtn_class=0;
-      int rtn_recs=0;
-      int some;
+  char *arg_next();
+  int out_recs[MAX_RDBE], out_class[MAX_RDBE];
+  char outbuf[BUFSIZE];
+  int iplast[5];
+  int iwhich;
+  int rtn_class=0;
+  int rtn_recs=0;
+  int some;
+  int irdbe;
+  char rdbe, *prdbe;
 
-      void skd_run(), skd_par();      /* program scheduling utilities */
+  void skd_run(), skd_par();      /* program scheduling utilities */
 
-      some=FALSE;
-      for (i=0;i<MAX_RDBE;i++)
-	if(shm_addr->rdbe_active[i]!=0){
-	  some=TRUE;
-	  break;
-	}
+  irdbe=0;   /* assume this is an all device command to begin with */
 
-      if(!some) {
-	ierr=-302;
-	goto error;
-      }
+  if (command->equal != '=') {
+    ierr=-301;
+    goto error;
+  }
 
-      if (command->equal != '=' ||
-          command->argv[0]==NULL ) {
-	ierr=-301;
-	goto error;
-      }
+  /* is the first parameter the RDBE to address? */
 
-/* if we get this far it is a set-up command so parse it */
+  rdbe=' ';
+  ierr=arg_char(command->argv[0],&rdbe,' ',FALSE);
+  if(ierr ==-100) {
+    ierr=0;
+    goto parse;
+  }
+  prdbe=strchr(unit_letters+1,rdbe);
+  if(ierr==-200||prdbe==NULL|| prdbe-unit_letters>MAX_RDBE ) {
+    ierr=0;
+    goto parse;
+  }
+
+  /* if we get this far it is for one device */
+
+  irdbe=prdbe-unit_letters;
+
+  /* if we get this far it is a set-up command so parse it */
 
 parse:
-      for (i=0;i<MAX_RDBE;i++)
-	if(itask == i+1 || shm_addr->rdbe_active[i]!=0 && itask ==0 ) {
-	  out_recs[i]=0;
-	  out_class[i]=0;
-	}
+  some=FALSE;
+  for (i=0;i<MAX_RDBE;i++)
+    if(irdbe == i+1 || shm_addr->rdbe_active[i]!=0 && irdbe ==0 ) {
+      some=TRUE;
+      out_recs[i]=0;
+      out_class[i]=0;
+    }
 
-      ilast=0;                                      /* last argv examined */
-      ptr=arg_next(command,&ilast);
+  if(irdbe==0)
+    if(!some) {
+      ierr=-302;
+      goto error;
 
-      while( ptr != NULL) {
-	strcpy(outbuf,ptr);
-	strcpy(outbuf,ptr);
-	if(outbuf[0]!=0 && outbuf[strlen(outbuf)-1]!=';')
-	   strcat(outbuf,";");
-	strcat(outbuf,"\n");
-	for (i=0;i<MAX_RDBE;i++)
-	  if(itask == i+1 || shm_addr->rdbe_active[i]!=0 && itask == 0) {
-	    cls_snd(&out_class[i], outbuf, strlen(outbuf), 0, 0);
-	    out_recs[i]++;
-	  }
-	ptr=arg_next(command,&ilast);
+    }
+  ilast=0;                                      /* last argv examined */
+  if(irdbe!=0)
+    ilast++;
+
+  ptr=arg_next(command,&ilast);
+  if(ptr==NULL) {
+    ierr=-101;
+    goto error;
+  }
+
+  while( ptr != NULL) {
+    strcpy(outbuf,ptr);
+    if(outbuf[0]!=0 && outbuf[strlen(outbuf)-1]!=';')
+      strcat(outbuf,";");
+    strcat(outbuf,"\n");
+    for (i=0;i<MAX_RDBE;i++)
+      if(irdbe == i+1 || shm_addr->rdbe_active[i]!=0 && irdbe == 0) {
+        cls_snd(&out_class[i], outbuf, strlen(outbuf), 0, 0);
+        out_recs[i]++;
       }
+    ptr=arg_next(command,&ilast);
+  }
 
 rdbcn:
-      for (i=0;i<MAX_RDBE;i++)
-	if(itask == i+1 || shm_addr->rdbe_active[i]!=0 && itask == 0) {
-	  char name[6];
-	  ip[0]=6;
-	  ip[1]=out_class[i];
-	  ip[2]=out_recs[i];
-	  sprintf(name,"rdbc%c",unit_letters[i+1]);
-	  iwhich=i+1;
-	  skd_run_p(name,'p',ip,&iwhich); /* from here until the last
-                                            skd_run_p(NULL,'w',...) is
-                                            processed, we have to handle
-                                            errors locally, no passing up
-                                            i.e. too hard to unwind */
-	}
-      for(j=0;j<5;j++)
-	iplast[j]=0;
+  for (i=0;i<MAX_RDBE;i++)
+    if(irdbe == i+1 || shm_addr->rdbe_active[i]!=0 && irdbe == 0) {
+      char name[6];
+      ip[0]=6;
+      ip[1]=out_class[i];
+      ip[2]=out_recs[i];
+      sprintf(name,"rdbc%c",unit_letters[i+1]);
+      iwhich=i+1;
+      skd_run_p(name,'p',ip,&iwhich); /* from here until the last
+                                         skd_run_p(NULL,'w',...) is
+                                         processed, we have to handle
+                                         errors locally, no passing up
+                                         i.e. too hard to unwind */
+    }
+  for(j=0;j<5;j++)
+    iplast[j]=0;
 
-      for (i=0;i<MAX_RDBE;i++)
-	if(itask == i+1 || shm_addr->rdbe_active[i]!=0 && itask == 0) {
-	  skd_run_p(NULL,'w',ip,&iwhich);
-	  skd_par(ip);
-	  rdbe_dis(command,itask,iwhich,ip,&rtn_class,&rtn_recs);
-	  if(itask !=0)
-	    continue;
+  for (i=0;i<MAX_RDBE;i++)
+    if(irdbe == i+1 || shm_addr->rdbe_active[i]!=0 && irdbe == 0) {
+      skd_run_p(NULL,'w',ip,&iwhich);
+      skd_par(ip);
+      rdbe_dis(command,iwhich,ip,&rtn_class,&rtn_recs);
+      if(irdbe !=0)
+        continue;
 
-	  if(ip[2]!=0 && iplast[2]!=0) {
-	    logita(NULL,iplast[2],iplast+3,iplast+4);
-	  }
-	  if(ip[2]!=0)
-	    for(j=2;j<5;j++)
-	      iplast[j]=ip[j];
-	}
-                                 /* local error processing no longer require */
-      if(iplast[2]!=0)
-	for(j=2;j<5;j++)
-	  ip[j]=iplast[j];
+      if(ip[2]!=0 && iplast[2]!=0) {
+        logita(NULL,iplast[2],iplast+3,iplast+4);
+      }
+      if(ip[2]!=0)
+        for(j=2;j<5;j++)
+          iplast[j]=ip[j];
+    }
+  /* local error processing no longer require */
+  if(iplast[2]!=0)
+    for(j=2;j<5;j++)
+      ip[j]=iplast[j];
 
-      ip[0]=rtn_class;
-      ip[1]=rtn_recs;
+  ip[0]=rtn_class;
+  ip[1]=rtn_recs;
 
-      return;
+  return;
 
 error:
-      ip[0]=0;
-      ip[1]=0;
-      ip[2]=ierr;
-      memcpy(ip+3,"2m",2);
-      return;
+  ip[0]=0;
+  ip[1]=0;
+  ip[2]=ierr;
+  memcpy(ip+3,"2m",2);
+  return;
 }
--- quikv/rdbe_dis.c
+++ quikv/rdbe_dis.c
@@ -32,95 +32,90 @@
 #define BUFSIZE MAX_CLS_MSG_BYTES
 extern char unit_letters[];
 
-void rdbe_dis(command,itask,iwhich,ip,out_class,out_recs)
+void rdbe_dis(command,iwhich,ip,out_class,out_recs)
 struct cmd_ds *command;
-int itask,iwhich;
+int iwhich;
 int ip[5];
 int *out_class;
 int *out_recs;
 {
-      int ierr, count, i;
-      char output[MAX_OUT],*start;
-      int rtn1;    /* argument for cls_rcv - unused */
-      int rtn2;    /* argument for cls_rcv - unused */
-      int msgflg=0;  /* argument for cls_rcv - unused */
-      int save=0;    /* argument for cls_rcv - unused */
-      int nchars;
-      char inbuf[BUFSIZE+1],*first;
-      int n;
-      char who[3];
+  int ierr, count, i;
+  char output[MAX_OUT],*start;
+  int rtn1;    /* argument for cls_rcv - unused */
+  int rtn2;    /* argument for cls_rcv - unused */
+  int msgflg=0;  /* argument for cls_rcv - unused */
+  int save=0;    /* argument for cls_rcv - unused */
+  int nchars;
+  char inbuf[BUFSIZE+1],*first;
+  int n;
+  char who[3];
 
-   /* format output buffer */
+  /* format output buffer */
 
-      if(itask == 0)
-	sprintf(output,"%s%c",command->name,unit_letters[iwhich]);
-      else
-	strcpy(output,command->name);
+  sprintf(output,"%s(%c)/",command->name,unit_letters[iwhich]);
+  start=output+strlen(output);
 
-      strcat(output,"/");
-      start=output+strlen(output);
+  for (i=0;i<ip[1];i++) {
+    if ((nchars =
+          cls_rcv(ip[0],inbuf,BUFSIZE,&rtn1,&rtn2,msgflg,save)) <= 0) {
+      ierr = -401;
+      goto error;
+    }
+    inbuf[nchars]=0;
 
-      for (i=0;i<ip[1];i++) {
-	if ((nchars =
-	     cls_rcv(ip[0],inbuf,BUFSIZE,&rtn1,&rtn2,msgflg,save)) <= 0) {
-	  ierr = -401;
-	  goto error;
-	}
-	inbuf[nchars]=0;
-
-	first=inbuf;
-	while(strlen(first)>0) {
-	  *start=0;
-	  if(strlen(first)+1<=sizeof(output)-strlen(output)) {
-	    strcpy(start,first);
-	    if(strlen(output)>0 && output[strlen(output)-1]=='\n')
-	      output[strlen(output)-1]='\0';
-	    first+=strlen(first);
-	  } else {
-	    int last;
-	    n=sizeof(output)-strlen(output)-1;
-	    for(last=n;last>(n-35) && last>0;last--) {
-	      if(first[last-1]==':') {
-		n=last;
-		break;
-	      }
-	    }
-	    if(index(":",first[n-1])==NULL)
-	      for(last=n;last>(n-35) && last>0;last--) {
-		if(first[last-1]==',') {
-		  n=last;
-		  break;
-		}
-	      }
-	    if(index(":,",first[n-1])==NULL)
-	      for(last=n;last>(n-35) && last>1;last--) {
-		if(first[last-1]==' ') {
-		  n=last-1;
-		  break;
-		}
-	      }
-	    strncpy(start,first,n);
-	    start[n]=0;
-	    first+=n;
-	  }
-	  if(strlen(start)>0) {
-	    cls_snd(out_class,output,strlen(output),0,0);
-	    (*out_recs)++;
-	  }
-	}
+    first=inbuf;
+    while(strlen(first)>0) {
+      *start=0;
+      if(strlen(first)+1<=sizeof(output)-strlen(output)) {
+        strcpy(start,first);
+        if(strlen(output)>0 && output[strlen(output)-1]=='\n')
+          output[strlen(output)-1]='\0';
+        first+=strlen(first);
+      } else {
+        int last;
+        n=sizeof(output)-strlen(output)-1;
+        for(last=n;last>(n-35) && last>0;last--) {
+          if(first[last-1]==':') {
+            n=last;
+            break;
+          }
+        }
+        if(index(":",first[n-1])==NULL)
+          for(last=n;last>(n-35) && last>0;last--) {
+            if(first[last-1]==',') {
+              n=last;
+              break;
+            }
+          }
+        if(index(":,",first[n-1])==NULL)
+          for(last=n;last>(n-35) && last>1;last--) {
+            if(first[last-1]==' ') {
+              n=last-1;
+              break;
+            }
+          }
+        strncpy(start,first,n);
+        start[n]=0;
+        first+=n;
       }
+      if(strlen(start)>0) {
+        cls_snd(out_class,output,strlen(output),0,0);
+        (*out_recs)++;
+      }
+    }
+  }
 
-      return;
+  return;
 
 error:
-      cls_clr(ip[0]);
-      ip[0]=0;
-      ip[1]=0;
-      if(ip[2]!=0)
-	logit(NULL,ip[2],ip+3);
-      ip[2]=ierr;
-      memcpy(ip+3,"2m",2);
-      snprintf(who,3,"c%c",unit_letters[iwhich]);
-      memcpy(ip+4,who,2);
-      return;
+  cls_clr(ip[0]);
+  ip[0]=0;
+  ip[1]=0;
+  if(ip[2]!=0)
+    logit(NULL,ip[2],ip+3);
+  ip[2]=ierr;
+  memcpy(ip+3,"2m",2);
+  snprintf(who,3,"c%c",unit_letters[iwhich]);
+  memcpy(ip+4,who,2);
+  return;
 }
