d34fe57c37e091925b09b2389787a613407a248d
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
2024-08-30 14:32:38 +0000

    Correct error numbers/messages for rdbe_2_rdbe_dot
    Includes correcy rdbe_dot_mon data structure

--- include/rdbe_dot_ds.h
+++ include/rdbe_dot_ds.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 NVI, Inc.
+ * Copyright (c) 2020, 2024 NVI, Inc.
  *
  * This file is part of VLBI Field System
  * (see http://github.com/nvi-inc/fs).
@@ -25,21 +25,9 @@ struct rdbe_dot_mon {
     struct m5state state;
   } time ;
   struct {
-    char status[33];
-    struct m5state state;
-  } status ;
-  struct {
-    char OS_time[33];
-    struct m5state state;
-  } OS_time ;
-  struct {
     char DOT_OS_time_diff[33];
     struct m5state state;
   } DOT_OS_time_diff ;
-  struct {
-    char Actual_DOT_time[33];
-    struct m5state state;
-  } Actual_DOT_time ;
   struct {
     int vdif_epoch;
     struct m5state state;
