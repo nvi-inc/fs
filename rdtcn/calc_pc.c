08cc1ee284a2c4ab9452442e48e480ebad46576b
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
2025-05-01 22:15:55 +0000

    WIP: calc ts and pc

---
+++ rdtcn/calc_pc.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2020-2023 NVI, Inc.
+ *
+ * This file is part of VLBI Field System
+ * (see http://github.com/nvi-inc/fs).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <math.h>
+
+#include "../include/params.h"
+#include "../include/fs_types.h"
+#include "../include/fscom.h"
+
+#include "packet_r2dbe.h"
+
+extern struct fscom *shm_addr;
+
+void calc_pc( r2dbe_multicast_t *t)
+{
+    int i;
+    double x[4096],y[4096],amp[4096],phs[4096];
+    double xt,yt, theta, cost,sint;
+    for (i=0;i<4096;i++) {
+      theta=-2*M_PI*t->pcal_freq*(i%8)/4096e6;
+      cost=cos(theta);
+      sint=sin(theta);
+      x[i]=t->pcal_cos[i]*cost-t->pcal_sin[i]*sint;
+      y[i]=t->pcal_sin[i]*cost+t->pcal_cos[i]*sint;
+    }
+    FFT(1,12,&x,&y);
+    for (i=0;i<4096;i++) {
+	    amp[i]=1e-7*sqrt(pow(x[i],2.0)+pow(y[i],2.0));
+      phs[i]=atan2(y[i],x[i])*180/M_PI;
+    }
+#ifdef WEH
+    printf(" pcal_ifx %d\n",t->pcal_ifx);
+    for (i=0;i<4096;i++) {
+        printf(" i %d pcal_sin %d pcal_cos %d amp %g phs %g\n",
+            i,t->pcal_sin[i],t->pcal_cos[i],amp[i],phs[i]);
+    }
+#endif
+}
