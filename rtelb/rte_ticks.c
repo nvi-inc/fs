bc2ec035f899d09bce43aa8845a8d2cd1e96552a
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
2026-04-12 13:03:45 +0000

    Cleanup computer time model implementation
    * Computer time model (in ticks) is now referenced to seconds at boot,
      maximum range of about 497 days, instead of to (incorrect) ticks at
      boot. The error was in rte_ticks().
    * Retain 248 as the limit from boot time in case non-computer model is needed.
    * rte_rawt() and rte_ticks() have converged as a result.
    * Previously, rte_ticks() then rte_fixt() did not work correctly. This
      only caused a problem in fmset, which had a workaround, any other
      place that used actual time, used rte_rawt() then rte_fixt(), which
      worked correctly.

--- rtelb/rte_ticks.c
+++ rtelb/rte_ticks.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 NVI, Inc.
+ * Copyright (c) 2020, 2026 NVI, Inc.
  *
  * This file is part of VLBI Field System
  * (see http://github.com/nvi-inc/fs).
@@ -18,6 +18,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 /* rte_ticks.c - return raw system ticks in clock HZ */
+/*              used when when only approximate relative time is needeed */
+
 
 #include <stdlib.h>
 #include <sys/times.h>
@@ -33,14 +35,28 @@ int *lRawTicks;
 {
      struct tms buffer;
      clock_t ticks;
-     
-     ticks=times(&buffer);
-     if(ticks == (clock_t) -1) {
-       perror("using times()");
-       exit(-1);
+
+     int index=01 & shm_addr->time.index;
+     if(shm_addr->time.model!='c'
+             && shm_addr->time.epoch[index]!=0
+             && shm_addr->time.icomputer[index]==0 ) {
+         ticks=times(&buffer);
+         if(ticks == (clock_t) -1) {
+             perror("using times()");
+             exit(-1);
+         }
+         /* limit about 248 days */
+         *lRawTicks=(signed) ((unsigned int) ticks - shm_addr->time.ticks_off);
+     } else {
+        struct timeval tv;
+         if(0!= gettimeofday(&tv, NULL)) {
+             perror("getting timeofday, fatal\n");
+             exit(-1);
+         }
+         /* limit about 497 days */
+         *lRawTicks=(tv.tv_sec-shm_addr->time.secs_off)*100+
+             +tv.tv_usec/10000;
      }
-     *lRawTicks=(signed) ((unsigned int) ticks - shm_addr->time.ticks_off);
 
      return;
 }
-     
