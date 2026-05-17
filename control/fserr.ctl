cbf76251635dc6e8dbd6f774c4676fba5f244942
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
2026-05-17 02:41:14 +0000

    Handle DBBC3 commands for percentage loss

--- control/fserr.ctl
+++ control/fserr.ctl
@@ -2877,7 +2877,7 @@ DN  -26
 DBBC3 multicast: Time-out with no DBBC3 commands (data_valid=on); will now count time-outs in background.
 ""
 DN  -27
-DBBC3 multicast: You should AVOID using DBBC3 commands (and FMSET) while recording, calibration data is being lost.
+DBBC3 multicast: Using DBBC3 commands (or FMSET) while recording is probably causing loss of calibration date (TO).
 ""
 DN  -28
 DBBC3 multicast: No messages were received in the last minute; continuing to count time-outs.
@@ -2926,6 +2926,9 @@ DBBC3 multicast: DBBC3 personality should be DBBC_E (equip.ctl), but it is DBBC_
 ""
 DN  -43
 DBBC3 multicast: DBBC3 personality should be DBBC_E (equip.ctl), but it is DBBC_V, see DN -30 error above
+""
+DN  -44
+DBBC3 multicast: Using DBBC3 commands (or FMSET) while recording is probably causing loss of calibration date (%).
 ""
 DO -111
 Can't use '*' (previous value) for external-LO IF.
--- dbtcn/read_mcast.c
+++ dbtcn/read_mcast.c
@@ -74,6 +74,7 @@ ssize_t read_mcast(int sock, char buf[], size_t buf_size, int it[6],
     static int irecv;
     static int recv_start;
     static int percent=-1;
+    static int was_recv_valid_error;
 
     if(!recv_start)
        rte_ticks(&recv_start);
@@ -189,10 +190,18 @@ ssize_t read_mcast(int sock, char buf[], size_t buf_size, int it[6],
 //            printf(" debug_ticks%6000 %4d\n", debug_ticks%6000);
         irecv=(irecv+1)%MAX_RECV;
         rte_ticks(recv+irecv);
-        if(!data_valid)
-            if(shm_addr->dbbc3_command_active ||
-                    shm_addr->dbbc3_command_count != was_count_recv)
+        if(shm_addr->dbbc3_command_active ||
+                shm_addr->dbbc3_command_count != was_count_recv) {
+// debug percent:
+//            printf(" irecv %d recv[irecv] %d >was_recv+500 %d\n",
+//                     irecv,recv[irecv],was_recv+500);
+            if(!data_valid)
                 rte_ticks(&recv_start);
+            else if(to_count <= 0 && recv[irecv]>=was_recv_valid_error+500) { /* <= for not counting OR none so far */
+                logit(NULL,-44,"dn");
+                was_recv_valid_error=recv[irecv];
+            }
+        }
         if(recv_start<=recv[irecv]-60*100) {
             int i;
             int icount_recv=0;
@@ -206,15 +215,15 @@ ssize_t read_mcast(int sock, char buf[], size_t buf_size, int it[6],
             int expected=60/(shm_addr->dbbc3_mcast_arrival/100+1);
             float factor=1-percent/100.0;
             int limit=expected*factor+0.5;
-            if(limit<1)
-                limit=1;
+            if (limit>=expected)
+                limit=expected-1;;
 // debug percent:
 //                     printf(" icount_recv %d max count %d\n", icount_recv,limit);
-            if(icount_recv<=limit && to_count < 0) {
+            if(icount_recv<=limit && to_count < 0) { /* < for not counting */
                 if(data_valid)
-                    logitn(NULL,-29,"dn",100*(expected-icount_recv)/expected);
+                    logitn(NULL,-29,"dn",(int) (0.5+100.0*(expected-icount_recv)/expected));
                 else
-                    logitn(NULL,29,"dn",100*(expected-icount_recv)/expected);
+                    logitn(NULL,29,"dn",(int) (0.5+100.0*(expected-icount_recv)/expected));
                 rte_ticks(&recv_start);
             }
         }
